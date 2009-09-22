76a0e42a1bddc0808c256bc628f8ba914cc73fb2
Scott Graham <scott.hgrc@h4ck3r.net>
2009-09-21 23:21:37 -0700

    fix compilation of triple quoted strings

---
+++ test/run/t200.py
@@ -0,0 +1,9 @@
+print """this is a triple quote string"""
+print '''this is a triple tick string'''
+print """this is a triple quote
+string that spans
+multiple lines"""
+print '''this is a triple tick string
+that spans
+multiple lines
+'''
---
+++ test/run/t201.py
@@ -0,0 +1,8 @@
+print """this is a triple quote
+string that '"'' spans lines
+and "" '"\\n \nhas crazy '""
+crap ''' embedded"""
+print '''this is a triple tick
+string that '"'' spans lines
+and "" '"\\n \nhas crazy '""
+crap """ embedded'''
