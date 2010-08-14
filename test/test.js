549a6e46fc4cc57ea3d83847f8ecfb521ca09501
Scott Graham <scott.hgrc@h4ck3r.net>
2010-08-14 14:55:51 -0700

    couple more tests work

--- test/test.js
+++ test/test.js
@@ -317,7 +317,7 @@ function main()
         print(sprintf("symtab: %d/%d", symtabpass, symtabpass + symtabfail));
     }
 
-    for (i = 0; i <= 144; ++i)
+    for (i = 0; i <= 146; ++i)
     {
         testRun(sprintf("test/run/t%02d", i));
     }
