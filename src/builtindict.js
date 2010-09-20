3edb69d9243eb1b18fd0bec28995f05a2eb80dd4
Scott Graham <scott.hgrc@h4ck3r.net>
2010-09-19 20:49:30 -0700

    oops, add missing file broken since big checkin for advanced compilation

---
+++ src/builtindict.js
@@ -0,0 +1,29 @@
+Sk.builtins = {
+'range': Sk.builtin.range,
+'len': Sk.builtin.len,
+'min': Sk.builtin.min,
+'max': Sk.builtin.max,
+'abs': Sk.builtin.abs,
+'ord': Sk.builtin.ord,
+'chr': Sk.builtin.chr,
+'dir': Sk.builtin.dir,
+'repr': Sk.builtin.repr,
+'open': Sk.builtin.open,
+'isinstance': Sk.builtin.isinstance,
+'hash': Sk.builtin.hash,
+'getattr': Sk.builtin.getattr,
+
+'dict': Sk.builtin.dict,
+'file': Sk.builtin.file,
+'function': Sk.builtin.func,
+'generator': Sk.builtin.generator,
+'list': Sk.builtin.list,
+'long': Sk.builtin.lng,
+'method': Sk.builtin.method,
+'object': Sk.builtin.object,
+'slice': Sk.builtin.slice,
+'str': Sk.builtin.str,
+'tuple': Sk.builtin.tuple,
+'type': Sk.builtin.type
+};
+goog.exportSymbol("Sk.builtins", Sk.builtins);
