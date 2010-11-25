97c39038c90ce0670a9a97e15c4a9504da7740e8
Scott Graham <scott.hgrc@h4ck3r.net>
2010-11-24 18:56:48 -0800

    add not-working tests

---
+++ test/run/t258.py
@@ -0,0 +1,1 @@
+print [x for x in range(1,10) if False] or ["hello" for x in range(1,10) if True]
---
+++ test/run/t259.py
@@ -0,0 +1,25 @@
+print "-----"
+print [] and 5
+print {} and 5
+print False and 5
+print True and 5
+print "-----"
+print [] or 5
+print {} or 5
+print False or 5
+print True or 5
+print "-----"
+print 5 and []
+print 5 and {}
+print 5 and False
+print 5 and True
+print "-----"
+print 5 or []
+print 5 or {}
+print 5 or False
+print 5 or True
+print "-----"
+print [] or {}
+print {} or []
+print [] or False
+print [] or True
