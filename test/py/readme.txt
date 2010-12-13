f7cf2c9f6f6ed0c3abb38505376b2afef10d85c6
Scott Graham <scott.hgrc@h4ck3r.net>
2010-12-12 22:46:08 -0800

    add some tests from py test suite

---
+++ test/py/readme.txt
@@ -0,0 +1,3 @@
+These are taken from the CPython 2.x test suite.
+
+Some support code has been modified. (test_support, unittest, etc.)
---
+++ test/py/test/test_support.py
@@ -0,0 +1,19 @@
+import unittest
+
+def run_unittest(*classes):
+    """Run tests from unittest.TestCase-derived classes."""
+    valid_types = (unittest.TestSuite, unittest.TestCase)
+    suite = unittest.TestSuite()
+    for cls in classes:
+        if isinstance(cls, str):
+            if cls in sys.modules:
+                suite.addTest(unittest.findTestCases(sys.modules[cls]))
+            else:
+                raise ValueError("str arguments must be keys in sys.modules")
+        elif isinstance(cls, valid_types):
+            suite.addTest(cls)
+        else:
+            suite.addTest(unittest.makeSuite(cls))
+    _run_suite(suite)
+
+
