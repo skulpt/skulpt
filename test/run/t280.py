37cadd894534ffe40b63e244207514cefca297fa
Scott Graham <scott.hgrc@h4ck3r.net>
2010-12-12 22:45:52 -0800

    add wip test on star args

---
+++ test/run/t280.py
@@ -0,0 +1,26 @@
+def f(*a):
+    print a
+
+"""
+def g(x, *a):
+    print x, a
+
+def h(x, y, *a):
+    print x, y, a
+
+def i(x, y=4, *a):
+    print x, y, a
+    """
+
+f()
+"""f(1)
+f(1, 2, 3)
+g(1)
+g(1, 2, 3)
+h(1, 2)
+h(1, 2, 3)
+h(1, 2, 3, 4)
+i(1)
+i(1, 2, 3)
+i(1, 2, 3, 4)
+"""
