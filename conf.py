4b1904f90cd501a990da6f085a7b669c2d12a493
jelkner <jeff@elkner.net>
2011-03-31 14:21:08 -0400

    Working on chapter 3.

--- conf.py
+++ conf.py
@@ -105,10 +105,10 @@ html_style = "style.css"
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "How to Think Like a Computer Scientist: Learning with Python 3"
+html_title = "How to Think Like a Computer Scientist: Learning with Python"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = "How to Think Like a Computer Scientist: Learning with Python 3"
+html_short_title = "How to Think Like a Computer Scientist: Learning with Python"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -174,7 +174,7 @@ latex_font_size = '12pt'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'thinkcspy3.tex', u'How to Think Like a Computer Scientist: Learning with Python 3 Documentation',
+  ('index', 'thinkcspy3.tex', u'How to Think Like a Computer Scientist: Learning with Python Documentation',
    u'Peter Wentworth, Jeffrey Elkner, Allen B. Downey and Chris Meyers', 'manual'),
 ]
 
