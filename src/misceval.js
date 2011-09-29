dd907904047062099b8e054efb822e03eb2cba87
Brad Miller <bonelake@gmail.com>
2011-09-29 07:00:13 -0500

    Remove my hack from print to make it python3 like...  The same thing can be done in my private Sk.output function.

--- src/misceval.js
+++ src/misceval.js
@@ -278,16 +278,6 @@ Sk.misceval.print_ = function(x)   // this was function print(x)   not sure why.
         if (x !== "\n") Sk.output(' ');
         Sk.misceval.softspace_ = false;
     }
-    // bnm -- Make this version of print act like Python3.x print function.
-    if (x.charAt(0) == '(') {
-        x = x.slice(1,-1);
-	x = '['+x+']'
-	try {
-            var xl = eval(x);
-            x = xl.join(' ');
-	} catch(err) {
-	}
-    }
     var s = new Sk.builtin.str(x);
     Sk.output(s.v);
     var isspace = function(c)
