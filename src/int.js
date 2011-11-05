77df3f0f9e7e0da695edde597314a3938edf6ec4
Brad Miller <bonelake@gmail.com>
2011-11-05 11:53:39 -0500

    Make int constructor smarter about checking for invalid numbers.  Also make it support the binary literal. The int constructor now throws an error if you specify '0x' but the base is not 0 or 16.

--- src/int.js
+++ src/int.js
@@ -4,7 +4,19 @@ Sk.builtin.int_ = function(x, base)
     {
         // todo; this should handle longs too
         if (base === undefined) base = 10; // default radix is 10, not dwim
-        if (!isNaN(x.v) && base <= 10)
+		if ( x.v.substring(0,2).toLowerCase() == '0x' && base != 16 && base != 0) {
+			throw new Sk.builtin.ValueError("int: Argument: " + x.v + " is not a valid literal");
+		}
+		if ( x.v.substring(0,2).toLowerCase() == '0b' ) { 
+			if (base != 2 && base != 0) {
+				throw new Sk.builtin.ValueError("int: Argument: " + x.v + " is not a valid literal");
+			} else {
+				x.v = x.v.substring(2);
+				base = 2;
+			}
+		}
+
+        if (!isNaN(x.v))
             return parseInt(x.v, base);
         else {
             throw new Sk.builtin.ValueError("int: Argument: " + x.v + " is not number");
