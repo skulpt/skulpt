76957da294656071b817479096a6cf24685a887f
Brad Miller <bonelake@gmail.com>
2012-02-11 14:43:10 -0600

    Clean up analysis chapter for use in class
    Signed-off-by: Brad Miller <bonelake@gmail.com>

--- numberFigs.py
+++ numberFigs.py
@@ -4,13 +4,29 @@ import sys
 import re
 
 chap = sys.stdin.read()
-anchors = re.compile(r'\.\. _(lst_.*):')
-matches = anchors.findall(chap)
+lst_anchors = re.compile(r'\.\. _(lst_.*):')
+lst_matches = lst_anchors.findall(chap)
+fig_anchors = re.compile(r'\.\. _(fig_.*):')
+fig_matches = fig_anchors.findall(chap)
+tbl_anchors = re.compile(r'\.\. _(tbl_.*):')
+tbl_matches = tbl_anchors.findall(chap)
 
 figNum = 1
-for fig in matches:
+for fig in fig_matches:
     ## TODO replace replace with re.sub after a chapter has been numbered once
-    chap = chap.replace('Listing x <%s>'%fig, 'Listing %d <%s>'%(figNum,fig))
+    chap = chap.replace('Figure x <%s>'%fig, 'Figure %d <%s>'%(figNum,fig))
     figNum += 1
-    
+
+lstNum = 1
+for lst in lst_matches:
+    ## TODO replace replace with re.sub after a chapter has been numbered once
+    chap = chap.replace('Listing x <%s>'%lst, 'Listing %d <%s>'%(lstNum,lst))
+    lstNum += 1
+
+tblNum = 1
+for tbl in tbl_matches:
+    ## TODO replace replace with re.sub after a chapter has been numbered once
+    chap = chap.replace('Table x <%s>'%tbl, 'Table %d <%s>'%(tblNum,tbl))
+    tblNum += 1
+
 print(chap)
