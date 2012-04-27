a5af2942bdd8dc71fd7ac08bee1a5512c64fa4dc
Brad Miller <bonelake@gmail.com>
2012-04-27 17:08:53 -0500

    Use new look and feel
    Signed-off-by: Brad Miller <bonelake@gmail.com>

--- source/conf.py
+++ source/conf.py
@@ -96,7 +96,9 @@ html_theme = 'sphinxdoc'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {'nosidebar': 'true'}
+
+html_style = "style.css"
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
