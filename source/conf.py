8ec41dc3c9b4049541cc7ee5acbaf34a8254c07c
Brad Miller <bonelake@gmail.com>
2012-04-27 22:01:44 -0500

    add sidebar and image.
    Signed-off-by: Brad Miller <bonelake@gmail.com>

--- source/conf.py
+++ source/conf.py
@@ -96,7 +96,7 @@ html_theme = 'sphinxdoc'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'nosidebar': 'true'}
+#html_theme_options = {'nosidebar': 'true'}
 
 html_style = "style.css"
 
@@ -112,7 +112,7 @@ html_style = "style.css"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "_static/logo.jpg"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
