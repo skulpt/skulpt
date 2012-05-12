e04b84f5a3fdddaf8a484d3f5220459da6492f98
Brad Miller <bonelake@gmail.com>
2012-05-12 12:55:38 -0500

    remove more cruft from old websupport

--- models/0.py
+++ models/0.py
@@ -3,8 +3,8 @@ settings = Storage()
 from os import uname
 
 settings.migrate = True
-settings.title = 'Data Structures'
-settings.subtitle = 'powered by web2py'
+settings.title = 'Runestone Interactive'
+settings.subtitle = 'eBooks for Python'
 settings.author = 'Brad Miller'
 settings.author_email = 'you@example.com'
 settings.keywords = ''
@@ -18,19 +18,12 @@ settings.login_method = 'local'
 settings.login_config = ''
 settings.course_id = 'devcourse'
 settings.plugins = []
+
 if 'local' in uname()[1]:
 	settings.database_uri = 'postgres://bmiller:grouplens@localhost/eds'
-	settings.sphinx_datadir = '/Users/bmiller/src/eds/applications/eds/data'
-	settings.sphinx_staticdir = '/Users/bmiller/src/eds/applications/eds/static'
-	settings.sphinx_docroot = '/eds/view'
-	settings.sphinx_sourcedir = '/Users/bmiller/src/eds/applications/eds/source'
-	settings.sphinx_builddir = '/Users/bmiller/src/eds/applications/eds/'
 elif 'webfaction' in uname()[1]:
 	settings.database_uri = 'postgres://bnmnetp_eds:grouplens@web318.webfaction.com/bnmnetp_eds'
-	settings.sphinx_datadir = '/home/bnmnetp/webapps/web2py/web2py/applications/eds/data'
-	settings.sphinx_staticdir = '/home/bnmnetp/webapps/web2py/web2py/applications/eds/static'
-	settings.sphinx_docroot = '/eds/view'
-	settings.sphinx_sourcedir = '/home/bnmnetp/webapps/web2py/web2py/applications/eds/source'
-	settings.sphinx_builddir = '/home/bnmnetp/webapps/web2py/web2py/applications/eds/'
+elif 'luther' in uname()[1]:
+	settings.database_uri = 'sqlite://storage.sqlite'
 else:
 	raise RuntimeError('Host unknown, senttings not configured')	
