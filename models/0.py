eb444a71c5fee64d014a2e83f21e7e9de2eadb07
Brad Miller <bonelake@gmail.com>
2012-05-28 14:37:08 -0500

    check for Darwin if local not in uname. -- This is kind of clunky but helps identify our development boxes.
    Signed-off-by: Brad Miller <bonelake@gmail.com>

--- models/0.py
+++ models/0.py
@@ -19,7 +19,7 @@ settings.login_config = ''
 settings.course_id = 'devcourse'
 settings.plugins = []
 
-if 'local' in uname()[1]:
+if 'local' in uname()[1] or 'Darwin' in uname()[0]:
 	settings.database_uri = 'sqlite://storage.sqlite'
 elif 'webfaction' in uname()[1]:
 	settings.database_uri = 'postgres://bnmnetp_courselib:f635ac32@web318.webfaction.com/bnmnetp_courselib'
