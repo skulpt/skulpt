f16e66f7da5d42638ea1556d20ba3bf9e45b294a
Brad Miller <bonelake@gmail.com>
2012-06-02 13:17:08 -0500

    cleanups on test cases
    numeric differences between real python and skulpt can be a pain.
    
    Signed-off-by: Brad Miller <bonelake@gmail.com>

--- test/run/t322.py
+++ test/run/t322.py
@@ -4,5 +4,7 @@ print(int('0b0110',0))
 print(int(3.2))
 
 
-def test(y='K', x='Z'): print(x+y)
-test(x='P')
+def foo(y='K', x='Z'): 
+    print(x+y)
+    
+foo(x='P')
--- test/run/t326.py
+++ test/run/t326.py
@@ -1,4 +1,4 @@
 import math
 
-print math.radians(180)
-print math.degrees(math.radians(180))
+print "%10.5f" % math.radians(180)
+print "%10.5f" % math.degrees(math.radians(180))
