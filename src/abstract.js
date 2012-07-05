d40709449b3c78a348b4de76b8f08bf7bab3916a
Scott Rixner <rixners@gmail.com>
2012-07-05 07:15:05 -0500

    Make % operator match Python for all cases a/b positive/negative.

--- src/abstract.js
+++ src/abstract.js
@@ -154,7 +154,7 @@ Sk.abstr.boNumPromote_ = {
     "Add": function(a, b) { return a + b; },
     "Sub": function(a, b) { return a - b; },
     "Mult": function(a, b) { return a * b; },
-    "Mod": function(a, b) { return (b < 0 ? -1 : 1) * (Math.abs(a) % b); },
+    "Mod": function(a, b) { var m = a % b; return ((m * b) < 0 ? (m + b) : m); },
     "Div": function(a, b) {
         if (b === 0)
             throw new Sk.builtin.ZeroDivisionError("integer division or modulo by zero");
