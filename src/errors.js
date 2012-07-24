937073fa74e66163ad656d46f360df47b261a5a3
Brad Miller <bonelake@gmail.com>
2012-07-24 10:20:27 -0500

    modify multi-line error message to have the type of error at the beginning to be consistent with all other error messages.

--- src/errors.js
+++ src/errors.js
@@ -19,16 +19,18 @@ Sk.builtin.Exception.prototype.tp$str = function()
 {
     var ret = "";
     //print(JSON.stringify(this.args));
+
+    ret += this.tp$name;
+    if (this.args)
+        ret += ": " + this.args.v[0].v;
+
     if (this.args.v.length > 4)		//	RNL from length > 1
     {
-        var ret = "File \"" + this.args.v[1].v + "\", " + "line " + this.args.v[2] + "\n" +
+        ret += "\nFile \"" + this.args.v[1].v + "\", " + "line " + this.args.v[2] + "\n" +
             this.args.v[4].v + "\n";
         for (var i = 0; i < this.args.v[3]; ++i) ret += " ";
         ret += "^\n";
     }
-    ret += this.tp$name;
-    if (this.args)
-        ret += ": " + this.args.v[0].v;
     return new Sk.builtin.str(ret);
 };
 
