15394e947826b74bf7ab0f0cb0eff598c4901ccf
Scott Rixner <rixner@rice.edu>
2013-02-27 16:38:34 -0600

    Give all functions names.

--- src/builtin.js
+++ src/builtin.js
@@ -5,7 +5,7 @@ Sk.builtin = {};
 // todo; these should all be func objects too, otherwise str() of them won't
 // work, etc.
 
-Sk.builtin.range = function(start, stop, step)
+Sk.builtin.range = function range(start, stop, step)
 {
     var ret = [];
     var i;
@@ -44,7 +44,7 @@ Sk.builtin.range = function(start, stop, step)
     return new Sk.builtin.list(ret);
 };
 
-Sk.builtin.round = function(number, ndigits)
+Sk.builtin.round = function round(number, ndigits)
 {
     var result, multiplier;
 
@@ -64,7 +64,7 @@ Sk.builtin.round = function(number, ndigits)
     return result;
 };
 
-Sk.builtin.len = function(item)
+Sk.builtin.len = function len(item)
 {
     Sk.builtin.pyCheckArgs("len", arguments, 1, 1);
 
@@ -296,7 +296,7 @@ Sk.builtin.open = function open(filename, mode, bufsize)
     return new Sk.builtin.file(filename, mode, bufsize);
 };
 
-Sk.builtin.isinstance = function(obj, type)
+Sk.builtin.isinstance = function isinstance(obj, type)
 {
     Sk.builtin.pyCheckArgs("isinstance", arguments, 2, 2);
 
@@ -385,7 +385,7 @@ Sk.builtin.hash = function hash(value)
     // todo; throw properly for unhashable types
 };
 
-Sk.builtin.getattr = function(obj, name, default_)
+Sk.builtin.getattr = function getattr(obj, name, default_)
 {
     Sk.builtin.pyCheckArgs("getattr", arguments, 2, 3);
     Sk.builtin.pyCheckType("name", "string", Sk.builtin.checkString(name));
@@ -401,7 +401,7 @@ Sk.builtin.getattr = function(obj, name, default_)
     return ret;
 };
 
-Sk.builtin.input = function(obj, name, default_)
+Sk.builtin.input = function input(obj, name, default_)
 {
     var x = prompt(obj.v);
     return new Sk.builtin.str(x);
