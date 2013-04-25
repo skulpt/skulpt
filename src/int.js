89ba422cb42a7e0e6b0bfd46fc94c911816cc0df
Scott Rixner <rixner@rice.edu>
2013-04-24 23:08:08 -0500

    Better handling of octals for updated JS engine.

--- src/int.js
+++ src/int.js
@@ -4,24 +4,33 @@ Sk.builtin.int_ = function(x, base)
     {
         // todo; this should handle longs too
         if (base === undefined) base = 10; // default radix is 10, not dwim
-		if ( x.v.substring(0,2).toLowerCase() == '0x' && base != 16 && base != 0) {
-			throw new Sk.builtin.ValueError("int: Argument: " + x.v + " is not a valid literal");
-		}
-		if ( x.v.substring(0,2).toLowerCase() == '0b' ) { 
-			if (base != 2 && base != 0) {
-				throw new Sk.builtin.ValueError("int: Argument: " + x.v + " is not a valid literal");
-			} else {
-				x.v = x.v.substring(2);
-				base = 2;
-			}
-		}
+	if ( x.v.substring(0,2).toLowerCase() == '0x' ) {
+	    if (base != 16 && base != 0) {
+		throw new Sk.builtin.ValueError("int: Argument: " + x.v + " is not a valid literal");
+	    } else {
+		base = 16;
+	    }
+	}
+	else if ( x.v.substring(0,2).toLowerCase() == '0b' ) { 
+	    if (base != 2 && base != 0) {
+		throw new Sk.builtin.ValueError("int: Argument: " + x.v + " is not a valid literal");
+	    } else {
+		x.v = x.v.substring(2);
+		base = 2;
+	    }
+	}
+	else if ( x.v.charAt(0) == '0' ) {
+	    if (x.v == '0') return 0;
+	    if (base == 8 || base == 0) {
+		base = 8;
+	    }
+	}
 
         if (!isNaN(x.v) && x.v.indexOf(".") < 0)
             return parseInt(x.v, base);
         else {
             throw new Sk.builtin.ValueError("int: Argument: " + x.v + " is not a valid literal");
         }
-
     }
     // sneaky way to do truncate, floor doesn't work < 0, round doesn't work on the .5> side
     // bitwise ops convert to 32bit int in the "C-truncate-way" we want.
