5b153a72347fff8309b055fb99bd2be348c0ca4d
Scott Rixner <rixner@rice.edu>
2013-04-25 21:55:52 -0500

    Clean up error messages in int().
    Still doesn't properly convert strings for all bases.y

--- src/int.js
+++ src/int.js
@@ -2,36 +2,56 @@ Sk.builtin.int_ = function(x, base)
 {
     if (x instanceof Sk.builtin.str)
     {
+	var s = x.v;
+
         // todo; this should handle longs too
         if (base === undefined) base = 10; // default radix is 10, not dwim
-	if ( x.v.substring(0,2).toLowerCase() == '0x' ) {
+
+	if (base < 2 || base > 36) {
+	    if (base != 0) {
+		throw new Sk.builtin.ValueError("int() base must be >= 2 and <= 36");
+	    }
+	}
+
+	if ( s.substring(0,2).toLowerCase() == '0x' ) {
 	    if (base != 16 && base != 0) {
-		throw new Sk.builtin.ValueError("int: Argument: " + x.v + " is not a valid literal");
+		throw new Sk.builtin.ValueError("invalid literal for int() with base " + base + ": '" + x.v + "'");
 	    } else {
 		base = 16;
 	    }
 	}
-	else if ( x.v.substring(0,2).toLowerCase() == '0b' ) { 
+	else if ( s.substring(0,2).toLowerCase() == '0b' ) { 
 	    if (base != 2 && base != 0) {
-		throw new Sk.builtin.ValueError("int: Argument: " + x.v + " is not a valid literal");
+		throw new Sk.builtin.ValueError("invalid literal for int() with base " + base + ": '" + x.v + "'");
 	    } else {
-		x.v = x.v.substring(2);
+		s = s.substring(2);
 		base = 2;
 	    }
 	}
-	else if ( x.v.charAt(0) == '0' ) {
-	    if (x.v == '0') return 0;
+	else if ( s.charAt(0) == '0' ) {
+	    if (s == '0') return 0;
 	    if (base == 8 || base == 0) {
 		base = 8;
 	    }
 	}
 
-        if (!isNaN(x.v) && x.v.indexOf(".") < 0)
-            return parseInt(x.v, base);
+	// !isNaN is not the right check for all bases
+        if (!isNaN(s) && s.indexOf(".") < 0) {
+	    var val = parseInt(s, base);
+	    if (isNaN(val)) {
+		throw new Sk.builtin.ValueError("invalid literal for int() with base " + base + ": '" + x.v + "'");
+	    }
+            return val;
+	}
         else {
-            throw new Sk.builtin.ValueError("int: Argument: " + x.v + " is not a valid literal");
+	    throw new Sk.builtin.ValueError("invalid literal for int() with base " + base + ": '" + x.v + "'");
         }
     }
+
+    if (base !== undefined) {
+	throw new Sk.builtin.TypeError("int() can't convert non-string with explicit base");
+    }
+
     // sneaky way to do truncate, floor doesn't work < 0, round doesn't work on the .5> side
     // bitwise ops convert to 32bit int in the "C-truncate-way" we want.
     return x | 0;
