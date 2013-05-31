8764d0afc55b697be06c76fe3218140e6dba039f
Peter Harrington <peter.b.harrington@gmail.com>
2013-05-30 22:09:47 -0700

    completed rstrip()

---
+++ test/run/t347.py
@@ -0,0 +1,5 @@
+print "we are stripping".rstrip("g")
+print "we are not stripping".rstrip("n")
+print "we are stripping".rstrip("ing")
+print "we are not stripping".rstrip("we")
+print "stripping whitespace   ".rstrip()
