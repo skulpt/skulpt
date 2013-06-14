17358153e9ca1515b88440ddbbc77f09f6e7e255
Brad Miller <bonelake@gmail.com>
2013-06-14 15:01:35 -0500

    improve got x expected Y messages and added a space before the feedback message

--- src/lib/unittestgui/__init__.py
+++ src/lib/unittestgui/__init__.py
@@ -50,27 +50,27 @@ class unittest:
 
     def assertEqual(self, actual, expected, feedback=""):
         res = actual==expected
-        self.appendResult(res,actual,expected, feedback)
+        self.appendResult(res,str(actual)+' to be equal to ',expected, feedback)
 
     def assertNotEqual(actual, expected, feedback=""):
         res = actual != expected
-        self.appendResult(res,actual,expected,feedback)
+        self.appendResult(res,str(actual)+' to not equal ',expected,feedback)
 
     def assertTrue(self,x, feedback=""):
         res = x
-        self.appendResult(res,x,True,feedback)
+        self.appendResult(res,str(x)+' to be ',True,feedback)
 
     def assertFalse(self,x, feedback=""):
         res = not x
-        self.appendResult(res,x,False,feedback)
+        self.appendResult(res,str(x)+' to be ',False,feedback)
 
     def assertIs(self,a,b, feedback=""):
         res = a is b
-        self.appendResult(res,a,b,feedback)
+        self.appendResult(res,str(a)+' to be the same object as ',b,feedback)
 
     def assertIsNot(self,a,b, feedback=""):
         res = a is not b
-        self.appendResult(res,a,b,feedback)
+        self.appendResult(res,str(a)+' to not be the same object as ',b,feedback)
 
     def assertIsNone(self,x, feedback=""):
         res = x is None
@@ -78,23 +78,23 @@ class unittest:
 
     def assertIsNotNone(self,x, feedback=""):
         res = x is not None
-        self.appendResult(res,x,None,feedback)
+        self.appendResult(res,str(x)+' to not be ',None,feedback)
 
     def assertIn(self,a,b, feedback=""):
         res = a in b
-        self.appendResult(res,a,b,feedback)
+        self.appendResult(res,str(a)+' to be in ',b,feedback)
 
     def assertNotIn(self,a,b, feedback=""):
         res = a not in b
-        self.appendResult(res,a,b,feedback)
+        self.appendResult(res,str(a)+' to not be in ',b,feedback)
 
     def assertIsInstance(self,a,b, feedback=""):
         res = isinstance(a,b)
-        self.appendResult(res,a,b,feedback)
+        self.appendResult(res,str(a)+' to be an instance of ',b,feedback)
 
     def assertNotIsInstance(self,a,b, feedback=""):
         res = not isinstance(a,b)
-        self.appendResult(res,a,b,feedback)
+        self.appendResult(res,str(a)+' to not be an instance of ',b,feedback)
 
     def assertAlmostEqual(self,a,b, feedback=""):
         res = round(a-b,7) == 0
@@ -102,23 +102,23 @@ class unittest:
 
     def assertNotAlmostEqual(self,a,b, feedback=""):
         res = round(a-b,7) != 0
-        self.appendResult(res,a,b,feedback)
+        self.appendResult(res,str(a)+' to not equal ',b,feedback)
 
     def assertGreater(self,a,b, feedback=""):
         res = a > b
-        self.appendResult(res,a,b,feedback)
+        self.appendResult(res,str(a)+' to be greater than ',b,feedback)
 
     def assertGreaterEqual(self,a,b, feedback=""):
         res = a >= b
-        self.appendResult(res,a,b,feedback)
+        self.appendResult(res,str(a)+' to be greater than or equal to ',b,feedback)
 
     def assertLess(self,a,b, feedback=""):
         res = a < b
-        self.appendResult(res,a,b,feedback)
+        self.appendResult(res,str(a)+' to be less than ',b,feedback)
 
     def assertLessEqual(self,a,b, feedback=""):
         res = a <= b
-        self.appendResult(res,a,b,feedback)
+        self.appendResult(res,str(a)+' to be less than or equal to ',b,feedback)
 
     def appendResult(self,res,actual,expected,feedback):
         if res == 'Error':
@@ -127,7 +127,7 @@ class unittest:
             msg = 'Pass'
             self.numPassed += 1
         else:
-            msg = 'Fail: expected %s got %s' % (str(actual),str(expected)) + feedback
+            msg = 'Fail: expected %s  %s ' % (str(actual),str(expected)) + feedback
             self.numFailed += 1
 
         pTag = document.createElement('li')
