0ad72bff2ae92d01c700313777df6265e6a5ba5b
Marie Chatfield <marie.chatfield@gmail.com>
2013-07-03 13:03:41 -0500

    Make checkInt handle nmber and lng objects.

--- src/function.js
+++ src/function.js
@@ -66,7 +66,10 @@ Sk.builtin.checkNumber = function (arg) {
 goog.exportSymbol("Sk.builtin.checkNumber", Sk.builtin.checkNumber);
 
 Sk.builtin.checkInt = function (arg) {
-    return (arg !== null) && (typeof arg === "number") && (arg === (arg|0));
+    return (arg !== null) && ((typeof arg === "number" && arg === (arg|0))
+			      || (arg instanceof Sk.builtin.nmber
+				  && arg.skType === Sk.builtin.nmber.int$)
+			      || arg instanceof Sk.builtin.lng);
 };
 goog.exportSymbol("Sk.builtin.checkInt", Sk.builtin.checkInt);
 
