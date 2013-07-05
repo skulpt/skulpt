b54a3c2e15aee07a4764277a2a29fa38b0f76706
Marie Chatfield <marie.chatfield@gmail.com>
2013-07-05 17:53:29 -0500

    A few more test cases.

---
+++ test/run/t452.py
@@ -0,0 +1,35 @@
+x = 1
+x += 4
+print x == 5
+x -= 2
+print x == 3
+x *= 4
+print x == 12
+x /= 6
+print x == 2
+x %= 5
+print x == 2
+
+x = 1L
+x += 4L
+print x == 5L
+x -= 2L
+print x == 3L
+x *= 4L
+print x == 12L
+x /= 6L
+print x == 2L
+x %= 5L
+print x == 2L
+
+x = 1.5
+x += 4.2
+print x == 5.7
+x -= 3.7
+print x == 2.0
+x *= 4.25
+print x == 8.5
+x /= 4
+print x == 2.125
+x %= 2
+print x == 0.125
---
+++ test/run/t453.py
@@ -0,0 +1,12 @@
+def isPrototypeOf(x):
+    print x
+
+def toSource(x):
+    print x
+
+def hasOwnProperty(x):
+    print x
+
+isPrototypeOf(1)
+toSource(2)
+hasOwnProperty(3)
