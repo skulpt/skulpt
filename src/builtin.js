9d971c4a67a2a9aafaa771fc3f6c8ab66fc3f4a0
Albert-Jan Nijburg <albertjan@curit.com>
2013-07-24 11:29:50 +0200

    Get typename correctly

--- src/builtin.js
+++ src/builtin.js
@@ -736,7 +736,7 @@ Sk.builtin.map = function map(fun, seq) {
     }
 
     if (seq.tp$iter === undefined){
-        throw new Sk.builtin.TypeError("'" + iterable.skType + "' object is not iterable");
+        throw new Sk.builtin.TypeError("'" + Sk.abstr.typeName(iterable) + "' object is not iterable");
     }
 
     var retval = [],
@@ -775,7 +775,7 @@ Sk.builtin.filter = function filter(fun, iterable) {
 	
 	//todo: need to find a proper way to tell what type it is.
 	if (iterable.tp$iter === undefined){
-		throw new Sk.builtin.TypeError("'" + iterable.skType + "' object is not iterable");
+		throw new Sk.builtin.TypeError("'" + Sk.abstr.typeName(iterable) + "' object is not iterable");
 	}
 	
 	//simulate default identity function
