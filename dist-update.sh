982c42216eacb37643816ad97764f645d68d0aad
albertjan <albertjan@curit.com>
2013-08-07 19:39:10 +0200

    after succesfull build push new version of dist.

--- dist-update.sh
+++ dist-update.sh
@@ -4,12 +4,13 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.name "Travis"
   #using token clone gh-pages branch
   cd $HOME
-  git clone --quiet https://${GH_TOKEN}@github.com/albertjan/skulpt.git v8 > /dev/null
-  #go into diractory and copy data we're interested in to that directory
-  cd v8/support/d8
-  cp $HOME/build/albertjan/d8 ./d8x64
+  git clone --quiet https://${GH_TOKEN}@github.com/bnmnetp/skulpt.git dist > /dev/null
+  cd dist
+  ./m dist
+  cd dist
   #add, commit and push files
-  git add d8x64
+  git add skulpt.js
+  git add builtin.js
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [ci skip]"
   git push -fq origin master > /dev/null
   echo -e "Done magic with coverage\n"
