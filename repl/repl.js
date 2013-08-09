e6b7725dae3de6fcb84aa43d487da8cd9d03a5e1
Albert-Jan Nijburg <albertjan@curit.com>
2013-08-09 15:19:42 +0200

    behaves even better.
    Removes print statements from blocks of code that don't define anything.
    Better error locating prints code sofar and marks the line with the
    error.

--- repl/repl.js
+++ repl/repl.js
@@ -4,20 +4,28 @@ Sk.configure({
 });
 
 var compilableLines = [];
+var re = new RegExp("\s*print");
 
 print ("Python 2.6(ish) (skulpt, " + new Date() + ")");
 print ("[v8: " + version() + "] on a system");
 print ('Don\'t type "help", "copyright", "credits" or "license" unless you\'ve assigned something to them');
 
 while (true){
-    write('>>> ');
     var lines = [];
-    lines.push(readline());
-    if (lines[0][lines[0].length - 1] === ':'){
+    write('>>> ');
 
+    var removePrints = false;
+
+    lines.push(readline());
+
+    if (lines[0][lines[0].length - 1] === ':'){
         var additionallines = [];
         var curline = "";
         do{
+            if (curline == "" && lines[0].indexOf("def") == "-1" && lines[0].indexOf("class") == "-1"){
+                //remove print statements in blocks when they don't define something.
+                removePrints = true;
+            }
             write('... ');
             curline = readline()
             lines.push(curline);
@@ -29,17 +37,30 @@ while (true){
     if (lines.length == 1){
         if (lines[0].indexOf('=') == -1 && lines[0].indexOf(':') == -1) {
             linesToCompile[linesToCompile.length - 1] = "print " + lines[0];
-        }
-        if (lines[0].indexOf("quit()") != -1){
+            lines.pop();
+        } else if (lines[0].indexOf("quit()") != -1){
             quit(0);
         }
     }
 
     try{
         Sk.importMainWithBody("repl", false, linesToCompile.join('\n'));
-        compilableLines = compilableLines.concat(lines);
+        compilableLines = compilableLines.concat(lines.map(function(str){
+            if(re.test(str) && removePrints) {
+                return str.replace(/print.*/g, "pass");
+            } else {
+                return str;
+            }
+        }));
     } catch (err) {
         print(err);
-        print (linesToCompile);
+        var index = -1;
+        if ((index = err.toString().indexOf("on line")) != -1){
+            index = parseInt(err.toString().substr(index + 8), 10);
+        }
+        var line = 0;
+        print (linesToCompile.map(function(str){
+            return ++line + (index == line ? ">" : " ") + ": " + str;
+        }).join('\n'));
     }
 }
