0cf30b1e34ffe695e641a4b92bbc5293878d5e70
Albert-Jan Nijburg <albertjan@curit.com>
2013-08-19 16:24:41 +0200

    expanded dist-update to synchronize tags between the repositories

--- dist-update.sh
+++ dist-update.sh
@@ -3,14 +3,57 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_TEST_RESULT" == "0" ]]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
   cd $HOME
+  
   #clone skulpt
-  git clone --quiet https://${GH_TOKEN}@github.com/skulpt/skulpt.git skulpt > /dev/null
+  git clone --quiet https://${GH_TOKEN_TEST}@github.com/skulpt/skulpt.git skulpt > /dev/null
   #clone dist
-  git clone --quiet https://${GH_TOKEN}@github.com/skulpt/skulpt-dist.git dist > /dev/null
+  git clone --quiet https://${GH_TOKEN_TEST}@github.com/skulpt/skulpt-dist.git dist > /dev/null
+  
+  #compare tags
+  cd $HOME  
+  cd skulpt  
+  git tag > ../tags-skulpt  
+  cd ../dist  
+  git tag > ../tags-dist  
+  cd ..  
+  grep -Fxvf tags-dist tags-skulpt > new-tags
+  
+  for TAG in $(cut -d, -f2 < new-tags)
+  do
+    #we have a new tag
+    export NEWTAG=true
+    #build skulpt at this tag
+    cd $HOME/skulpt
+    git checkout tags/$TAG
+    ./skulpt.py dist
+    #create zip and tarbals
+    cd dist
+    tar -czf skulpt-latest.tar.gz *.js 
+    zip skulpt-latest.zip *zip
+    mkdir -p ../doc/static/dist
+    mv *.zip ../doc/static/dist/
+    mv *.tar.gz ../doc/static/dist/
+    cp *.js ../../dist/
+    #put the new version in the dist repository
+    cd ../../dist
+    git add .
+    git commit -m "Skulpt version: $TAG"
+    git tag -a $TAG
+    git push -fq --tags origin master > /dev/null
+  done
+
+  #reset dist repository to HEAD just to be sure
+  cd $HOME
+  cd dist
+  git reset HEAD --hard
+  cd $HOME
+
   #build skulpt
   cd skulpt
+  git reset HEAD --hard
   ./skulpt.py dist
   cd dist
+  
   #add, commit and push files to the dist repository
   cp *.js ../../dist/
   cd ../../dist
--- site-update.sh
+++ site-update.sh
@@ -1,10 +1,6 @@
-if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_TEST_RESULT" == "0" && "false" == "true" ]]; then
+if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_TEST_RESULT" == "0" && "$NEWTAG" == "true" ]]; then
   echo "Updating site"
-  cd $HOME
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis"
-  git clone --quiet https://${GH_TOKEN_SKULPT}@github.com/bnmnetp/skulpt.git site > /dev/null
-  cd site/doc
-  $HOME/google-appengine/appcfg.py --oauth2_refresh_token=${GAE_REFRESH} update /
+  cd $HOME/skulpt/doc
+  $HOME/google-appengine/appcfg.py --oauth2_refresh_token=${GAE_REFRESH_TEST} update ./
   echo "Successfully updated skulpt.org
 fi
