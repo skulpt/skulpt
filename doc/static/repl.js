1b99eb2bb742e0cb4f2e575f5a41e921ccce30a9
albertjan <albertjan@curit.com>
2013-10-03 19:35:01 +0200

    Comments and improvements to webrepl

--- doc/static/repl.js
+++ doc/static/repl.js
@@ -1,25 +1,27 @@
 $(function () {
     var repl = new CodeMirrorREPL('interactive', {
-        mode: "python",
-        theme: "solarized dark",
-    });
-
-    var compilableLines = [];
-    //finds lines starting with "print"
-
-    var re = new RegExp("\s*print");
-    var importre = new RegExp("\s*import");
-    var mls = new RegExp("'''");
-    var defre = new RegExp("def.*|class.*");
+            mode: "python",
+            theme: "solarized dark"
+        }),
+        compilableLines = [],
+        //finds lines starting with "print" 
+        re = new RegExp("\\s*print"),
+        //finds import statements
+        importre = new RegExp("\\s*import"),
+        //finds multuline string constants
+        mls = new RegExp("'''"),
+        //finds defining statements
+        defre = new RegExp("def.*|class.*");
 
     repl.print("Python 2.6(ish) (skulpt, " + new Date() + ")");
     repl.print("[" + navigator.userAgent + "] on " + navigator.platform);
     repl.print('Don\'t type "help", "copyright", "credits" or "license" unless you\'ve assigned something to them');
 
     repl.isBalanced = function (code) {
-        var lines = code.split('\n');
-        var depth = 0;
-        var mlsopened = false
+        var lines = code.split('\n'),
+            depth = 0,
+            mlsopened = false;
+        
         for (var l in lines){
             if (lines[l].match(/'''/) !== null && lines[l].match(/'''/).length == 1) {
                 mlsopened = !mlsopened;
@@ -36,9 +38,9 @@ $(function () {
 
     //Loop
     repl.eval = function (code) {
-
         Sk.configure({ 
             output: function(str) {
+                //strip out line-feeds
                 if (str.replace(/\n/g, "") != ""){
                     repl.print(str);
                 }
@@ -49,23 +51,33 @@ $(function () {
                 return Sk.builtinFiles["files"][x];
             }
         });
+        //split lines on linefeed
+        var lines = code.split('\n'),
+            //concatenate them to the lines collected up till now
+            linesToCompile = compilableLines.concat(lines);
 
-        var lines = code.split('\n');
-
-        var linesToCompile = compilableLines.concat(lines);
-
+        //it's a onliner
         if (lines.length == 1) {
+            //if it's a statement that should be printed (not containing an = or def or class or an empty line)
             if (lines[0].indexOf('=') == -1 && !defre.test(lines[0]) && !importre.test(lines[0]) && lines[0].length > 0) {
-                //Print
+                //if it doesn't contain print make sure it doesn't print None
                 if (!re.test(lines[0])) {
+                    //remove the statement
                     linesToCompile.pop();
+                    //evaluate it if nessecary
                     linesToCompile.push("evaluationresult = " + lines[0]);
+                    //print the result if not None
                     linesToCompile.push("if not evaluationresult == None: print evaluationresult");
                 }
-                lines.pop();
             }
         }        
-
+        
+        //filter out empty lines
+        linesToCompile.filter(function(l){ return (!str || /^\s*$/.test(str)); });
+        
+        //don't compile if there isn't anything to compile.
+        if (linesToCompile.count === 0) { return; }
+        
         try {
             //Evaluate
             Sk.importMainWithBody("repl", false, linesToCompile.join('\n'));
