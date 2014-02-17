154e7996d2045e1ec11ed21b33c69d7806ae47b0
Christian Kerl <christian.kerl@in.tum.de>
2014-02-17 14:31:59 +0100

    removed unnecessary libs

--- src/lib/pythonds/graphs/__init__.py
+++
@@ -1,5 +0,0 @@
-
-
-from .adjGraph import Graph
-from .adjGraph import Vertex
-from .priorityQueue import PriorityQueue
--- src/lib/urllib/__init__.js
+++
@@ -1,6 +0,0 @@
-var $builtinmodule = function(name)
-{
-  var urllib = {};
-
-  return urllib;
-};
