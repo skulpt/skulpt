48a427c1e88b179fa7576325958ecddb38785261
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2014-03-02 21:27:00 -0600

    implement capwords

--- src/lib/string/__init__.js
+++ src/lib/string/__init__.js
@@ -19,5 +19,44 @@ var $builtinmodule = function(name) {
     mod.punctuation = Sk.builtin.str('!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~', Sk.builtin.str);
     mod.whitespace = Sk.builtin.str('\t\n\x0b\x0c\r ', Sk.builtin.str);
 
+    mod.split = new Sk.builtin.func(function(s, sep, maxsplit) {
+        return Sk.misceval.callsim(Sk.builtin.str.prototype['split'], s, sep, maxsplit);
+    });
+
+    /*
+     * string.capwords(s)
+     *    Split the argument into words using split(), capitalize
+     *    each word using capitalize(), and join the capitalized
+     *    words using join(). Note that this replaces runs of
+     *    whitespace characters by a single space, and removes
+     *    leading and trailing whitespace.
+    */
+    mod.capwords = new Sk.builtin.func(function(s, sep) {
+        Sk.builtin.pyCheckArgs('capwords', arguments, 1, 2);
+        if (!Sk.builtin.checkString(s)) {
+            throw new Sk.builtin.TypeError("s must be a string");
+        }
+        if (sep === undefined) {
+            sep = Sk.builtin.str(' ');
+        }
+        if(!Sk.builtin.checkString(sep)) {
+            throw new Sk.builtin.TypeError("sep must be a string");
+        }
+
+        var words = Sk.misceval.callsim(mod.split, s);
+
+        var capWords = [];
+        for (var i=0; i<words.v.length; i++) {
+            var word = Sk.builtin.list.prototype['list_subscript_'].call(words, i);
+            var cap = Sk.misceval.callsim(Sk.builtin.str.prototype['capitalize'], word);
+            capWords.push(cap);
+        }
+
+        return Sk.misceval.callsim(Sk.builtin.str.prototype['join'], sep, Sk.builtin.list(capWords));
+
+    });
+
+
+
     return mod;
 };
