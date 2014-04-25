60bbdf837566550e976cc8aa32e26ae904fcb26a
albertjan <albertjan@curit.com>
2014-04-25 21:29:10 +0200

    revert more of the midnight changes

--- src/dict.js
+++ src/dict.js
@@ -81,11 +81,11 @@ Sk.builtin.dict.prototype.key$lookup = function(bucket, key)
         }
     }
     
-    if (Sk.misceval.richCompareBool(bucket.$hash, kf(key), 'Eq'))
-    {
-        //is it possible that buckets have more than one item?
-        return bucket.items[0];
-    }
+//    if (Sk.misceval.richCompareBool(bucket.$hash, kf(key), 'Eq'))
+//    {
+//        //is it possible that buckets have more than one item?
+//        return bucket.items[0];
+//    }
     return null;
 }   
 
