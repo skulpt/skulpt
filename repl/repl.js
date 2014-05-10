f2a57fa45a6635b790ba93f51c1dea5af4e753ce
albertjan <albertjan@curit.com>
2014-05-10 13:59:41 +0200

    adds retain globals fixes command line repl

--- repl/repl.js
+++ repl/repl.js
@@ -1,4 +1,4 @@
-Sk.configure({ output: write, read: read, systemexit: true });
+Sk.configure({ output: write, read: read, systemexit: true, retainglobals: true });
 
 var compilableLines = [],
     //finds lines starting with "print"
@@ -13,111 +13,79 @@ var compilableLines = [],
     emptyline = new RegExp("^\\s*$"),
     lines = [];
 
-print ("Python 2.6(ish) (skulpt, " + new Date() + ")");
-print ("[v8: " + version() + "] on a system");
-print ('Don\'t type "help", "copyright", "credits" or "license" unless you\'ve assigned something to them');
+print("Python 2.6(ish) (skulpt, " + new Date() + ")");
+print("[v8: " + version() + "] on a system");
+print('Don\'t type "help", "copyright", "credits" or "license" unless you\'ve assigned something to them');
 
-function isBalanced(lines){
-    var depth = 0,
-        mlsopened = false;
+function isBalanced(lines) {
+    'use strict';
+	var depth = 0,
+        mlsopened = false,
+		l;
     
-    for (var l in lines){
-        if (lines[l].match(/'''/) !== null && lines[l].match(/'''/).length == 1) {
-            mlsopened = !mlsopened;
-        }
-        if (!mlsopened && lines[l].substr(lines[l].length -1) == ":") {
-            depth++;
-        }
-        if (!mlsopened && lines[l] == "" && depth > 0){
-            depth--;
-        }
+    for (l = 0; l < lines.length; l = l + 1) {
+		if (lines[l] !== undefined) {
+			if (lines[l].match(/'''/) !== null && lines[l].match(/'''/).length === 1) {
+				mlsopened = !mlsopened;
+			}
+			if (!mlsopened && lines[l].substr(lines[l].length - 1) === ":") {
+				depth = depth + 1;
+			}
+			if (!mlsopened && lines[l] === "" && depth > 0) {
+				depth = depth - 1;
+			}
+		}
     }
-    return depth == 0 && !mlsopened;
+    return depth === 0 && !mlsopened;
 }
 
 //Loop
-while (true){
-    var removePrints = false;
-   
+while (true) {
     write(isBalanced(lines) ? '>>> ' : '... ');
 
     //Read
     lines.push(readline());
-    
+	
     //See if it is ready to be evaluated;
     if (!isBalanced(lines)) { continue; }
 
-    //strip empty lines
-    lines = lines.filter(function(str) { return !emptyline.test(str); });
-    
-    //concat that to the lines we allready have.
-    var linesToCompile = compilableLines.concat(lines);
-
     //it's a onliner
-    if (lines.length == 1) {
+    if (lines.length === 1) {
         //if it's a statement that should be printed (not containing an = or def or class or an empty line)
-        if (lines[0].indexOf('=') == -1 && !defre.test(lines[0]) && !importre.test(lines[0]) && lines[0].length > 0) {
+        if (lines[0].indexOf('=') === -1 && !defre.test(lines[0]) && !importre.test(lines[0]) && lines[0].length > 0) {
             //if it doesn't contain print make sure it doesn't print None
             if (!re.test(lines[0])) {
-                //remove the statement
-                linesToCompile.pop();
+				//remove the statement
                 //evaluate it if nessecary
-                linesToCompile.push("evaluationresult = " + lines[0]);
+                lines.push("evaluationresult = " + lines.pop());
                 //print the result if not None
-                linesToCompile.push("if not evaluationresult == None: print evaluationresult");
+                lines.push("if not evaluationresult == None: print evaluationresult");
             }
-            //make sure it doesnt' end up in the list with lines to compile the next run
-            lines.pop();
         }
-    }  
-    
-    //don't compile if there isn't anything to compile.
-    if (linesToCompile.length === 0) { continue; }
-    
-    try{
+    }
+
+    try {
         //Evaluate
-        Sk.importMainWithBody("repl", false, linesToCompile.join('\n'));
-        //remove print statements when a block is created that doesn't define anything
-        var removePrints = false;
-        compilableLines = compilableLines.concat(lines.map(function (str) {
-            //non defining block statement
-            if (str.substr(str.length -1) == ":" && !defre.test(str)) {
-                removePrints = true;
-                return str;
-            }
-        
-            //end of non defining block statement
-            if(str == "" && removePrints){
-                removePrints = false;
-                return str;
-            }
-            
-            if (re.test(str) && removePrints) {
-                //strip prints from non defining block statements.
-                return str.replace(/print.*/g, "pass");
-            } else {
-                return str;
-            }
-        }));
-        //reset collected lines
-        lines = [];
+        Sk.importMainWithBody("repl", false, lines.join('\n'));
     } catch (err) {
-        if (err instanceof Sk.builtin.SystemExit)
-        {
+        if (err instanceof Sk.builtin.SystemExit) {
             quit();
         }
+		
         print(err);
 
         var index = -1;
         //find the line number
-        if ((index = err.toString().indexOf("on line")) != -1) {
+        if ((index = err.toString().indexOf("on line")) !== -1) {
             index = parseInt(err.toString().substr(index + 8), 10);
         }
         var line = 0;
         //print the accumulated code with a ">" before the broken line.
         //Don't add the last statement to the accumulated code
-        print(linesToCompile.map(function (str) {
-            return ++line + (index == line ? ">" : " ") + ": " + str;
+        print(lines.map(function (str) {
+            return ++line + (index === line ? ">" : " ") + ": " + str;
         }).join('\n'));
-    }
+    } finally {
+		lines = [];
+	}
 }
