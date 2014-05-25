80122ec9b100efbb962fad2889f24dfb6ec1f5b1
albertjan <albertjan@curit.com>
2014-05-25 17:34:14 +0200

    this shouldn't have been removed.

--- src/int.js
+++ src/int.js
@@ -138,6 +138,9 @@ Sk.builtin.int_ = function (x, base) {
     }
 
     x = Sk.builtin.asnum$(x);
+    if (x > Sk.builtin.lng.threshold$ || x < -Sk.builtin.lng.threshold$ - 1) {
+        return new Sk.builtin.nmber(x, Sk.builtin.nmber.int$); 
+    }
     return new Sk.builtin.nmber(x | 0, Sk.builtin.nmber.int$);
 };
 Sk.builtin.int_.co_varnames = [ "base" ];
