f2a25d3ef9d89c903db23a773aa35efcd8f3f1e6
Brad Miller <bonelake@gmail.com>
2014-07-02 10:40:00 -0500

    reformat sys.js

--- src/builtin/sys.js
+++ src/builtin/sys.js
@@ -1,34 +1,35 @@
-var $builtinmodule = function(name)
-{
+var $builtinmodule = function (name) {
+    var i;
     var sys = {};
 
     var args = [];
     var argv = Sk.getSysArgv();
-    for (var i = 0; i < argv.length; ++i)
+    for (i = 0; i < argv.length; ++i) {
         args.push(new Sk.builtin.str(argv[i]));
-    sys.argv = new Sk.builtins['list'](args);
+    }
+    sys.argv = new Sk.builtins["list"](args);
 
-    sys.copyright = Sk.builtin['str']("Copyright 2009-2010 Scott Graham.\nAll Rights Reserved.\n");
+    sys.copyright = Sk.builtin["str"]("Copyright 2009-2010 Scott Graham.\nAll Rights Reserved.\n");
 
     sys.modules = Sk.sysmodules;
 
     sys.path = Sk.realsyspath;
 
-    sys.getExecutionLimit = new Sk.builtin.func(function() {
+    sys.getExecutionLimit = new Sk.builtin.func(function () {
         return Sk.builtin.assk$(Sk.execLimit, Sk.builtin.nmber.int$);
     });
 
-    sys.setExecutionLimit = new Sk.builtin.func(function(t) {
-        if (t !==  undefined) {
+    sys.setExecutionLimit = new Sk.builtin.func(function (t) {
+        if (t !== undefined) {
             Sk.execLimit = Sk.builtin.asnum$(t);
         }
     });
 
-    sys.resetTimeout = new Sk.builtin.func(function() {
+    sys.resetTimeout = new Sk.builtin.func(function () {
         Sk.execStart = new Date();
     });
 
-    sys.debug = new Sk.builtin.func(function() {
+    sys.debug = new Sk.builtin.func(function () {
         debugger;
     });
 
