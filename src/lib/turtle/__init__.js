da66655d9597f8b552dd4d7d59c2ff1b2c58d5a9
Benjamin Wheeler <bzwheeler@gmail.com>
2014-12-22 16:03:03 -0500

    Properly account for negative radius on turtle ending angle

--- src/lib/turtle/__init__.js
+++ src/lib/turtle/__init__.js
@@ -765,12 +765,15 @@ return (function() {
             w  = extent / steps;
             w2 = 0.5 * w;
             l  = radius * Math.sin(w/self._fullCircle*Turtle.RADIANS);
-            endAngle = angle + extent;
+
             if (radius < 0) {
                 l = -l;
                 w = -w;
                 w2 = -w2;
-                endAngle = -endAngle;
+                endAngle = angle - extent;
+            }
+            else {
+                endAngle = angle + extent;
             }
 
             promise = getFrameManager().willRenderNext() ? Promise.resolve() : new InstantPromise();
