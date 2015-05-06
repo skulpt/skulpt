31ec4ad37bd37f706cfda5c83ea3acb2ca8f236f
Benjamin Wheeler <bzwheeler@gmail.com>
2015-05-06 11:19:35 -0400

    Adds 'Date' as a reserved name
    Added Date to the _reservedNames object
    Updated Compiler.prototype.cclass to use 'fixReservedNames' method to protect class names

--- src/compile.js
+++ src/compile.js
@@ -211,7 +211,8 @@ var reservedNames_ = {
     "unwatch": true,
     "valueOf": true,
     "watch": true,
-    "length": true
+    "length": true,
+    "Date": true
 };
 
 function fixReservedNames (name) {
@@ -1809,7 +1810,7 @@ Compiler.prototype.cclass = function (s) {
     entryBlock = this.newBlock("class entry");
 
     this.u.prefixCode = "var " + scopename + "=(function $" + s.name.v + "$class_outer($globals,$locals,$rest){var $gbl=$globals,$loc=$locals;";
-    this.u.switchCode += "return(function " + s.name.v + "(){";
+    this.u.switchCode += "return(function " + fixReservedNames(s.name.v) + "(){";
     this.u.switchCode += "var $blk=" + entryBlock + ",$exc=[],$ret=undefined,currLineNo=undefined,currColNo=undefined;"
     if (Sk.execLimit !== null) {
         this.u.switchCode += "if (typeof Sk.execStart === 'undefined') {Sk.execStart = new Date()}";
