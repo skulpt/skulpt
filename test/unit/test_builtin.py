51849e5abfb83f6d44986ec5210c90db31ee50ed
Brad Miller <bonelake@mac.com>
2015-05-24 10:57:38 +0200

    change assertRaises tests to use the same call syntax as the CPython tests.

--- test/unit/test_builtin.py
+++ test/unit/test_builtin.py
@@ -9,28 +9,20 @@ class BuiltinTest(unittest.TestCase):
     def test_getattr(self):
         import sys
         self.assertTrue(getattr(sys, 'maxint') is sys.maxint)
-        def foo():
-            getattr(sys,1)
-        self.assertRaises(TypeError, foo)
+        self.assertRaises(TypeError, getattr, sys, 1)
         self.assertRaises(TypeError, getattr)
 
     def test_hasattr(self):
         import sys
         self.assertTrue(hasattr(sys, 'maxint'))
-        def foo():
-            hasattr(sys,1)
-        self.assertRaises(TypeError, foo)
+        self.assertRaises(TypeError, hasattr, sys, 1)
         self.assertRaises(TypeError, hasattr)
 
     def test_setattr(self):
         setattr(sys, 'spam', 1)
         self.assertEqual(sys.spam, 1)
-        def foo():
-            setattr(sys,1,'spam')
-        def bar():
-            setattr(1,'spam',9)
-        self.assertRaises(TypeError, foo)
-        self.assertRaises(AttributeError, bar)
+        self.assertRaises(TypeError, setattr, sys, 1, 'spam')
+        self.assertRaises(AttributeError, setattr, 1, 'spam', 9)
         self.assertRaises(TypeError, setattr)
 
 
