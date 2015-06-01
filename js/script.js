301dc07ee20ba5c3b8428d7b9432ff349ceca231
Elliott Hauser <eah13@mac.com>
2015-06-01 16:54:36 +0000

    Update Using.html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+var distance = $('[data-magellan-destination]').offset().top,
+    $window = $(window);
+
+$(window).scroll(function() {
+    if($(window).scrollTop() >= distance) {  
+        $('.floating-nav').fadeIn('fast');
+    }else{
+        $('.floating-nav').fadeOut('fast');
+    }
+});
+
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
