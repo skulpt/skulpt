1841db31a20ea45f61e0164892c63cf422e151e4
Marie Chatfield <marie.chatfield@gmail.com>
2015-06-10 13:05:20 -0500

    Make operator lib work with new numbers.

--- src/lib/operator.js
+++ src/lib/operator.js
@@ -66,17 +66,17 @@ var $builtinmodule = function (name) {
     mod.__add__ = mod.add;
 
     mod.and_ = new Sk.builtin.func(function (a, b) {
-        return Sk.builtin.nmber.prototype['nb$and'].call(a, b);
+        return Sk.abstr.binary_op_(a, b, "BitAnd");
     });
     mod.__and__ = mod.and_;
 
     mod.div = new Sk.builtin.func(function (a, b) {
-        return Sk.builtin.nmber.prototype['nb$divide'].call(a, b);
+        return Sk.abstr.binary_op_(a, b, "Div");
     });
     mod.__div__ = mod.div;
 
     mod.floordiv = new Sk.builtin.func(function (a, b) {
-        return Sk.builtin.nmber.prototype['nb$floor_divide'].call(a, b);
+        return Sk.abstr.binary_op_(a, b, "FloorDiv");
     });
     mod.__floordiv__ = mod.floordiv;
 
@@ -97,22 +97,22 @@ var $builtinmodule = function (name) {
     mod.__invert__ = mod.inv;
 
     mod.lshift = new Sk.builtin.func(function (a, b) {
-        return Sk.builtin.nmber.prototype['nb$lshift'].call(a, b);
+        return Sk.abstr.binary_op_(a, b, "LShift");
     });
     mod.__lshift__ = mod.lshift;
 
     mod.mod = new Sk.builtin.func(function (a, b) {
-        return Sk.builtin.nmber.prototype['nb$remainder'].call(a, b);
+        return Sk.abstr.binary_op_(a, b, "Mod");
     });
     mod.__mod__ = mod.mod;
 
     mod.divmod = new Sk.builtin.func(function (a, b) {
-        return Sk.builtin.nmber.prototype['nb$divmod'].call(a, b);
+        return Sk.abstr.binary_op_(a, b, "DivMod");
     });
     mod.__divmod__ = mod.divmod;
 
     mod.mul = new Sk.builtin.func(function (a, b) {
-        return Sk.builtin.nmber.prototype['nb$multiply'].call(a, b);
+        return Sk.abstr.binary_op_(a, b, "Mult");
     });
     mod.__mul__ = mod.mul;
 
@@ -122,7 +122,7 @@ var $builtinmodule = function (name) {
     mod.__neg__ = mod.neg;
 
     mod.or_ = new Sk.builtin.func(function (a, b) {
-        return Sk.builtin.nmber.prototype['nb$or'].call(a, b);
+        return Sk.abstr.binary_op_(a, b, "BitOr");
     });
     mod.__or__ = mod.or_;
 
@@ -132,17 +132,17 @@ var $builtinmodule = function (name) {
     mod.__pos__ = mod.pos;
 
     mod.pow = new Sk.builtin.func(function (a, b) {
-        return Sk.builtin.nmber.prototype['nb$power'].call(a, b);
+        return Sk.abstr.binary_op_(a, b, "Pow");
     });
     mod.__pow__ = mod.pow;
 
     mod.rshift = new Sk.builtin.func(function (a, b) {
-        return Sk.builtin.nmber.prototype['nb$rshift'].call(a, b);
+        return Sk.abstr.binary_op_(a, b, "RShift");
     });
     mod.__rshift__ = mod.rshift;
 
     mod.sub = new Sk.builtin.func(function (a, b) {
-        return Sk.builtin.nmber.prototype['nb$subtract'].call(a, b);
+        return Sk.abstr.binary_op_(a, b, "Sub");
     });
     mod.__sub__ = mod.sub;
 
@@ -150,7 +150,7 @@ var $builtinmodule = function (name) {
     mod.__truediv__ = mod.div;
 
     mod.xor = new Sk.builtin.func(function (a, b) {
-        return Sk.builtin.nmber.prototype['nb$xor'].call(a, b);
+        return Sk.abstr.binary_op_(a, b, "BitXor");
     });
     mod.__xor__ = mod.xor;
 
