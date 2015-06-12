8e17091e5124f303cce8d90b2e90db14eb80180f
Marie Chatfield <marie.chatfield@gmail.com>
2015-06-12 14:48:44 -0500

    Begin documenting seqtype and numtype.

--- src/numtype.js
+++ src/numtype.js
@@ -1,3 +1,14 @@
+/**
+ * @constructor
+ * Sk.builtin.numtype
+ *
+ * @description
+ * Abstract class for Python numeric types.
+ *
+ * @extends {Sk.builtin.object}
+ *
+ * @return {undefined} Cannot instantiate a Sk.builtin.numtype object
+ */
 Sk.builtin.numtype = function () {
 
     Sk.abstr.superConstructor(Sk.builtin.numtype, this);
@@ -6,6 +17,13 @@ Sk.builtin.numtype = function () {
 
 Sk.abstr.setUpInheritance("NumericType", Sk.builtin.numtype, Sk.builtin.object);
 
+/**
+ * Python wrapper of \_\_abs\_\_ method.
+ *
+ * @name  __abs__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__abs__"] = new Sk.builtin.func(function (self) {
 
     if (self.nb$abs === undefined) {
@@ -17,6 +35,13 @@ Sk.builtin.numtype.prototype["__abs__"] = new Sk.builtin.func(function (self) {
 
 });
 
+/**
+ * Python wrapper of \_\_neg\_\_ method.
+ *
+ * @name  __neg__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__neg__"] = new Sk.builtin.func(function (self) {
 
     if (self.nb$negative === undefined) {
@@ -28,6 +53,13 @@ Sk.builtin.numtype.prototype["__neg__"] = new Sk.builtin.func(function (self) {
 
 });
 
+/**
+ * Python wrapper of \_\_pos\_\_ method.
+ *
+ * @name  __pos__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__pos__"] = new Sk.builtin.func(function (self) {
 
     if (self.nb$positive === undefined) {
@@ -39,6 +71,13 @@ Sk.builtin.numtype.prototype["__pos__"] = new Sk.builtin.func(function (self) {
 
 });
 
+/**
+ * Python wrapper of \_\_int\_\_ method.
+ *
+ * @name  __int__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__int__"] = new Sk.builtin.func(function (self) {
 
     if (self.nb$int_ === undefined) {
@@ -50,6 +89,13 @@ Sk.builtin.numtype.prototype["__int__"] = new Sk.builtin.func(function (self) {
 
 });
 
+/**
+ * Python wrapper of \_\_long\_\_ method.
+ *
+ * @name  __long__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__long__"] = new Sk.builtin.func(function (self) {
 
     if (self.nb$lng === undefined) {
@@ -61,6 +107,13 @@ Sk.builtin.numtype.prototype["__long__"] = new Sk.builtin.func(function (self) {
 
 });
 
+/**
+ * Python wrapper of \_\_float\_\_ method.
+ *
+ * @name  __float__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__float__"] = new Sk.builtin.func(function (self) {
 
     if (self.nb$float_ === undefined) {
@@ -72,6 +125,13 @@ Sk.builtin.numtype.prototype["__float__"] = new Sk.builtin.func(function (self) 
 
 });
 
+/**
+ * Python wrapper of \_\_add\_\_ method.
+ *
+ * @name  __add__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__add__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$add === undefined) {
@@ -83,6 +143,13 @@ Sk.builtin.numtype.prototype["__add__"] = new Sk.builtin.func(function (self, ot
 
 });
 
+/**
+ * Python wrapper of \_\_radd\_\_ method.
+ *
+ * @name  __radd__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__radd__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$reflected_add === undefined) {
@@ -94,6 +161,13 @@ Sk.builtin.numtype.prototype["__radd__"] = new Sk.builtin.func(function (self, o
 
 });
 
+/**
+ * Python wrapper of \_\_sub\_\_ method.
+ *
+ * @name  __sub__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__sub__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$subtract === undefined) {
@@ -105,6 +179,13 @@ Sk.builtin.numtype.prototype["__sub__"] = new Sk.builtin.func(function (self, ot
 
 });
 
+/**
+ * Python wrapper of \_\_rsub\_\_ method.
+ *
+ * @name  __rsub__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__rsub__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$reflected_subtract === undefined) {
@@ -116,6 +197,13 @@ Sk.builtin.numtype.prototype["__rsub__"] = new Sk.builtin.func(function (self, o
 
 });
 
+/**
+ * Python wrapper of \_\_mul\_\_ method.
+ *
+ * @name  __mul__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__mul__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$multiply === undefined) {
@@ -127,6 +215,13 @@ Sk.builtin.numtype.prototype["__mul__"] = new Sk.builtin.func(function (self, ot
 
 });
 
+/**
+ * Python wrapper of \_\_rmul\_\_ method.
+ *
+ * @name  __rmul__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__rmul__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$reflected_multiply === undefined) {
@@ -138,6 +233,13 @@ Sk.builtin.numtype.prototype["__rmul__"] = new Sk.builtin.func(function (self, o
 
 });
 
+/**
+ * Python wrapper of \_\_div\_\_ method.
+ *
+ * @name  __div__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__div__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$divide === undefined) {
@@ -149,6 +251,13 @@ Sk.builtin.numtype.prototype["__div__"] = new Sk.builtin.func(function (self, ot
 
 });
 
+/**
+ * Python wrapper of \_\_rdiv\_\_ method.
+ *
+ * @name  __rdiv__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__rdiv__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$reflected_divide === undefined) {
@@ -160,6 +269,13 @@ Sk.builtin.numtype.prototype["__rdiv__"] = new Sk.builtin.func(function (self, o
 
 });
 
+/**
+ * Python wrapper of \_\_floordiv\_\_ method.
+ *
+ * @name  __floordiv__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__floordiv__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$floor_divide === undefined) {
@@ -171,6 +287,13 @@ Sk.builtin.numtype.prototype["__floordiv__"] = new Sk.builtin.func(function (sel
 
 });
 
+/**
+ * Python wrapper of \_\_rfloordiv\_\_ method.
+ *
+ * @name  __rfloordiv__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__rfloordiv__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$reflected_floor_divide === undefined) {
@@ -182,6 +305,13 @@ Sk.builtin.numtype.prototype["__rfloordiv__"] = new Sk.builtin.func(function (se
 
 });
 
+/**
+ * Python wrapper of \_\_mod\_\_ method.
+ *
+ * @name  __mod__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__mod__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$remainder === undefined) {
@@ -193,6 +323,13 @@ Sk.builtin.numtype.prototype["__mod__"] = new Sk.builtin.func(function (self, ot
 
 });
 
+/**
+ * Python wrapper of \_\_rmod\_\_ method.
+ *
+ * @name  __rmod__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__rmod__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$reflected_remainder === undefined) {
@@ -204,6 +341,13 @@ Sk.builtin.numtype.prototype["__rmod__"] = new Sk.builtin.func(function (self, o
 
 });
 
+/**
+ * Python wrapper of \_\_divmod\_\_ method.
+ *
+ * @name  __divmod__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__divmod__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$divmod === undefined) {
@@ -215,6 +359,13 @@ Sk.builtin.numtype.prototype["__divmod__"] = new Sk.builtin.func(function (self,
 
 });
 
+/**
+ * Python wrapper of \_\_rdivmod\_\_ method.
+ *
+ * @name  __rdivmod__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__rdivmod__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$reflected_divmod === undefined) {
@@ -226,6 +377,13 @@ Sk.builtin.numtype.prototype["__rdivmod__"] = new Sk.builtin.func(function (self
 
 });
 
+/**
+ * Python wrapper of \_\_pow\_\_ method.
+ *
+ * @name  __pow__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__pow__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$power === undefined) {
@@ -237,6 +395,13 @@ Sk.builtin.numtype.prototype["__pow__"] = new Sk.builtin.func(function (self, ot
 
 });
 
+/**
+ * Python wrapper of \_\_rpow\_\_ method.
+ *
+ * @name  __rpow__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__rpow__"] = new Sk.builtin.func(function (self, other) {
 
     if (self.nb$reflected_power === undefined) {
@@ -248,6 +413,13 @@ Sk.builtin.numtype.prototype["__rpow__"] = new Sk.builtin.func(function (self, o
 
 });
 
+/**
+ * Python wrapper of \_\_coerce\_\_ method.
+ *
+ * @name  __coerce__
+ * @instance
+ * @memberOf Sk.builtin.numtype.prototype
+ */
 Sk.builtin.numtype.prototype["__coerce__"] = new Sk.builtin.func(function (self, other) {
 
     throw new Sk.builtin.NotImplementedError("__coerce__ is not yet implemented");
@@ -260,9 +432,40 @@ Sk.abstr.registerPythonFunctions(Sk.builtin.numtype,
      "__div__", "__rdiv__", "__floordiv__", "__rfloordiv__",
      "__mod__", "__rmod__", "__divmod__", "__rdivmod__", "__coerce__"]);
 
+/**
+ * Add a Python object to this instance and return the result (i.e. this + other).
+ *
+ * Returns NotImplemented if addition between this type and other type is unsupported.
+ *
+ * Javscript function, returns Python object.
+ *
+ * @param  {!Sk.builtin.object} other The Python object to add.
+ * @return {(Sk.builtin.numtype|Sk.builtin.NotImplemented)} The result of the addition.
+ */
+Sk.builtin.numtype.prototype.nb$add = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
 
 Sk.builtin.numtype.prototype.nb$reflected_add = function (other) {
     return this.nb$add(other);
+};
+
+Sk.builtin.numtype.prototype.nb$inplace_add = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+/**
+ * Subtract a Python object from this instance and return the result (i.e. this - other).
+ *
+ * Returns NotImplemented if subtraction between this type and other type is unsupported.
+ *
+ * Javscript function, returns Python object.
+ *
+ * @param  {!Sk.builtin.object} other The Python object to subtract.
+ * @return {(Sk.builtin.numtype|Sk.builtin.NotImplemented)} The result of the subtraction.
+ */
+Sk.builtin.numtype.prototype.nb$subtract = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
 };
 
 Sk.builtin.numtype.prototype.nb$reflected_subtract = function (other) {
@@ -270,6 +473,205 @@ Sk.builtin.numtype.prototype.nb$reflected_subtract = function (other) {
     return negative_this.nb$add(other);
 };
 
+Sk.builtin.numtype.prototype.nb$inplace_subtract = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+/**
+ * Multiply this instance by a Python object and return the result (i.e. this * other).
+ *
+ * Returns NotImplemented if multiplication between this type and other type is unsupported.
+ *
+ * Javscript function, returns Python object.
+ *
+ * @param  {!Sk.builtin.object} other The multiplier, which must be a Python object.
+ * @return {(Sk.builtin.numtype|Sk.builtin.NotImplemented)} The result of the multiplication
+ */
+Sk.builtin.numtype.prototype.nb$multiply = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+
 Sk.builtin.numtype.prototype.nb$reflected_multiply = function (other) {
     return this.nb$multiply(other);
+};
+
+Sk.builtin.numtype.prototype.nb$inplace_multiply = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+/**
+ * Divide this instance by a Python object and return the result (i.e this / other).
+ *
+ * Returns NotImplemented if division between this type and other type is unsupported.
+ *
+ * Javscript function, returns Python object.
+ *
+ * @param  {!Sk.builtin.object} other The divisor, which must be a Python object.
+ * @return {(Sk.builtin.numtype|Sk.builtin.NotImplemented)} The result of the division
+ */
+Sk.builtin.numtype.prototype.nb$divide = function (other) {
+    return this.nb$floor_divide(other);
+};
+
+Sk.builtin.numtype.prototype.nb$reflected_divide = function (other) {
+    return this.nb$reflected_floor_divide(other);
+};
+
+Sk.builtin.numtype.prototype.nb$inplace_divide = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+/**
+ * Floor divide this instance by a Python object and return the result (i.e. this // other).
+ *
+ * Returns NotImplemented if floor division between this type and other type is unsupported.
+ *
+ * Javscript function, returns Python object.
+ *
+ * @param  {!Sk.builtin.object} other The divisor, which must be a Python object.
+ * @return {(Sk.builtin.numtype|Sk.builtin.NotImplemented)} The result of the floor division
+ */
+Sk.builtin.numtype.prototype.nb$floor_divide = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+Sk.builtin.numtype.prototype.nb$reflected_floor_divide = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+Sk.builtin.numtype.prototype.nb$inplace_floor_divide = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+/**
+ * Modulo this instance by a Python object and return the result (i.e. this % other).
+ *
+ * Returns NotImplemented if modulation between this type and other type is unsupported.
+ *
+ * Javscript function, returns Python object.
+ *
+ * @param  {!Sk.builtin.object} other The divisor, which must be a Python object.
+ * @return {(Sk.builtin.numtype|Sk.builtin.NotImplemented)} The result of the modulation
+ */
+Sk.builtin.numtype.prototype.nb$remainder = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+Sk.builtin.numtype.prototype.nb$reflected_remainder = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+Sk.builtin.numtype.prototype.nb$inplace_remainder = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+/**
+ * Compute the quotient and the remainder of this instance and a given Python object and return the result.
+ *
+ * Returns NotImplemented if division or modulo operations between this type and other type are unsupported.
+ *
+ * Javscript function, returns Python object.
+ *
+ * @param  {!Sk.builtin.object} other The divisor, which must be a Python object.
+ * @return {(Sk.builtin.tuple|Sk.builtin.NotImplemented)} The result of the operation.
+ * If both operations are supported, a Python tuple containing (quotient, remainder) in that order.
+ */
+Sk.builtin.numtype.prototype.nb$divmod = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+Sk.builtin.numtype.prototype.nb$reflected_divmod = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+/**
+ * Raise this instance by a Python object, optionally modulo the exponent, and return the final result.
+ *
+ * If mod is undefined, return this \*\* other. Else, return (this \*\* other) % mod.
+ *
+ * Returns NotImplemented if exponentiation or modulation between this type and other type is unsupported.
+ *
+ * Javscript function, returns Python object.
+ *
+ * @param  {!Sk.builtin.object} other The exponent, which must be a Python object.
+ * @param  {!Sk.builtin.object=} mod The optional divisor, which must be a Python object if defined.
+ * @return {(Sk.builtin.numtype|Sk.builtin.NotImplemented)} The result of the exponentiation.
+ */
+Sk.builtin.numtype.prototype.nb$power = function (other, mod) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+Sk.builtin.numtype.prototype.nb$reflected_power = function (other, mod) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+Sk.builtin.numtype.prototype.nb$inplace_power = function (other) {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+/**
+ * Compute the absolute value of this instance and return.
+ *
+ * Javascript function, returns Python object.
+ *
+ * @return {(Sk.builtin.numtype|Sk.builtin.NotImplemented)} The absolute value
+ */
+Sk.builtin.numtype.prototype.nb$abs = function () {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+/**
+ * Compute the unary negative of this instance (i.e. -this).
+ *
+ * Javscript function, returns Python object.
+ *
+ * @return {(Sk.builtin.numtype|Sk.builtin.NotImplemented)} A copy of this instance with the value negated
+ */
+Sk.builtin.numtype.prototype.nb$negative = function () {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+/**
+ * Compute the unary positive of this instance (i.e. +this).
+ *
+ * Javscript function, returns Python object.
+ *
+ * @return {(Sk.builtin.numtype|Sk.builtin.NotImplemented)} A copy of this instance with the value unchanged
+ */
+Sk.builtin.numtype.prototype.nb$positive = function () {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+/**
+ * Determine if this instance is nonzero.
+ *
+ * Javscript function, returns Javascript object or Sk.builtin.NotImplemented.
+ *
+ * @return {(boolean|Sk.builtin.NotImplemented)} true if this instance is not equal to zero, false otherwise
+ */
+Sk.builtin.numtype.prototype.nb$nonzero = function () {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+/**
+ * Determine if this instance is negative.
+ *
+ * Javscript function, returns Javascript object or Sk.builtin.NotImplemented.
+ *
+ * @return {(boolean|Sk.builtin.NotImplemented)} true if this instance is negative, false otherwise
+ */
+Sk.builtin.numtype.prototype.nb$isnegative = function () {
+    return Sk.builtin.NotImplemented.NotImplemented$;
+};
+
+/**
+ * Determine if this instance is positive.
+ *
+ * Javscript function, returns Javascript object or Sk.builtin.NotImplemented.
+ *
+ * @return {(boolean|Sk.builtin.NotImplemented)} true if this instance is positive, false otherwise
+ */
+Sk.builtin.numtype.prototype.nb$ispositive = function () {
+    return Sk.builtin.NotImplemented.NotImplemented$;
 };
--- src/seqtype.js
+++ src/seqtype.js
@@ -1,3 +1,14 @@
+/**
+ * @constructor
+ * Sk.builtin.seqtype
+ *
+ * @description
+ * Abstract class for Python sequence types.
+ *
+ * @extends {Sk.builtin.object}
+ *
+ * @return {undefined} Cannot instantiate a Sk.builtin.seqtype object
+ */
 Sk.builtin.seqtype = function () {
 
     Sk.abstr.superConstructor(Sk.builtin.seqtype, this);
@@ -6,6 +17,13 @@ Sk.builtin.seqtype = function () {
 
 Sk.abstr.setUpInheritance("SequenceType", Sk.builtin.seqtype, Sk.builtin.object);
 
+/**
+ * Python wrapper of \_\_len\_\_ method.
+ *
+ * @name  __len__
+ * @instance
+ * @memberOf Sk.builtin.seqtype.prototype
+ */
 Sk.builtin.seqtype.prototype["__len__"] = new Sk.builtin.func(function (self) {
 
     Sk.builtin.pyCheckArgs("__len__", arguments, 0, 0, false, true);
@@ -14,6 +32,13 @@ Sk.builtin.seqtype.prototype["__len__"] = new Sk.builtin.func(function (self) {
 
 });
 
+/**
+ * Python wrapper of \_\_iter\_\_ method.
+ *
+ * @name  __iter__
+ * @instance
+ * @memberOf Sk.builtin.seqtype.prototype
+ */
 Sk.builtin.seqtype.prototype["__iter__"] = new Sk.builtin.func(function (self) {
 
     Sk.builtin.pyCheckArgs("__iter__", arguments, 0, 0, false, true);
@@ -22,6 +47,13 @@ Sk.builtin.seqtype.prototype["__iter__"] = new Sk.builtin.func(function (self) {
 
 });
 
+/**
+ * Python wrapper of \_\_contains\_\_ method.
+ *
+ * @name  __contains__
+ * @instance
+ * @memberOf Sk.builtin.seqtype.prototype
+ */
 Sk.builtin.seqtype.prototype["__contains__"] = new Sk.builtin.func(function (self, item) {
 
     Sk.builtin.pyCheckArgs("__contains__", arguments, 1, 1, false, true);
@@ -34,6 +66,13 @@ Sk.builtin.seqtype.prototype["__contains__"] = new Sk.builtin.func(function (sel
 
 });
 
+/**
+ * Python wrapper of \_\_getitem\_\_ method.
+ *
+ * @name  __getitem__
+ * @instance
+ * @memberOf Sk.builtin.seqtype.prototype
+ */
 Sk.builtin.seqtype.prototype["__getitem__"] = new Sk.builtin.func(function (self, key) {
 
     Sk.builtin.pyCheckArgs("__getitem__", arguments, 1, 1, false, true);
@@ -42,6 +81,13 @@ Sk.builtin.seqtype.prototype["__getitem__"] = new Sk.builtin.func(function (self
 
 });
 
+/**
+ * Python wrapper of \_\_add\_\_ method.
+ *
+ * @name  __add__
+ * @instance
+ * @memberOf Sk.builtin.seqtype.prototype
+ */
 Sk.builtin.seqtype.prototype["__add__"] = new Sk.builtin.func(function (self, other) {
 
     Sk.builtin.pyCheckArgs("__add__", arguments, 1, 1, false, true);
@@ -50,6 +96,13 @@ Sk.builtin.seqtype.prototype["__add__"] = new Sk.builtin.func(function (self, ot
 
 });
 
+/**
+ * Python wrapper of \_\_mul\_\_ method.
+ *
+ * @name  __mul__
+ * @instance
+ * @memberOf Sk.builtin.seqtype.prototype
+ */
 Sk.builtin.seqtype.prototype["__mul__"] = new Sk.builtin.func(function (self, n) {
 
     Sk.builtin.pyCheckArgs("__mul__", arguments, 1, 1, false, true);
@@ -62,6 +115,13 @@ Sk.builtin.seqtype.prototype["__mul__"] = new Sk.builtin.func(function (self, n)
 
 });
 
+/**
+ * Python wrapper of \_\_rmul\_\_ method.
+ *
+ * @name  __rmul__
+ * @instance
+ * @memberOf Sk.builtin.seqtype.prototype
+ */
 Sk.builtin.seqtype.prototype["__rmul__"] = new Sk.builtin.func(function (self, n) {
 
     Sk.builtin.pyCheckArgs("__rmul__", arguments, 1, 1, false, true);
