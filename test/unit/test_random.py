f42637880e264be7fa79e10f252662ad83121d4c
Antti Ajanki <antti.ajanki@iki.fi>
2016-01-19 19:41:38 +0200

    Implement random.gauss() and the alias random.normalvariate()

--- test/unit/test_random.py
+++ test/unit/test_random.py
@@ -17,6 +17,8 @@ class Test_Distributions(unittest.TestCase):
         x = [i/float(N) for i in xrange(1,N)]
         for variate, args, mu, sigmasqrd in [
                 (random.uniform, (1.0,10.0), (10.0+1.0)/2, (10.0-1.0)**2/12),
+                (random.gauss, (-5.0,2.0), -5.0, 2.0**2),
+                (random.normalvariate, (2.0,0.8), 2.0, 0.8**2),
                 (random.triangular, (0.0, 1.0, 1.0/3.0), 4.0/9.0, 7.0/9.0/18.0) #,
 #                 (g.expovariate, (1.5,), 1/1.5, 1/1.5**2),
 #                 (g.paretovariate, (5.0,), 5.0/(5.0-1),
