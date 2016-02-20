11c9c9a6bd7372c3526c6a6910ba5362b6a6feb9
Antti Ajanki <antti.ajanki@iki.fi>
2016-02-20 19:29:13 +0200

    Implement the reversed() builtin

--- test/unit/test_builtin.py
+++ test/unit/test_builtin.py
@@ -248,6 +248,16 @@ class BuiltinTest(unittest.TestCase):
         self.assertEqual(abs(bar), 3)
         self.assertEqual(abs(-3), 3)
 
+    def test_reversed(self):
+        class TestReversed:
+            def __reversed__(self):
+                return [5, 4, 3, 2, 1]
+
+        self.assertEqual(list(reversed([1, 2, 3])), [3, 2, 1])
+
+        obj = TestReversed()
+        self.assertEqual(reversed(obj), [5, 4, 3, 2, 1])
+
     def test_reduce(self):
         add = lambda x, y: x+y
         self.assertEqual(reduce(add, ['a', 'b', 'c'], ''), 'abc')
