fd3d592b2402ada7747d3ab055e1f067e6718962
Albert-Jan Nijburg <albertjan@curit.com>
2016-02-27 13:17:10 +0000

    fix

--- src/builtin.js
+++ src/builtin.js
@@ -802,7 +802,9 @@ Sk.builtin.setattr = function setattr (obj, name, value) {
 
 Sk.builtin.raw_input = function (prompt) {
     var sys = Sk.importModule("sys");
-    Sk.misceval.callsimOrSuspend(sys["$d"]["stdout"]["write"], sys["$d"]["stdout"], prompt);
+    if (prompt) {
+        Sk.misceval.callsimOrSuspend(sys["$d"]["stdout"]["write"], sys["$d"]["stdout"], prompt);
+    }
     return Sk.misceval.callsimOrSuspend(sys["$d"]["stdin"]["readline"], sys["$d"]["stdin"]);
 };
 
