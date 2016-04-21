3574ca6d38865d4bfc4477bf20dd89999350abb8
Brian Marks <brianpmarks@gmail.com>
2016-04-21 21:40:55 +0000

    For processing: Adds alpha arg to fill and green, blue, alpha args to colorMode.

--- src/lib/processing.js
+++ src/lib/processing.js
@@ -1234,20 +1234,24 @@ var $builtinmodule = function (name) {
 
     });
 
-    mod.fill = new Sk.builtin.func(function (r, g, b) {
+    mod.fill = new Sk.builtin.func(function (r, g, b, alpha) {
         // r will be either:
         //      a number in which case the fill will be grayscale
         //      a color object
         // g, and b may be undefined.  If they hold values it will
         // be assumed that we have an r,g,b color tuple
+        // alpha may also be undefined - if defined, it is the opacity of the fill
         if (typeof(g) !== "undefined") {
             g = g.v;
         }
         if (typeof(b) !== "undefined") {
             b = b.v;
         }
+        if (typeof(alpha) !== "undefined") {
+            alpha = alpha.v;
+        }
 
-        mod.processing.fill(r.v, g, b);
+        mod.processing.fill(r.v, g, b, alpha);
 
     });
 
@@ -1270,14 +1274,27 @@ var $builtinmodule = function (name) {
     });
 
 
-    mod.colorMode = new Sk.builtin.func(function (model, maxV) {
+    mod.colorMode = new Sk.builtin.func(function (mode, maxV, maxG, maxB, maxAlpha) {
+        // mode is one of RGB or HSB
+        // maxV is either the max value for all color elements
+        // or the range for Red/Hue (depending on mode) if maxG and maxB are defined
         if (typeof(maxV) === "undefined") {
             maxV = 255;
         }
         else {
             maxV = maxV.v;
         }
-        mod.processing.colorMode(model.v, maxV);
+        if (typeof(maxG) !== "undefined") {
+            maxG = maxG.v;
+        }
+        if (typeof(maxB) !== "undefined") {
+            maxB = maxB.v;
+        }
+        if (typeof(maxAlpha) !== "undefined") {
+            maxAlpha = maxAlpha.v;
+        }
+
+        mod.processing.colorMode(mode.v, maxV, maxG, maxB, maxAlpha);
     });
 
     mod.noFill = new Sk.builtin.func(function () {
