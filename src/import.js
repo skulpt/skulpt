15321d2891cebcb82612455693a47faa8d325901
Albert-Jan Nijburg <anijburg@autotask.com>
2016-05-15 18:03:20 +0100

    assign to Sk.builtins

--- src/import.js
+++ src/import.js
@@ -238,8 +238,10 @@ Sk.doOneTimeInitialization = function () {
 
     // compile internal python files and add them to the __builtin__ module
     for (var file in Sk.internalPy.files) {
-        var mod = Sk.importBuiltinWithBody(file.split(".")[0], false, Sk.internalPy.files[file], false);
-        Sk.importStar(mod, Sk.globals, Sk.globals);
+        var fileWithoutExtension = file.split(".")[0].split('/')[1];
+        var mod = Sk.importBuiltinWithBody(fileWithoutExtension, false, Sk.internalPy.files[file], false);
+        goog.asserts.assert(mod["$d"][fileWithoutExtension] !== undefined, "Should have imported name " + fileWithoutExtension);
+        Sk.builtins[fileWithoutExtension] = mod["$d"][fileWithoutExtension];
     }
 };
 
