eba5213d7c0cd74bd5e5c0f8834f849a10ceb917
Scott Rixner <rixner@rice.edu>
2016-05-20 15:34:19 -0500

    This code is currently not written such that it can handle getting suspensions back.

--- src/import.js
+++ src/import.js
@@ -574,8 +574,8 @@ Sk.builtin.__import__ = function (name, globals, locals, fromlist) {
                 if (fromName != "*" && ret.$d[fromName] == null && (ret.$d[dottedName] != null || ret.$d.__name__.v == dottedName)) {
                     // add the module name to our requiredImport list
                     fromImportName = "" + name + "." + fromName;
-                    fromNameRet = Sk.importModuleInternal_(fromImportName, undefined, undefined, undefined, true, currentDir);
-                    ret["$d"][fromName] = Sk.abstr.gattr(fromNameRet, fromName, true);
+                    fromNameRet = Sk.importModuleInternal_(fromImportName, undefined, undefined, undefined, false, currentDir);
+                    ret["$d"][fromName] = Sk.abstr.gattr(fromNameRet, fromName, false);
                 }
             }
         }
