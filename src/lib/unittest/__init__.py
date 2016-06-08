4298e3655cf793154ffeb0eda60d5a0fd0715a68
Brad Miller <bonelake@mac.com>
2016-06-08 10:26:44 -0400

    save working version of units with better gui output

--- src/lib/unittest/__init__.py
+++ src/lib/unittest/__init__.py
@@ -25,12 +25,12 @@ class TestCase:
     def tearDown(self):
         pass
     def cleanName(self,funcName):
-    # work around skulpts lack of an __name__ 
+    # work around skulpts lack of an __name__
         funcName = str(funcName)
         funcName = funcName[13:]
         funcName = funcName[:funcName.find('<')-3]
         return funcName
-        
+
     def main(self):
 
         for func in self.tlist:
@@ -55,75 +55,111 @@ class TestCase:
 
     def assertEqual(self, actual, expected, feedback=""):
         res = actual==expected
-        self.appendResult(res,str(actual)+' to be equal to ',expected, feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to equal %s" % (str(actual),str(expected))
+        self.appendResult(res, actual ,expected, feedback)
 
     def assertNotEqual(self, actual, expected, feedback=""):
         res = actual != expected
-        self.appendResult(res,str(actual)+' to not equal ',expected,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to not equal %s" % (str(actual),str(expected))
+        self.appendResult(res, actual, expected, feedback)
 
     def assertTrue(self,x, feedback=""):
         res = bool(x) is True
-        self.appendResult(res,str(x)+' to be ',True,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to be True" % (str(x))
+        self.appendResult(res, x, True, feedback)
 
     def assertFalse(self,x, feedback=""):
         res = not bool(x)
-        self.appendResult(res,str(x)+' to be ',False,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to be False" % (str(x))
+        self.appendResult(res, x, False, feedback)
 
     def assertIs(self,a,b, feedback=""):
         res = a is b
-        self.appendResult(res,str(a)+' to be the same object as ',b,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to be the same object as %s" % (str(a),str(b))
+        self.appendResult(res, a, b, feedback)
 
     def assertIsNot(self,a,b, feedback=""):
         res = a is not b
-        self.appendResult(res,str(a)+' to not be the same object as ',b,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to not be the same object as %s" % (str(a),str(b))
+        self.appendResult(res, a, b, feedback)
 
     def assertIsNone(self,x, feedback=""):
         res = x is None
-        self.appendResult(res,x,None,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to be None" % (str(x))
+        self.appendResult(res, x, None, feedback)
 
     def assertIsNotNone(self,x, feedback=""):
         res = x is not None
-        self.appendResult(res,str(x)+' to not be ',None,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to not be None" % (str(x))
+        self.appendResult(res, x, None, feedback)
 
-    def assertIn(self,a,b, feedback=""):
+    def assertIn(self, a, b, feedback=""):
         res = a in b
-        self.appendResult(res,str(a)+' to be in ',b,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to be in %s" % (str(a),str(b))
+        self.appendResult(res, a, b, feedback)
 
-    def assertNotIn(self,a,b, feedback=""):
+    def assertNotIn(self, a, b, feedback=""):
         res = a not in b
-        self.appendResult(res,str(a)+' to not be in ',b,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to not be in %s" % (str(a),str(b))
+        self.appendResult(res, a, b, feedback)
 
     def assertIsInstance(self,a,b, feedback=""):
         res = isinstance(a,b)
-        self.appendResult(res,str(a)+' to be an instance of ',b,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to be an instance of %s" % (str(a), str(b))
+        self.appendResult(res, a, b, feedback)
 
     def assertNotIsInstance(self,a,b, feedback=""):
         res = not isinstance(a,b)
-        self.appendResult(res,str(a)+' to not be an instance of ',b,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to not be an instance of %s" % (str(a),str(b))
+        self.appendResult(res, a, b, feedback)
 
     def assertAlmostEqual(self, a, b, places=7, feedback=""):
         res = round(a-b, places) == 0
-        self.appendResult(res,str(a)+' to equal ',b,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to equal %s" % (str(actual),str(expected))
+        self.appendResult(res, a, b, feedback)
 
     def assertNotAlmostEqual(self, a, b, places=7, feedback=""):
         res = round(a-b, places) != 0
-        self.appendResult(res,str(a)+' to not equal ',b,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to not equal %s" % (str(a),str(b))
+        self.appendResult(res, a, b, feedback)
 
     def assertGreater(self,a,b, feedback=""):
         res = a > b
-        self.appendResult(res,str(a)+' to be greater than ',b,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to be greater than %s" % (str(a),str(b))
+        self.appendResult(res, a, b, feedback)
 
     def assertGreaterEqual(self,a,b, feedback=""):
         res = a >= b
-        self.appendResult(res,str(a)+' to be greater than or equal to ',b,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to be >= %s" % (str(a),str(b))
+        self.appendResult(res, a, b, feedback)
 
-    def assertLess(self,a,b, feedback=""):
+    def assertLess(self, a, b, feedback=""):
         res = a < b
-        self.appendResult(res,str(a)+' to be less than ',b,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to be less than %s" % (str(a),str(b))
+        self.appendResult(res, a, b, feedback)
 
     def assertLessEqual(self,a,b, feedback=""):
         res = a <= b
-        self.appendResult(res,str(a)+' to be less than or equal to ',b,feedback)
+        if not res and feedback == "":
+            feedback = "Expected %s to be <= %s" % (str(a),str(b))
+        self.appendResult(res, a, b, feedback)
 
     def appendResult(self,res,actual,expected,feedback):
         if res:
@@ -154,7 +190,7 @@ class TestCase:
             actualerror = "No Error"
 
         self.appendResult(res, str(exception()), actualerror, "")
-        
+
     def fail(self, msg=None):
         if msg is None:
             msg = 'Fail'
@@ -182,4 +218,3 @@ def main(verbose=False, names=None):
                 tc.main()
             except:
                 print("Uncaught Error in: ", name)
-
--- src/lib/unittest/gui.py
+++ src/lib/unittest/gui.py
@@ -2,59 +2,113 @@ import document
 from unittest import TestCase
 
 class TestCaseGui(TestCase):
-	def __init__(self):
-		TestCase.__init__(self)
-
-		self.divid = document.currentDiv()
-		self.mydiv = document.getElementById(self.divid)
-		res = document.getElementById(self.divid+'_unit_results')
-		if res:
-			self.resdiv = res
-			res.innerHTML = ''
-		else:
-			self.resdiv = document.createElement('div')
-			self.resdiv.setAttribute('id',self.divid+'_unit_results')
-			self.resdiv.setAttribute('class','unittest-results')
-		self.mydiv.appendChild(self.resdiv)
-
-
-	def main(self):
-		l = document.createElement('ul')
-		self.resdiv.appendChild(l)
-		self.resList = l
-
-		for func in self.tlist:
-			try:
-				self.setup()
-				func()
-				self.tearDown()
-			except Exception as e:
-				self.appendResult('Error', None, None, e)
-				self.numFailed += 1
-		self.showSummary()
-
-	def appendResult(self,res,actual,expected,feedback):
-		if res == 'Error':
-			msg = 'Error: %s' % feedback
-		elif res:
-			msg = 'Pass'
-			self.numPassed += 1
-		else:
-			msg = 'Fail: expected %s  %s ' % (str(actual),str(expected)) + feedback
-			self.numFailed += 1
-
-		pTag = document.createElement('li')
-		pTag.innerHTML = msg
-		self.resList.appendChild(pTag)
+     def __init__(self):
+          TestCase.__init__(self)
+          self.divid = document.currentDiv()
+          self.mydiv = document.getElementById(self.divid)
+          res = document.getElementById(self.divid+'_unit_results')
+          if res:
+              self.resdiv = res
+              res.innerHTML = ''
+          else:
+              self.resdiv = document.createElement('div')
+              self.resdiv.setAttribute('id',self.divid+'_unit_results')
+              self.resdiv.setAttribute('class','unittest-results')
+              self.mydiv.appendChild(self.resdiv)
 
 
+     def main(self):
+         t = document.createElement('table')
+         self.resTable = t
+         self.resdiv.appendChild(self.resTable)
 
-	def showSummary(self):
-		pct = self.numPassed / (self.numPassed+self.numFailed) * 100
-		pTag = document.createElement('p')
-		pTag.innerHTML = "You passed: " + str(pct) + "% of the tests"
-		self.resdiv.appendChild(pTag)
-		if pct < 90:
-			self.resdiv.setCSS('background-color','#de8e96')
-		else:
-			self.resdiv.setCSS('background-color','#83d382')
+         headers = ['Result','Actual Value','Expected Value','Notes']
+         row = document.createElement('tr')
+         for item in headers:
+             head = document.createElement('th')
+             head.innerHTML = item
+             head.setCSS('text-align','center')
+             row.appendChild(head)
+         self.resTable.appendChild(row)
+
+         for func in self.tlist:
+             try:
+                 self.setup()
+                 func()
+                 self.tearDown()
+             except Exception as e:
+                 self.appendResult('Error', None, None, e)
+                 self.numFailed += 1
+                 self.showSummary()
+
+     def appendResult(self,res,actual,expected,param):
+         trimActual = False
+         if len(str(actual)) > 15:
+             trimActual = True
+             actualType = type(actual)
+         trimExpected = False
+         if len(str(expected)) > 15:
+             trimExpected = True
+             expectedType = type(expected)
+         row = document.createElement('tr')
+         err = False
+         if res == 'Error':
+             err = True
+             msg = 'Error: %s' % param
+             errorData = document.createElement('td')
+             errorData.innerHTML = 'ERROR'
+             errorData.setCSS('background-color','#de8e96')
+             errorData.setCSS('text-align','center')
+             row.appendChild(errorData)
+         elif res:
+             passed = document.createElement('td')
+             passed.innerHTML = 'PASS'
+             passed.setCSS('background-color','#83d382')
+             passed.setCSS('text-align','center')
+             row.appendChild(passed)
+             self.numPassed += 1
+         else:
+             fail = document.createElement('td')
+             fail.innerHTML = 'FAIL'
+             fail.setCSS('background-color','#de8e96')
+             fail.setCSS('text-align','center')
+             row.appendChild(fail)
+             self.numFailed += 1
+
+
+         act = document.createElement('td')
+         if trimActual:
+             actHTML = str(actual)[:5] + "..." + str(actual)[-5:]
+             if actualType == str:
+                 actHTML = repr(actHTML)
+             act.innerHTML = actHTML
+         else:
+             act.innerHTML = repr(actual)
+         act.setCSS('text-align','center')
+         row.appendChild(act)
+
+         expect = document.createElement('td')
+         if trimExpected:
+             expectedHTML = str(expected)[:5] + "..." + str(expected)[-5:]
+             if expectedType == str:
+                 expectedHTML = repr(expectedHTML)
+             expect.innerHTML = expectedHTML
+         else:
+             expect.innerHTML = repr(expected)
+         expect.setCSS('text-align','center')
+         row.appendChild(expect)
+         inp = document.createElement('td')
+         if err:
+             inp.innerHTML = msg
+         else:
+             inp.innerHTML = param
+         inp.setCSS('text-align','center')
+         row.appendChild(inp)
+         self.resTable.appendChild(row)
+
+
+     def showSummary(self):
+         pct = self.numPassed / (self.numPassed+self.numFailed) * 100
+         pTag = document.createElement('p')
+         pTag.innerHTML = "You passed: " + str(pct) + "% of the tests"
+         self.resdiv.appendChild(pTag)
