6c31989eac4d2af9cab850f907a01657426950fc
Meredydd Luff <meredydd@senatehouse.org>
2016-07-28 21:03:49 +0100

    Revert "Extract try/catch out of richCompareBool"
    This is a bug from pull request #604
    
    This reverts commit d975afd564b0df2f0427603598f4af33eeda01e4.

--- src/misceval.js
+++ src/misceval.js
@@ -211,15 +211,6 @@ Sk.misceval.swappedOp_ = {
     "NotIn": "In_"
 };
 
-Sk.misceval._tryCatch = function (func, catchHandler) {
-    try {
-        return func();
-    } catch (err) {
-        return catchHandler(err);
-    }
-};
-goog.exportSymbol("Sk.misceval._tryCatch", Sk.misceval._tryCatch);
-
 /**
 * @param{*} v
 * @param{*} w
@@ -456,7 +447,7 @@ Sk.misceval.richCompareBool = function (v, w, op, canSuspend) {
 
     vcmp = Sk.abstr.lookupSpecial(v, "__cmp__");
     if (vcmp) {
-        return Sk.misceval._tryCatch(function() {
+        try {
             ret = Sk.misceval.callsim(vcmp, v, w);
             if (Sk.builtin.checkNumber(ret)) {
                 ret = Sk.builtin.asnum$(ret);
@@ -478,15 +469,15 @@ Sk.misceval.richCompareBool = function (v, w, op, canSuspend) {
             if (ret !== Sk.builtin.NotImplemented.NotImplemented$) {
                 throw new Sk.builtin.TypeError("comparison did not return an int");
             }
-        }, function (e) {
+        } catch (e) {
             throw new Sk.builtin.TypeError("comparison did not return an int");
-        });
+        }
     }
 
     wcmp = Sk.abstr.lookupSpecial(w, "__cmp__");
     if (wcmp) {
         // note, flipped on return value and call
-        return Sk.misceval._tryCatch(function() {
+        try {
             ret = Sk.misceval.callsim(wcmp, w, v);
             if (Sk.builtin.checkNumber(ret)) {
                 ret = Sk.builtin.asnum$(ret);
@@ -508,9 +499,9 @@ Sk.misceval.richCompareBool = function (v, w, op, canSuspend) {
             if (ret !== Sk.builtin.NotImplemented.NotImplemented$) {
                 throw new Sk.builtin.TypeError("comparison did not return an int");
             }
-        }, function (e) {
+        } catch (e) {
             throw new Sk.builtin.TypeError("comparison did not return an int");
-        });
+        }
     }
 
     // handle special cases for comparing None with None or Bool with Bool
