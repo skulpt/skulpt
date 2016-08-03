c77b84abd28bf05d66fca7ae7c1cc2381733dd63
Brad Miller <bonelake@mac.com>
2016-08-03 11:45:25 -0500

    add pure python staticmethod and tests

---
+++ src/staticmethod.py
@@ -0,0 +1,8 @@
+class staticmethod(object):
+    "Emulate PyStaticMethod_Type() in Objects/funcobject.c"
+
+    def __init__(self, f):
+        self.f = f
+
+    def __get__(self, obj, objtype=None):
+        return self.f
