ae0e47952e784818a7f7c20e1807c1b7d587caad
OPiMedia <olivier_pirson_opi@yahoo.fr>
2016-08-17 12:53:29 +0200

    Corrected 2nd part Bug #620: lshift of 0 should not become long

--- test/unit/test_int.py
+++ test/unit/test_int.py
@@ -426,9 +426,16 @@ class IntTestCases(unittest.TestCase):
 
     def test_lshift_type(self):
         # Bug #620: lshift of 0 should not become long
+        # 0 << 0
         x = 0 << 0
         self.assertEqual(x, 0)
         self.assertIsInstance(x, int)
+
+        x = 0L << 0
+        self.assertEqual(x, 0L)
+        self.assertIsInstance(x, long)
+
+        # 0 <<        
         x = 0 << 1
         self.assertEqual(x, 0)
         self.assertIsInstance(x, int)
@@ -436,9 +443,6 @@ class IntTestCases(unittest.TestCase):
         self.assertEqual(x, 0)
         self.assertIsInstance(x, int)
 
-        x = 0L << 0
-        self.assertEqual(x, 0L)
-        self.assertIsInstance(x, long)
         x = 0L << 1
         self.assertEqual(x, 0L)
         self.assertIsInstance(x, long)
@@ -446,6 +450,15 @@ class IntTestCases(unittest.TestCase):
         self.assertEqual(x, 0L)
         self.assertIsInstance(x, long)
 
+        # << 0
+        x = 1 << 0
+        self.assertEqual(x, 1)
+        self.assertIsInstance(x, int)
+
+        x = 1L << 0
+        self.assertEqual(x, 1L)
+        self.assertIsInstance(x, long)
+        
 class IntTest(unittest.TestCase):
     def test_int_inherited(self):
         class c:
