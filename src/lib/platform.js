41c8acec26643cf39e62676d87eaf8f810ae98b2
Scott Rixner <rixner@rice.edu>
2017-07-07 16:24:59 -0500

    Add semi-colons and minor cleanup.

--- src/lib/platform.js
+++ src/lib/platform.js
@@ -1,18 +1,25 @@
 var $builtinmodule = function(name){
     var mod = {};
     var inBrowser = (typeof window != "undefined") && (typeof window.navigator != "undefined");
+
     mod.python_implementation = new Sk.builtin.func(function() {
-        // This checks that the correct number of arguments were passed
         Sk.builtin.pyCheckArgs("python_implementation", arguments, 0, 0);
         return new Sk.builtin.str("Skulpt");
-    })
+    });
+
     mod.node = new Sk.builtin.func(function() {
         Sk.builtin.pyCheckArgs("node", arguments, 0, 0);
         return new Sk.builtin.str("");
-    })
+    });
+
     mod.version = new Sk.builtin.func(function() {
-        var vers;
         Sk.builtin.pyCheckArgs("version", arguments, 0, 0);
+        return new Sk.builtin.str("");
+    });
+
+    mod.python_version = new Sk.builtin.func(function() {
+        var vers;
+        Sk.builtin.pyCheckArgs("python_version", arguments, 0, 0);
         if (Sk.python3) {
             vers = "3.2.0";
         }
@@ -20,7 +27,8 @@ var $builtinmodule = function(name){
             vers = "2.7.0";
         }
         return new Sk.builtin.str(vers);
-    })
+    });
+
     mod.system = new Sk.builtin.func(function() {
         var sys;
         Sk.builtin.pyCheckArgs("system", arguments, 0, 0);
@@ -31,7 +39,8 @@ var $builtinmodule = function(name){
             sys = "";
         }
         return new Sk.builtin.str(sys);
-    })
+    });
+
     mod.machine = new Sk.builtin.func(function() {
         var plat;
         Sk.builtin.pyCheckArgs("machine", arguments, 0, 0);
@@ -42,7 +51,8 @@ var $builtinmodule = function(name){
             plat = "";
         }
         return new Sk.builtin.str(plat);
-    })
+    });
+
     mod.release = new Sk.builtin.func(function() {
         var appVers;
         Sk.builtin.pyCheckArgs("release", arguments, 0, 0);
@@ -53,20 +63,18 @@ var $builtinmodule = function(name){
             appVers = "";
         }
         return new Sk.builtin.str(appVers);
-    })
+    });
+
     mod.architecture = new Sk.builtin.func(function() {
         Sk.builtin.pyCheckArgs("architecture", arguments, 0, 0);
-        return new Sk.builtin.str("('64bit', '')");
-    })
-//    mod.processor = new Sk.builtin.func(function() {
-//        Sk.builtin.pyCheckArgs("processor", arguments, 0, 0);
-//        //implementation
-//        return new Sk.builtin.str(...);
-//    })
-//    mod.uname = new Sk.builtin.func(function() {
-//        Sk.builtin.pyCheckArgs("uname", arguments, 0, 0);
-//       //implementation
-//       return new Sk.builtin.str(...);
-//    })
+        return new Sk.builtin.tuple([new Sk.builtin.str("64bit"),
+                                     new Sk.builtin.str("")]);
+    });
+
+    mod.processor = new Sk.builtin.func(function() {
+        Sk.builtin.pyCheckArgs("processor", arguments, 0, 0);
+        return new Sk.builtin.str("");
+    });
+
     return mod;
 };
