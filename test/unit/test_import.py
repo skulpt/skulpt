0c8a491223adfe2f28fe5b3d92d15a3dad7653cf
Meredydd Luff <meredydd@senatehouse.org>
2017-09-29 14:52:49 +0100

    Test to demonstrate that Python2 relative imports don't work

--- test/unit/test_import.py
+++ test/unit/test_import.py
@@ -26,5 +26,19 @@ class ImportFromModulesTest(unittest.TestCase):
         from unittest import gui as mystuff
         self.assertTrue(mystuff is not None)
 
+
+class ImportRelativeTest(unittest.TestCase):
+
+    def test_relative_import(self):
+        import importable_module
+
+        self.assertEqual(importable_module.version, "toplevel")
+
+        import subpackage
+
+        self.assertEqual(subpackage.imported_version, "subpackage")
+
+
+
 if __name__ == '__main__':
     unittest.main()
