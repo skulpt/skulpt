5d96b1fcf6675fb4903c97c053262b79db1ae9ee
Albert-Jan Nijburg <albertjan@skimlinks.com>
2017-10-29 21:21:35 +0000

    inject __class__ cell

--- src/compile.js
+++ src/compile.js
@@ -531,7 +531,7 @@ Compiler.prototype.ccall = function (e) {
             // so we should probably add self to the mangling
             // TODO: feel free to ignore the above
             out("if (typeof self === \"undefined\" || self.toString().indexOf(\"Window\") > 0) { throw new Sk.builtin.RuntimeError(\"super(): no arguments\") };")
-            args.push("self.ob$type");
+            args.push("$cell.__class__");
             args.push("self");
         }
         out ("$ret = Sk.misceval.callsimOrSuspend(", func, args.length > 0 ? "," : "", args, ");");
@@ -1553,7 +1553,7 @@ Compiler.prototype.cfromimport = function (s) {
  * @returns the name of the newly created function or generator object.
  *
  */
-Compiler.prototype.buildcodeobj = function (n, coname, decorator_list, args, callback) {
+Compiler.prototype.buildcodeobj = function (n, coname, decorator_list, args, callback, class_for_super) {
     var containingHasFree;
     var frees;
     var argnamesarr;
@@ -1746,6 +1746,10 @@ Compiler.prototype.buildcodeobj = function (n, coname, decorator_list, args, cal
     //
     this.u.varDeclsCode += "}";
 
+    // inject __class__ cell
+    if (class_for_super) {
+        this.u.varDeclsCode += "$cell.__class__=this." + class_for_super.v + ";";
+    }
 
     //
     // finally, set up the block switch that the jump code expects
@@ -1864,13 +1868,13 @@ Compiler.prototype.buildcodeobj = function (n, coname, decorator_list, args, cal
     }
 };
 
-Compiler.prototype.cfunction = function (s) {
+Compiler.prototype.cfunction = function (s, class_for_super) {
     var funcorgen;
     goog.asserts.assert(s instanceof FunctionDef);
     funcorgen = this.buildcodeobj(s, s.name, s.decorator_list, s.args, function (scopename) {
         this.vseqstmt(s.body);
         out("return Sk.builtin.none.none$;"); // if we fall off the bottom, we want the ret to be None
-    });
+    }, class_for_super);
     this.nameop(s.name, Store, funcorgen);
 };
 
@@ -2026,9 +2030,7 @@ Compiler.prototype.cclass = function (s) {
 
     this.u.private_ = s.name;
 
-    this.annotateClassBodyForSuperCall(s.body);
-
-    this.cbody(s.body);
+    this.cbody(s.body, s.name);
     out("return;");
 
     // build class
@@ -2042,16 +2044,6 @@ Compiler.prototype.cclass = function (s) {
 
     // store our new class under the right name
     this.nameop(s.name, Store, wrapped);
-};
-
-Compiler.prototype.annotateClassBodyForSuperCall = function annotateClassBodyForSuperCall(s) {
-    var functionDefs = s.filter(function (d) {
-        return d instanceof FunctionDef;
-    });
-
-    var functionDefsContainingSuperCall = functionDefs.filter(function (d) {
-        d.body
-    })
 };
 
 Compiler.prototype.ccontinue = function (s) {
@@ -2086,7 +2078,7 @@ Compiler.prototype.cbreak = function (s) {
 /**
  * compiles a statement
  */
-Compiler.prototype.vstmt = function (s) {
+Compiler.prototype.vstmt = function (s, class_for_super) {
     var i;
     var val;
     var n;
@@ -2099,7 +2091,7 @@ Compiler.prototype.vstmt = function (s) {
         debugBlock = this.newBlock("debug breakpoint for line "+s.lineno);
         out("if (Sk.breakpoints('"+this.filename+"',"+s.lineno+","+s.col_offset+")) {",
             "var $susp = $saveSuspension({data: {type: 'Sk.debug'}, resume: function() {}}, '"+this.filename+"',"+s.lineno+","+s.col_offset+");",
-            "$susp.$blk = "+debugBlock+";",
+            "$susp.$blk = " + debugBlock + ";",
             "$susp.optional = true;",
             "return $susp;",
             "}");
@@ -2112,7 +2104,7 @@ Compiler.prototype.vstmt = function (s) {
 
     switch (s.constructor) {
         case FunctionDef:
-            this.cfunction(s);
+            this.cfunction(s, class_for_super);
             break;
         case ClassDef:
             this.cclass(s);
@@ -2412,10 +2404,10 @@ Compiler.prototype.exitScope = function () {
     }
 };
 
-Compiler.prototype.cbody = function (stmts) {
+Compiler.prototype.cbody = function (stmts, class_for_super) {
     var i;
     for (i = 0; i < stmts.length; ++i) {
-        this.vstmt(stmts[i]);
+        this.vstmt(stmts[i], class_for_super);
     }
 };
 
@@ -2438,6 +2430,7 @@ Compiler.prototype.cprint = function (s) {
         out("Sk.misceval.print_(", /*dest, ',*/ "\"\\n\");");
     }
 };
+
 Compiler.prototype.cmod = function (mod) {
     //print("-----");
     //print(Sk.astDump(mod));
