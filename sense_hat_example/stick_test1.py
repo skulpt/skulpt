a2db0af3c1b8c3be055193566179a81c519ed5bf
Albert-Jan Nijburg <albert-jan.nijburg@babylonhealth.com>
2019-03-14 14:49:37 +0000

    remove special sense_hat module

--- sense_hat_example/stick_test1.py
+++
@@ -1,9 +0,0 @@
-from sense_hat import SenseHat
-from time import sleep
-
-sense = SenseHat()
-event = sense.stick.wait_for_event()
-print("The joystick was {} {}".format(event.action, event.direction))
-sleep(0.1)
-event = sense.stick.wait_for_event()
-print("The joystick was {} {}".format(event.action, event.direction))
