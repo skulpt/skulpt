607db5fc711a305e2a677291641651765a694937
Scott Rixner <rixner@rice.edu>
2019-05-22 17:06:50 -0500

    Minor cleanup.

--- test/testunit.js
+++ test/testunit.js
@@ -5,6 +5,7 @@ const path = require('path');
 require('../dist/' + process.argv[2]);
 Sk.js_beautify = require('js-beautify').js;
 
+// Setup for appropriate Python version
 var dir, pyver;
 
 if (process.argv[3] == "python2") {
@@ -15,30 +16,31 @@ if (process.argv[3] == "python2") {
     pyver = Sk.python3;
 }
 
-// Run each unit test
-var files = fs.readdirSync(dir);
 var passTot = 0;
 var failTot = 0;
-var input, buf, found;
+var buf, found;
 
 var regexp = /.*Ran.*passed:\s+(\d+)\s+failed:\s+(\d+)/g;
 
+// Configure Skulpt to run unit tests
+Sk.configure({
+    syspath: [dir],
+    read: (fname) => { return fs.readFileSync(fname, "utf8"); },
+    output: (args) => { buf += args; },
+    __future__: pyver
+});
+
+// Test each existing unit test file
+var files = fs.readdirSync(dir);
 files.forEach((file) => {
     let fullname = dir + "/" + file;
     let stat = fs.statSync(fullname);
 
     if (stat.isFile() && (path.extname(file) == "\.py")) {
-	input = fs.readFileSync(fullname, "utf8");
 	buf = "";
 	console.log(fullname);
 
-	Sk.configure({
-	    syspath: [dir],
-	    read: (fname) => { return fs.readFileSync(fname, "utf8"); },
-	    output: (args) => { buf += args; },
-	    __future__: pyver
-	});
-
+	// Run Skulpt
 	Sk.misceval.asyncToPromise(function() {
 	    return Sk.importMain(path.basename(file, ".py"), false, true);
 	}).then(function () {}, function(e) {
@@ -48,8 +50,10 @@ files.forEach((file) => {
 	    process.exit(1);
 	});
 
+	// Print results
 	console.log(buf);
-	
+
+	// Update results
 	while ((found = regexp.exec(buf)) !== null) {
 	    passTot += parseInt(found[1]);
 	    failTot += parseInt(found[2]);
