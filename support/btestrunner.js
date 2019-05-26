eb7328df017f087065147e25c9e31c98c137d54d
Scott Rixner <rixner@rice.edu>
2019-05-26 11:49:40 -0500

    Cleanup.

--- support/btestrunner.js
+++ support/btestrunner.js
@@ -11,99 +11,85 @@ function rununits(myDiv, pyver) {
     var units = (pyver == "python2") ? Sk.unit2 : Sk.unit3;
 
     function outf (text) {
-	mypre.appendChild(document.createTextNode(text));
-	window.scrollTo(0, document.body.scrollHeight);
-    }
-    
-    function showjs (text) {
-	var mypre = document.getElementById("runbrowser_jsout");
-	mypre.innerHTML = '';
-	mypre.appendChild(document.createTextNode(text));
+        mypre.appendChild(document.createTextNode(text));
+        window.scrollTo(0, document.body.scrollHeight);
     }
 
-    function myread (f) {
-	if (units["files"][f] !== undefined) {
-	    return units["files"][f];
-	}
+    function readf (f) {
+        if (units["files"][f] !== undefined) {
+            return units["files"][f];
+        }
 
-	if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][f] === undefined) {
+        if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][f] === undefined) {
             throw "File not found: '" + f + "'";
-	}
- 	
-	return Sk.builtinFiles["files"][f];
+        }
+
+        return Sk.builtinFiles["files"][f];
     };
 
-    function starttest (test, module) {
-    };
-
-    var passTot = 0;
-    var failTot = 0;
-    var regexp = /.*Ran.*passed:\s+(\d+)\s+failed:\s+(\d+)/g;
+    const regexp = /.*Ran.*passed:\s+(\d+)\s+failed:\s+(\d+)/g;
 
     Sk.buf = "";
-    
+
     Sk.configure({
-	syspath: [dir],
+        syspath: [dir],
         output: function (args) { Sk.buf += args; },
-        read: myread,
-        inputfunTakesPrompt: true,
-        debugout: showjs,
+        read: readf,
         debugging: false,
         __future__: pyver == "python2" ? Sk.python2 : Sk.python3
     });
 
     var modules = [];
-    
+
     for (var test in units["files"]) {
-	if (units["files"].hasOwnProperty(test)) {
-	    var lastslash = test.lastIndexOf('/');
-	    var module = test.substring(lastslash + 1, test.length - 3);
+        if (units["files"].hasOwnProperty(test)) {
+            var lastslash = test.lastIndexOf('/');
+            var module = test.substring(lastslash + 1, test.length - 3);
 
-	    modules.push([test, module]);
-	}	
-    }
-    
-    function runtest (index) {
-	var test = modules[index][0];
-	var module = modules[index][1];
-	
-	// Clear output buffer
-	Sk.buf = "";
-
-	// Print test name
-	outf(test + "\n");
-
-	// Run test
-	Sk.misceval.asyncToPromise(function() {
-	    return Sk.importMain(module, false, true);
-	}).then(function () {
-	    var found;
-	    
-	    // Print results
-	    outf(Sk.buf);
-	    
-	    // Update results
-	    while ((found = regexp.exec(Sk.buf)) !== null) {
-		passTot += parseInt(found[1]);
-		failTot += parseInt(found[2]);
-	    }	    
-	    
-	    // Let the browser update the DOM
-	    return new Promise(function (resolve) { setTimeout(resolve, 0); });
-	}).catch(function (e) {
-	    failTot += 1;
-	    outf("UNCAUGHT EXCEPTION: " + e);
-	    outf(e.stack);
-	}).then(function () {
-	    index += 1;
-	    if (index < modules.length) {
-		runtest(index);
-	    } else {
-		outf("Summary");
-		outf("Passed: " + passTot + " Failed: " + failTot);
-	    }
-	});
+            modules.push([test, module]);
+        }
     }
 
-    runtest(0);    
+    function runtest (tests, passed, failed) {
+        if (tests.length == 0) {
+            outf("Summary");
+            outf("Passed: " + passed + " Failed: " + failed);
+            return;
+        }
+
+        var test = tests.shift();
+
+        // Clear output buffer
+        Sk.buf = "";
+
+        // Print test name
+        outf(test[0] + "\n");
+
+        // Run test
+        Sk.misceval.asyncToPromise(function() {
+            return Sk.importMain(test[1], false, true);
+        }).then(function () {
+            var found;
+
+            // Print results
+            outf(Sk.buf);
+
+            // Update results
+            while ((found = regexp.exec(Sk.buf)) !== null) {
+                passed += parseInt(found[1]);
+                failed += parseInt(found[2]);
+            }
+
+            // Let the browser update the DOM
+            return new Promise(function (resolve) { setTimeout(resolve, 0); });
+        }).catch(function (err) {
+            failed += 1;
+            outf("UNCAUGHT EXCEPTION: " + err);
+            outf(err.stack);
+        }).then(function () {
+            runtest(tests, passed, failed)
+        });
+    }
+
+    runtest(modules, 0, 0);
 }
