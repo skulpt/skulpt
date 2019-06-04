a5e730ff69a2cb9ce153a10c72fc329d3c72410e
Scott Rixner <rixner@rice.edu>
2019-06-04 04:20:55 -0500

    Minify JS files in stdlib.

--- support/build/wrapmodules.js
+++ support/build/wrapmodules.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const uglifyjs = require('uglify-js');
 
-function processDirectories(dirs, recursive, exts, ret) {
+function processDirectories(dirs, recursive, exts, ret, minifyjs) {
     dirs.forEach((dir) => {
 	let files = fs.readdirSync(dir);
 
@@ -14,7 +15,15 @@ function processDirectories(dirs, recursive, exts, ret) {
 	    } else if (stat.isFile()) {
 		let ext = path.extname(file);
 		if (exts.includes(ext)) {
-		    ret.files[fullname] = fs.readFileSync(fullname, 'utf8');
+		    let contents = fs.readFileSync(fullname, 'utf8');
+		    if (minifyjs && (ext == ".js")) {
+			let result = uglifyjs.minify(contents);
+			if (result.error) {
+			    throw new Error("Error minimizing " + fullname + " (uglify js error code: " + result.error + ")");
+			}
+			contents = result.code;
+		    }
+		    ret.files[fullname] = contents;
 		}
 	    }
 	});
@@ -22,12 +31,12 @@ function processDirectories(dirs, recursive, exts, ret) {
 };
 
 
-function buildJsonFile(name, dirs, recursive, exts, outfile) {
+function buildJsonFile(name, dirs, recursive, exts, outfile, minifyjs) {
     var dir, file;
     let ret = {};
     ret.files = {};
 
-    processDirectories(dirs, recursive, exts, ret);
+    processDirectories(dirs, recursive, exts, ret, minifyjs);
 
     let contents = "Sk." + name + "=" + JSON.stringify(ret);
     fs.writeFileSync(outfile, contents, 'utf8');
@@ -35,11 +44,11 @@ function buildJsonFile(name, dirs, recursive, exts, outfile) {
 }
 
 if (process.argv.includes("internal")) {
-    buildJsonFile("internalPy", ["src"], false, [".py"], "src/internalpython.js");
+    buildJsonFile("internalPy", ["src"], false, [".py"], "src/internalpython.js", false);
 } else if (process.argv.includes("builtin")) {
-    buildJsonFile("builtinFiles", ["src/builtin", "src/lib"], true, [".js", ".py"], "dist/skulpt-stdlib.js");
+    buildJsonFile("builtinFiles", ["src/builtin", "src/lib"], true, [".js", ".py"], "dist/skulpt-stdlib.js", true);
 } else if (process.argv.includes("unit2")) {
-    buildJsonFile("unit2", ["test/unit"], true, [".py"], "support/tmp/unit2.js");
+    buildJsonFile("unit2", ["test/unit"], true, [".py"], "support/tmp/unit2.js", false);
 } else if (process.argv.includes("unit3")) {
-    buildJsonFile("unit3", ["test/unit3"], false, [".py"], "support/tmp/unit3.js");
+    buildJsonFile("unit3", ["test/unit3"], false, [".py"], "support/tmp/unit3.js", false);
 }
