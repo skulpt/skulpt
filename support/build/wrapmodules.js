0028730d74db0264db90f8dab0f3d90d0b1dcdba
Scott Rixner <rixner@rice.edu>
2019-06-04 04:52:07 -0500

    Easily allow libraries to be excluded to reduce skulpt-stdlib.js size.

--- support/build/wrapmodules.js
+++ support/build/wrapmodules.js
@@ -2,41 +2,51 @@ const fs = require('fs');
 const path = require('path');
 const uglifyjs = require('uglify-js');
 
-function processDirectories(dirs, recursive, exts, ret, minifyjs) {
+const excludeFileName = "libexcludes.json";
+
+function processDirectories(dirs, recursive, exts, ret, minifyjs, excludes) {
     dirs.forEach((dir) => {
-	let files = fs.readdirSync(dir);
+        let files = fs.readdirSync(dir);
 
-	files.forEach((file) => {
-	    let fullname = dir + '/' + file;
-	    let stat = fs.statSync(fullname)
+        files.forEach((file) => {
+            let fullname = dir + '/' + file;
 
-	    if (recursive && stat.isDirectory()) {
-		processDirectories([fullname], recursive, exts, ret);
-	    } else if (stat.isFile()) {
-		let ext = path.extname(file);
-		if (exts.includes(ext)) {
-		    let contents = fs.readFileSync(fullname, 'utf8');
-		    if (minifyjs && (ext == ".js")) {
-			let result = uglifyjs.minify(contents);
-			if (result.error) {
-			    throw new Error("Error minimizing " + fullname + " (uglify js error code: " + result.error + ")");
-			}
-			contents = result.code;
-		    }
-		    ret.files[fullname] = contents;
-		}
-	    }
-	});
+            if (!excludes.includes(fullname)) {
+                let stat = fs.statSync(fullname)
+
+                if (recursive && stat.isDirectory()) {
+                    processDirectories([fullname], recursive, exts, ret, minifyjs, excludes);
+                } else if (stat.isFile()) {
+                    let ext = path.extname(file);
+                    if (exts.includes(ext)) {
+                        let contents = fs.readFileSync(fullname, 'utf8');
+                        if (minifyjs && (ext == ".js")) {
+                            let result = uglifyjs.minify(contents);
+                            if (result.error) {
+                                throw new Error("Error minimizing " + fullname + " (uglify js error code: " + result.error + ")");
+                            }
+                            contents = result.code;
+                        }
+                        ret.files[fullname] = contents;
+                    }
+                }
+            }
+        });
     });
 };
 
 
-function buildJsonFile(name, dirs, recursive, exts, outfile, minifyjs) {
-    var dir, file;
+function buildJsonFile(name, dirs, exts, outfile, options) {
+    options = options || {};
+    let recursive = options.recursive || false;
+    let minifyjs = options.minifyjs || false;
+    let excludes = options.excludes || [];
+    let dir, file;
     let ret = {};
+
     ret.files = {};
 
-    processDirectories(dirs, recursive, exts, ret, minifyjs);
+    processDirectories(dirs, recursive, exts, ret, minifyjs, excludes);
 
     let contents = "Sk." + name + "=" + JSON.stringify(ret);
     fs.writeFileSync(outfile, contents, 'utf8');
@@ -44,11 +54,21 @@ function buildJsonFile(name, dirs, recursive, exts, outfile, minifyjs) {
 }
 
 if (process.argv.includes("internal")) {
-    buildJsonFile("internalPy", ["src"], false, [".py"], "src/internalpython.js", false);
+    buildJsonFile("internalPy", ["src"], [".py"], "src/internalpython.js");
 } else if (process.argv.includes("builtin")) {
-    buildJsonFile("builtinFiles", ["src/builtin", "src/lib"], true, [".js", ".py"], "dist/skulpt-stdlib.js", true);
+    let excludes = [];
+    if (fs.existsSync(excludeFileName)) {
+        excludes = JSON.parse(fs.readFileSync(excludeFileName));
+    }
+    let opts = {
+        recursive: true,
+        minifyjs: true,
+        excludes: excludes
+    };
+
+    buildJsonFile("builtinFiles", ["src/builtin", "src/lib"], [".js", ".py"], "dist/skulpt-stdlib.js", opts)
 } else if (process.argv.includes("unit2")) {
-    buildJsonFile("unit2", ["test/unit"], true, [".py"], "support/tmp/unit2.js", false);
+    buildJsonFile("unit2", ["test/unit"], [".py"], "support/tmp/unit2.js", { recursive: true });
 } else if (process.argv.includes("unit3")) {
-    buildJsonFile("unit3", ["test/unit3"], false, [".py"], "support/tmp/unit3.js", false);
+    buildJsonFile("unit3", ["test/unit3"], [".py"], "support/tmp/unit3.js");
 }
