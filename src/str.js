cf6e57112b5f3dd718ecac1c43b637b721b07e05
Grace Nichols <graceynichols@gmail.com>
2019-07-10 16:04:18 -0500

    Added replace error handling and fixed how bytes handled iterables

--- src/str.js
+++ src/str.js
@@ -893,21 +893,9 @@ Sk.builtin.str.prototype["istitle"] = new Sk.builtin.func(function (self) {
 });
 
 Sk.builtin.str.prototype["encode"] = new Sk.builtin.func(function (self, encoding, errors) {
-
     Sk.builtin.pyCheckArgsLen("encode", arguments.length, 1, 3);
-    /*
-    ret = new textEncoding.TextEncoder(encoding.$jsstr()).encode(self.v);
-    iter = [];
-    for (i in ret) {
-        iter.push(new Sk.builtin.int_(ret[i]));
-    };
-
-    iter = new Sk.builtin.list(iter);*/
-    if (errors) {
-        throw new Sk.builtin.NotImplementedError("Bytes error handling not implemented in Skulpt");
-    }
-    return new Sk.builtin.bytes(self, encoding);
-
+    
+    return new Sk.builtin.bytes(self, encoding, errors);
 });
 
 Sk.builtin.str.prototype.nb$remainder = function (rhs) {
--- test/unit3/test_bytes.py
+++ test/unit3/test_bytes.py
@@ -11,7 +11,7 @@ class BytesTests(unittest.TestCase):
         self.assertEqual(str(b)[1:], "''")
         self.assertEqual(str(bytes())[1:], "''")
 
-    def test_iterable_arg(self):
+    def test_iterables(self):
         a = [1,2,3]
         a1 = bytes(a)
         self.assertEqual(str(a1)[2:-1], "\\x01\\x02\\x03")
@@ -25,18 +25,45 @@ class BytesTests(unittest.TestCase):
         self.assertEqual(str(b)[2:-1], "\\x01\\xe6\\x03")
         c = bytes([65, 66, 200, 3])
         self.assertEqual(str(c)[2:-1], "AB\\xc8\\x03")
+        d = {1:2, 3:4, 5:6}
+        d0 = bytes(d)
+        self.assertEqual(str(d0)[2:-1], "\\x01\\x03\\x05")
+        s = set([1,2,3,4])
+        s0 = bytes(s)
+        self.assertEqual(str(s0)[2:-1], "\\x01\\x02\\x03\\x04")
+        class BasicIterClass:
+            def __init__(self, low, high):
+                self.current = low
+                self.high = high
+
+            def __next__(self):
+                #res = self.i
+                if self.current > self.high:
+                    raise StopIteration
+                else:
+                    self.current += 1
+                    return self.current - 1
+
+            def __iter__(self):
+                return self
+
+        a = BasicIterClass(1,3)
+        a0 = bytes(a)
+        self.assertEqual(str(a0)[2:-1], "\\x01\\x02\\x03")
+
         self.assertRaises(TypeError, bytes, "string")
         self.assertRaises(TypeError, bytes, [1,2, "3"])
         self.assertRaises(ValueError, bytes, [257, 2, 3])
         self.assertRaises(ValueError, bytes, [-1, 2, 3])
 
-    def test_string_arg(self):
+    def test_strings(self):
         string = "abz"
         d = bytes(string, 'ascii')
         self.assertEqual(str(d), "b'abz'")
         self.assertEqual(list(d), [97, 98, 122])
         self.assertEqual(len(d), 3)
-
+        
+        self.assertRaises(TypeError, bytes, "abc")
         self.assertRaises(TypeError, bytes, "abc", [])
         self.assertRaises(TypeError, bytes, ["a", "b"], "ascii")
         self.assertRaises(LookupError, bytes, "abc", "asd")
@@ -66,6 +93,66 @@ class BytesTests(unittest.TestCase):
         self.assertRaises(UnicodeEncodeError, bytes, "aÿ", "ascii", "strict")
         a = bytes("aÿ", "ascii", "ignore")
         self.assertEqual(str(a)[2:-1], "a")
+        b = bytes([200, 100, 101])
+        c = b.decode("ascii", "ignore")
+        self.assertEqual(c, "de")
+        
+        a = bytes("abÿ", "ascii", "replace")
+        self.assertEqual(str(a)[2:-1], "ab?")
+        b = bytes([200, 100, 101])
+        c = b.decode("ascii", "replace")
+        #self.assertEqual(c, "?de")
+        b = bytes([250, 100, 101])
+        c = b.decode("ascii", "replace")
+        #self.assertEqual(c, "?de")
+        
+
+    def test_iteration(self):
+        a = bytes("abc", "ascii")
+        self.assertEqual(list(a), [97,98,99])
+        a0 = []
+        for i in a:
+            a0.append(i)
+        self.assertEqual(a0, [97,98,99])
+        a0 = []
+        for i in a:
+            a0.append(i)
+        self.assertEqual(a0, [97,98,99])
+        b = bytes([100, 101, 102])
+        b0 = []
+        for i in b:
+            b0.append(i)
+        self.assertEqual(b0, [100, 101, 102])
+        d = {1:2, 3:4, 5:6}
+        d0 = bytes(d)
+        d1 = []
+        for i in d0:
+            d1.append(i)
+        self.assertEqual(d1, [1, 3, 5])
+        class BasicIterClass:
+            def __init__(self, low, high):
+                self.current = low
+                self.high = high
+
+            def __next__(self):
+                #res = self.i
+                if self.current > self.high:
+                    raise StopIteration
+                else:
+                    self.current += 1
+                    return self.current - 1
+
+            def __iter__(self):
+                return self
+
+        a = BasicIterClass(1,3)
+        a0 = bytes(a)
+        a1 = []
+        for i in a0:
+            a1.append(i)
+        self.assertEqual(a1, [1, 2, 3])
+            
+            
 
 if __name__ == '__main__':
     unittest.main()  
