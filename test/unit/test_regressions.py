skulpt/skulpt

commit 285ba1cf86a0457ef0ca083f9d37a29dc8347d14

Python 3 doesn't support comparisons between different built-in types

src/str.js
@@ -156,7 +156,11 @@ Sk.builtin.str.prototype.tp$iter = function () {
 
 Sk.builtin.str.prototype.tp$richcompare = function (other, op) {
     if (!(other instanceof Sk.builtin.str)) {
-        return undefined;
+        if (Sk.__future__.python3) {
+            return Sk.builtin.NotImplemented.NotImplemented$;
+        } else {
+            return false;
+        }
     }
 
     switch (op) {

test/unit3/test_compare.py
@@ -195,6 +195,11 @@ class ComparisonTest(unittest.TestCase):
             def __init__(self,x): self.x = x
             def __cmp__(self,other): return self.x
         self.assertRaises(TypeError, helper, A(-1), A(1), -1)
+
+        # Built-in type comparisons should no longer work in python 3
+        self.assertRaises(TypeError, lambda: (1,2) > [3,4])
+        self.assertRaises(TypeError, lambda: None > (1,2))
+        self.assertRaises(TypeError, lambda: 2 > "2")
 ##
 if __name__ == '__main__':
     unittest.main()

src/list.js
@@ -171,6 +171,9 @@ Sk.builtin.list.prototype.tp$richcompare = function (w, op) {
             return true;
         }
 
+        if (Sk.__future__.python3) {
+            return Sk.builtin.NotImplemented.NotImplemented$;
+        }
         // todo; other types should have an arbitrary order
         return false;
     }

src/misceval.js
@@ -211,6 +211,19 @@ Sk.misceval.swappedOp_ = {
     "NotIn": "In_"
 };
 
+Sk.misceval.opSymbols = {
+    "Eq"   : "==",
+    "NotEq": "!=",
+    "Lt"   : "<",
+    "LtE"  : "<=",
+    "Gt"   : ">",
+    "GtE"  : ">=",
+    "Is"   : "is",
+    "IsNot": "is not",
+    "In_"  : "in",
+    "NotIn": "not in"
+};
+
 /**
 * @param{*} v
 * @param{*} w
@@ -247,10 +260,11 @@ Sk.misceval.richCompareBool = function (v, w, op, canSuspend) {
     v_type = new Sk.builtin.type(v);
     w_type = new Sk.builtin.type(w);
 
-    // Python has specific rules when comparing two different builtin types
+    // Python 2 has specific rules when comparing two different builtin types
     // currently, this code will execute even if the objects are not builtin types
     // but will fall through and not return anything in this section
-    if ((v_type !== w_type) &&
+    if (!Sk.__future__.python3 &&
+        (v_type !== w_type) &&
         (op === "GtE" || op === "Gt" || op === "LtE" || op === "Lt")) {
         // note: sets are omitted here because they can only be compared to other sets
         numeric_types = [Sk.builtin.float_.prototype.ob$type,
@@ -546,7 +560,7 @@ Sk.misceval.richCompareBool = function (v, w, op, canSuspend) {
 
     vname = Sk.abstr.typeName(v);
     wname = Sk.abstr.typeName(w);
-    throw new Sk.builtin.TypeError("'" + "OPERATION SYMBOL" + "' not supported between instances of '" + vname + "' and '" + wname + "'");
+    throw new Sk.builtin.TypeError("'" + Sk.misceval.opSymbols[op] + "' not supported between instances of '" + vname + "' and '" + wname + "'");
     //throw new Sk.builtin.ValueError("don't know how to compare '" + vname + "' and '" + wname + "'");
 };
 Sk.exportSymbol("Sk.misceval.richCompareBool", Sk.misceval.richCompareBool);

src/slice.js
@@ -60,6 +60,9 @@ Sk.builtin.slice.prototype.tp$richcompare = function (w, op) {
             return true;
         }
 
+        if (Sk.__future__.python3) {
+            return Sk.builtin.NotImplemented.NotImplemented$;
+        }
         // todo; other types should have an arbitrary order
         return false;
     }

src/tuple.js
@@ -148,6 +148,9 @@ Sk.builtin.tuple.prototype.tp$richcompare = function (w, op) {
             return true;
         }
 
+        if (Sk.__future__.python3) {
+            return Sk.builtin.NotImplemented.NotImplemented$;
+        }
         // todo; other types should have an arbitrary order
         return false;
     }

commit b4a414d5c6522dc4ded6aadcf835d7c3bd428380

Un-break string equality in Python 2 (broken in 285ba1cf86a0457ef0ca083f9d37a29dc8347d14)

src/str.js
@@ -156,11 +156,7 @@ Sk.builtin.str.prototype.tp$iter = function () {
 
 Sk.builtin.str.prototype.tp$richcompare = function (other, op) {
     if (!(other instanceof Sk.builtin.str)) {
-        if (Sk.__future__.python3) {
-            return Sk.builtin.NotImplemented.NotImplemented$;
-        } else {
-            return false;
-        }
+        return Sk.builtin.NotImplemented.NotImplemented$;
     }
 
     switch (op) {

test/unit/test_regressions.py
@@ -1,0 +1,16 @@
+"""Tests for bug regressions."""
+
+import unittest
+
+class RegressionTest(unittest.TestCase):
+    def test_string_equality(self):
+        self.assertTrue("1" == "1")
+        self.assertFalse("1" != "1")
+        self.assertFalse("1" == "2")
+        self.assertTrue("1" != "2")
+        self.assertFalse("1" == 1)
+        self.assertTrue("1" != 1)
+
+
+if __name__ == "__main__":
+    unittest.main()

test/unit3/test_compare.py
@@ -197,9 +197,18 @@ class ComparisonTest(unittest.TestCase):
         self.assertRaises(TypeError, helper, A(-1), A(1), -1)
 
         # Built-in type comparisons should no longer work in python 3
+        # (but equality should)
         self.assertRaises(TypeError, lambda: (1,2) > [3,4])
+        self.assertFalse((1,2) == [3,4])
+        self.assertTrue((1,2) != [3,4])
+
         self.assertRaises(TypeError, lambda: None > (1,2))
+        self.assertFalse(None == (1,2))
+        self.assertTrue(None != (1,2))
+
         self.assertRaises(TypeError, lambda: 2 > "2")
+        self.assertFalse(2 == "2")
+        self.assertTrue(2 != "2")
 ##
 if __name__ == '__main__':
     unittest.main()