224c584e18b5d11d5af9752613b7c2ec2b66ef22
mrcork <stucork@gmail.com>
2020-02-03 20:30:33 +0800

    Initial commit for itertools

---
+++ src/lib/itertools.js
@@ -0,0 +1,95 @@
+var $builtinmodule = function (name) {
+    var mod = {};
+
+
+    mod.accumulate = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("accumulate is not yet implemented in Skulpt");
+    })
+
+
+    mod.chain = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("chain is not yet implemented in Skulpt");
+    })
+
+
+    mod.combinations = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("combinations is not yet implemented in Skulpt");
+    })
+
+
+    mod.combinations_with_replacement = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("combinations_with_replacement is not yet implemented in Skulpt");
+    })
+
+
+    mod.compress = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("compress is not yet implemented in Skulpt");
+    })
+
+
+    mod.count = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("count is not yet implemented in Skulpt");
+    })
+
+
+    mod.cycle = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("cycle is not yet implemented in Skulpt");
+    })
+
+
+    mod.dropwhile = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("dropwhile is not yet implemented in Skulpt");
+    })
+
+
+    mod.filterfalse = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("filterfalse is not yet implemented in Skulpt");
+    })
+
+
+    mod.groupby = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("groupby is not yet implemented in Skulpt");
+    })
+
+
+    mod.islice = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("islice is not yet implemented in Skulpt");
+    })
+
+
+    mod.permutations = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("permutations is not yet implemented in Skulpt");
+    })
+
+
+    mod.product = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("product is not yet implemented in Skulpt");
+    })
+
+
+    mod.repeat = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("repeat is not yet implemented in Skulpt");
+    })
+
+
+    mod.starmap = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("starmap is not yet implemented in Skulpt");
+    })
+
+
+    mod.takewhile = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("takewhile is not yet implemented in Skulpt");
+    })
+
+
+    mod.tee = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("tee is not yet implemented in Skulpt");
+    })
+
+
+    mod.zip_longest = new Sk.builtin.func( function(){
+        throw new Sk.builtin.NotImplementedError("zip_longest is not yet implemented in Skulpt");
+    })
+
+    return mod;
+}
