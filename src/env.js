6f5d592f59e5410962b211c44244191cef086a54
alexwenbj <alexwenbj@users.noreply.github.com>
2020-02-14 23:08:52 +0800

    add frozenset

--- src/env.js
+++ src/env.js
@@ -35,6 +35,7 @@ Sk.python2 = {
     // skulpt specific
     python3: false,
     set_repr: false,
+    frozenset_repr: false,
     class_repr: false,
     inherit_from_object: false,
     super_args: false,
@@ -57,6 +58,7 @@ Sk.python3 = {
     // skulpt specific
     python3: true,
     set_repr: true,
+    frozenset_repr: true,
     class_repr: true,
     inherit_from_object: true,
     super_args: true,
@@ -107,6 +109,7 @@ Sk.configure = function (options) {
     Sk.bool_check(Sk.__future__.division, "Sk.__future__.division");
     Sk.bool_check(Sk.__future__.unicode_literals, "Sk.__future__.unicode_literals");
     Sk.bool_check(Sk.__future__.set_repr, "Sk.__future__.set_repr");
+    Sk.bool_check(Sk.__future__.frozenset_repr, "Sk.__future__.frozenset_repr");
     Sk.bool_check(Sk.__future__.class_repr, "Sk.__future__.class_repr");
     Sk.bool_check(Sk.__future__.inherit_from_object, "Sk.__future__.inherit_from_object");
     Sk.bool_check(Sk.__future__.super_args, "Sk.__future__.super_args");
