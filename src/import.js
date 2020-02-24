7574e8bd791dbca8238a8df591a3fa5ba75ff647
mrcork <stucork@gmail.com>
2020-02-24 15:43:59 +0800

    fix abstr skulpt classes being parents

--- src/import.js
+++ src/import.js
@@ -70,12 +70,14 @@ Sk.doOneTimeInitialization = function (canSuspend) {
     // Register a Python class with an internal dictionary, which allows it to
     // be subclassed
     var setUpClass = function (child) {
-        var parent = child.tp$base || child.prototype.tp$base;
+        var parent = child.prototype.tp$base;
         var bases = [];
         var base;
 
-        for (base = parent; base !== undefined; base = base.tp$base || base.prototype.tp$base) {
-            bases.push(base);
+        for (base = parent; base !== undefined; base = base.prototype.tp$base) {
+            if (!base.sk$abstract) {
+                bases.push(base);
+            }
         }
 
         child.tp$mro = new Sk.builtin.tuple([child]);
