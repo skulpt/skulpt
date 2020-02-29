05b72ea04a2251fdb0aff7178a09d30ed3826d2e
alexwenbj <alexwenbj@users.noreply.github.com>
2020-02-29 13:49:52 +0800

    add __contains__ test case

--- test/unit/test_strfunctions.py
+++ test/unit/test_strfunctions.py
@@ -338,6 +338,17 @@ class string_functions(unittest.TestCase):
         #                  "\nabc\ndef\r\nghi\n\r".splitlines(keepends=True))
 
         # self.assertRaises(TypeError, "abc".splitlines, 42, 42)
+        
+    def test___contains__(self):
+        self.assertTrue(''.__contains__(''))
+        self.assertTrue('abc'.__contains__(''))
+        self.checkequal(False, 'abc'.__contains__('\0'))
+        self.assertTrue('\0abc'.__contains__('\0'))
+        self.assertTrue('abc\0'.__contains__('\0'))
+        self.assertTrue('\0abc'.__contains__('a'))
+        self.assertTrue('asdf'.__contains__('asdf'))
+        self.checkequal(False, 'asd'.__contains__('asdf'))
+        self.checkequal(False, ''.__contains__('asdf'))
 
 if __name__ == "__main__":
     unittest.main()
