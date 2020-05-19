191b74ef8fe4ff4426f998afe8b60770bfd5221a
mrcork <stucork@gmail.com>
2020-05-19 14:31:51 +0800

    more additions to slot defs

--- src/slotdefs.js
+++ src/slotdefs.js
@@ -1,44 +1,105 @@
 // quick note to self
 
 // in multpile inheritance do the search thing
-// if we hit a slotwrapper then just call it's raw function rather than calling the slot wrapper!
+// if we hit a slotwrapper then just call it"s raw function rather than calling the slot wrapper!
 
 Sk.SlotDef = function (_name, func, wrapper, doc, flags) {
-    this._name = _name;
-    this.slot_func = func; // this function is called when the sk$klass defines a dunder method
-    this.doc = doc;
-    this.wrapper = wrapper;
-    this.flags = flags || {};
+    this.$name = this.dunder_name = _name;
+    this.$slot_func = func; // this function is called when the sk$klass defines a dunder method
+    this.$doc = doc;
+    this.$wrapper = wrapper;
+    this.$flags = flags || {};
+    this.$wrapper.$flags = this.$flags;
+};
+
+Sk.tpSlots = {
+    tp$str: true,
+    $r: true,
+    // tp$descr_get: true,
+    // tp$descr_set: true,
+    // tp$hash: true,
+    tp$init: true,
+    // tp$new: true,
+    // tp$iter: true,
+    // tp$iternext: true,
+};
+
+
+Sk.subSlots = {
+    tp$as_number: [
+        "nb$add",
+        "nb$inplace_add",
+        "nb$subtract",
+        "nb$inplace_subtract",
+        "nb$multiply",
+        "nb$inplace_multiply",
+        "nb$remainder",
+        "nb$inplace_remainder",
+        "nb$divmod",
+        "nb$power",
+        "nb$inplace_power",
+        "nb$negative",
+        "nb$positive",
+        "nb$absolute",
+        "nb$bool",
+        "nb$invert",
+        "nb$lshift",
+        "nb$inplace_lshift",
+        "nb$rshift",
+        "nb$inplace_rshift",
+        "nb$and",
+        "nb$inplace_and",
+        "nb$xor",
+        "nb$inplace_xor",
+        "nb$or",
+        "nb$inplace_or",
+        "nb$int",
+        "nb$reserved",
+    ],
+    tp$as_mapping: ["sq$length", "mp$subscript", "mp$ass_subscript"],
+    tp$as_sequence: [
+        "sq$length",
+        "sq$concat",
+        "sq$repeat",
+        "mp$subscript",
+        "mp$ass_subscript",
+        "sq$contains",
+        "sq$inplace_concat",
+        "sq$inplace_repeat"
+    ],
+
+    tp$rich_compare: ["ob$lt", "ob$le", "ob$eq", "ob$ne", "ob$gt", "ob$ge"],
+    tp$await: ["am$await",
+        "am$aiter",
+        "am$anext",
+    ],
 };
 
 Sk.Slots = {};
 
 Sk.Slots.$r = new Sk.SlotDef("__repr__",
-    function $r () {
-        let func = this.ob$type.$typeLookup("__repr__");
+    function $r() {
+        let res;
+        const func = this.ob$type.$typeLookup("__repr__");
         if (func instanceof Sk.builtin.wrapper_descriptor) {
             // then just call the wrapped function
             return func.d$wrapped.call(this);
         } else if (func !== undefined) {
-            const res = Sk.misceval.callsimArray(func, [this]);
+            res = Sk.misceval.callsimArray(func, [this]);
             if (!(Sk.builtin.checkString(res))) {
                 throw new Sk.builtin.TypeError("__repr__ returned non-string (type " + Sk.abstr.typeName(res) + ")")
             }
-            return res;
         }
+        return res;
     },
-    function __repr__(self) {
-        // this = wrapped function
-        return this.call(self);
-    },
-    "Return repr(self).", {
-        NoArgs: true
-    }
+    Sk.builtin.GenericSlotCallNoArgs,
+    "Return repr(self).", 
+    {NoArgs: true}
 );
 
 Sk.Slots.tp$init = new Sk.SlotDef("__init__",
     function tp$init(args, kwargs) {
-        let func = this.ob$type.$typeLookup("__init__");
+        const func = this.ob$type.$typeLookup("__init__");
         if (func instanceof Sk.builtin.wrapper_descriptor) {
             return func.d$wrapped.call(this, args, kwargs);
         }
@@ -56,6 +117,26 @@ Sk.Slots.tp$init = new Sk.SlotDef("__init__",
         // this = wrapped function
         return this.call(self, args, kwargs);
     }
+);
+
+Sk.Slots.tp$str = new Sk.SlotDef("__str__",
+    function tp$str () {
+        let res;
+        const func = this.ob$type.$typeLookup("__str__");
+        if (func instanceof Sk.builtin.wrapper_descriptor) {
+            return func.d$wrapped.call(this);
+        } else if (func !== undefined) {
+            res = Sk.misceval.callsimArray(func, [this]);
+            if (!(Sk.builtin.checkString(res))) {
+                throw new Sk.builtin.TypeError("__str__ returned non-string (type " + Sk.abstr.typeName(res) + ")")
+            }
+        }
+        return res;
+    },
+    Sk.builtin.GenericSlotCallNoArgs,
+    "Return str(self).", 
+    {NoArgs: true}
+
 );
 
 
@@ -86,16 +167,16 @@ Sk.Slots.tp$init = new Sk.SlotDef("__init__",
  *
  * Because many of these functions are used in contexts in which Skulpt does not
  * [yet] handle suspensions, the assumption is that they must not suspend. However,
- * some of these built-in functions are acquiring 'canSuspend' arguments to signal
+ * some of these built-in functions are acquiring "canSuspend" arguments to signal
  * where this is not the case. These need to be spliced out of the argument list before
  * it is passed to python. Array values in this map contain [dunderName, argumentIdx],
- * where argumentIdx specifies the index of the 'canSuspend' boolean argument.
+ * where argumentIdx specifies the index of the "canSuspend" boolean argument.
  *
  * @type {Object}
  */
 Sk.dunderToSkulpt = {
-    "__repr__" : "$r",
-    "__init__" : "tp$init",
+    "__repr__": "$r",
+    "__init__": "tp$init",
     "__eq__": "ob$eq",
     "__ne__": "ob$ne",
     "__lt__": "ob$lt",
@@ -152,49 +233,49 @@ Sk.setupDunderMethods = function (py3) {
 
 
 
-    // // allocate a dunder method to a skulpt slot
-    // const magic_func = klass.prototype[dunder];
-    // let skulpt_name = Sk.dunderToSkulpt[dunder];
+// // allocate a dunder method to a skulpt slot
+// const magic_func = klass.prototype[dunder];
+// let skulpt_name = Sk.dunderToSkulpt[dunder];
 
-    // if (typeof (skulpt_name) === "string") {
-    //     // can't suspend so just use calsimArray
-    //     klass.prototype[skulpt_name] = function () {
-    //         let len, args, i;
-    //         len = arguments.length;
-    //         args = new Array(len + 1);
-    //         args[0] = this;
-    //         for (i = 0; i < len; i++) {
-    //             args[i + 1] = arguments[i];
-    //         }
-    //         return Sk.misceval.callsimArray(magic_func, args);
-    //     };
-    // } else {
-    //     // can suspend
-    //     let canSuspendIdx = skulpt_name[1];
-    //     skulpt_name = skulpt_name[0];
-    //     klass.prototype[skulpt_name] = function () {
-    //         let len, args, i, j;
-    //         let canSuspend = false;
-    //         len = arguments.length;
-    //         if (canSuspendIdx <= len) {
-    //             args = new Array(len);
-    //         } else {
-    //             args = new Array(len + 1);
-    //         }
-    //         args[0] = this;
-    //         j = 1;
-    //         for (i = 0; i < len; i++) {
-    //             if (i === (canSuspendIdx - 1)) {
-    //                 canSuspend = arguments[i];
-    //             } else {
-    //                 args[j] = arguments[i];
-    //                 j += 1;
-    //             }
-    //         }
-    //         if (canSuspend) {
-    //             return Sk.misceval.callsimOrSuspendArray(magic_func, args);
-    //         } else {
-    //             return Sk.misceval.callsimArray(magic_func, args);
-    //         }
-    //     };
-    // }
+// if (typeof (skulpt_name) === "string") {
+//     // can"t suspend so just use calsimArray
+//     klass.prototype[skulpt_name] = function () {
+//         let len, args, i;
+//         len = arguments.length;
+//         args = new Array(len + 1);
+//         args[0] = this;
+//         for (i = 0; i < len; i++) {
+//             args[i + 1] = arguments[i];
+//         }
+//         return Sk.misceval.callsimArray(magic_func, args);
+//     };
+// } else {
+//     // can suspend
+//     let canSuspendIdx = skulpt_name[1];
+//     skulpt_name = skulpt_name[0];
+//     klass.prototype[skulpt_name] = function () {
+//         let len, args, i, j;
+//         let canSuspend = false;
+//         len = arguments.length;
+//         if (canSuspendIdx <= len) {
+//             args = new Array(len);
+//         } else {
+//             args = new Array(len + 1);
+//         }
+//         args[0] = this;
+//         j = 1;
+//         for (i = 0; i < len; i++) {
+//             if (i === (canSuspendIdx - 1)) {
+//                 canSuspend = arguments[i];
+//             } else {
+//                 args[j] = arguments[i];
+//                 j += 1;
+//             }
+//         }
+//         if (canSuspend) {
+//             return Sk.misceval.callsimOrSuspendArray(magic_func, args);
+//         } else {
+//             return Sk.misceval.callsimArray(magic_func, args);
+//         }
+//     };
+// }
