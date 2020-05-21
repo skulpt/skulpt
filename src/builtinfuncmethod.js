530c316616c209f858742284e1f0a0636dc3bf26
mrcork <stucork@gmail.com>
2020-05-21 17:21:08 +0800

    builtinfuncormethod with case keeping if

--- src/builtinfuncmethod.js
+++ src/builtinfuncmethod.js
@@ -5,19 +5,109 @@
  * @description
  * this function is used by all builtin functions or methods
  * the tp$call method is defined based on the flags
- *
- */
-Sk.builtin.builtinFuncOrMethod = function (jsFunc, flags, doc, self, module) {
-    this.m$raw = jsFunc;
-    this.m$flags = flags;
-    this.m$doc = doc;
-    this.m$self = self;
-    this.m$module = module;
-    this.m$name = jsFunc.name || jsFunc.$name || "<native JS>";
+ * 
+ * flags: {
+ * NoArgs: true, raises exception if there are args
+ * OneArg: true, raises exception if there are more than one Arg
+ * MinArgs: int
+ * MaxArgs
+ * NamedArgs: Array e.g. [null, null, "name1", "name2"]
+ *            use null for posonly args 
+ *            ensures that the total number of args equals the named args
+ *            applies Defaults if to any named args
+ * Defaults: Array (used in conjuntion with defaults)
+ * FastCall && NoKwargs: true, check NoKewords and pass args the function will handle these
+ * FastCall: pass args, kwargs the function will handle this
+ * 
+ * VarArgs: 
+ * VarArgs || KewWords:
+ * }
+ */ 
+Sk.builtin.builtinFuncOrMethod = {
+    constructor: function (jsFunc, flags, doc, self, module) {
+        this.$raw = jsFunc;
+        this.$flags = flags;
+        this.$doc = doc;
+        this.$self = self;
+        this.$module = module;
+        this.$name = jsFunc.name || jsFunc.$name || "<native JS>";
+
+        switch (flags) {
+            case flags.FastCall && flags.NoKwargs:
+                this.tp$call = function (args, kwargs) {
+                    Sk.abstr.NoKwargs(this.$name, kwargs);
+                    return this.$raw.call(this.$self, args);
+                } 
+                break;
+            case flags.FastCall:
+                this.tp$call = function (args, kwargs) {
+                    return this.$raw.call(this.$self, args, kwargs);
+                }
+                break;
+            case flags.NoArgs:
+                this.tp$call = function (args, kwargs) {
+                    Sk.abstr.noArgs(this.$name, args, kwargs);
+                    return this.$raw.call(this.$self);
+                }
+                break;
+            case flags.OneArg:
+                this.tp$call = function (args, kwargs) {
+                    Sk.abstr.OneArg(this.$name, args, kwargs);
+                    return this.$raw.call(this.$self);
+                }
+                break;
+            case flags.NamedArgs:
+                this.tp$call = function (args, kwargs) {
+                    args = Sk.abstr.copyKeywordsToNamedArgs(this.$name, this.$flags.NamedArgs, args, kwargs, this.$flags.Defaults);
+                    return this.$raw.call(this.$self, args);
+                }
+                break;
+            case flags.MinArgs:
+                this.tp$call = function (args, kwargs) {
+                    Sk.abstr.NoKwargs(this.$name, kwargs)
+                    args = Sk.abstr.checkArgsLen(this.$name, args, this.$flags.MinArgs, this.$flags.MaxArgs);
+                }
+                break;
+            default:
+                this.tp$call = Sk.builtin.func.prototype.tp$call;
+        }
+        if (flags.FastCall && flags.NoKwargs) {
+            this.tp$call = function (args, kwargs) {
+                Sk.abstr.NoKwargs(this.$name, kwargs);
+                return this.$raw.call(this.$self, args);
+            }
+        } else if (flags.FastCall) {
+            this.tp$call = function (args, kwargs) {
+                return this.$raw.call(this.$self, args, kwargs);
+            }
+        } else if (flags.NoArgs) {
+            this.tp$call = function (args, kwargs) {
+                Sk.abstr.noArgs(this.$name, args, kwargs);
+                return this.$raw.call(this.$self);
+            }
+        } else if (flags.OneArg) {
+            this.tp$call = function (args, kwargs) {
+                Sk.abstr.OneArg(this.$name, args, kwargs);
+                return this.$raw.call(this.$self);
+            }
+        } else if (flags.NamedArgs) {
+            this.tp$call = function (args, kwargs) {
+                args = Sk.abstr.copyKeywordsToNamedArgs(this.$name, this.$flags.NamedArgs, args, kwargs, this.$flags.Defaults);
+                return this.$raw.call(this.$self, args);
+            }
+        } else if (flags.MinArgs) {
+            this.tp$call = function (args, kwargs) {
+                Sk.abstr.NoKwargs(this.$name, kwargs)
+                args = Sk.abstr.checkArgsLen(this.$name, args, this.$flags.MinArgs, this.$flags.MaxArgs);
+            }
+        }
+}
+
+
 };
 
-Sk.abstr.setUpInheritance("builtin_function_or_method", Sk.builtinFuncOrMethod, Sk.builtin.object);
-Sk.builtinFuncOrMethod.sk$acceptable_as_base_class = false;
+Sk.abstr.setUpInheritance("builtin_function_or_method", Sk.builtin.builtinFuncOrMethod, Sk.builtin.object);
+Sk.builtin.builtinFuncOrMethod.sk$acceptable_as_base_class = false;
 
 Sk.builtin.builtinFuncOrMethod.prototype.tp$call = function (args, kwargs) {
 
@@ -25,31 +115,31 @@ Sk.builtin.builtinFuncOrMethod.prototype.tp$call = function (args, kwargs) {
 };
 
 Sk.builtin.builtinFuncOrMethod.prototype.$r = function () {
-    if (this.m$self === undefined) {
-        return new Sk.builtin.str("<built-in function " + this.m$name + ">");
+    if (this.$self === undefined) {
+        return new Sk.builtin.str("<built-in function " + this.$name + ">");
     }
-    return new Sk.builtin.str("<built-in method "+ this.m$name + " of " + Sk.abstr.typeName(this) + " object>")
+    return new Sk.builtin.str("<built-in method "+ this.$name + " of " + Sk.abstr.typeName(this.$self) + " object>")
 };
 
 Sk.builtin.builtinFuncOrMethod.prototype.tp$getsets = [
     new Sk.GetSetDef("__module__",  
     function () {
-        return this.m$module ? new Sk.builtin.str(this.m$module) : Sk.builtin.none.none$;
+        return this.$module ? new Sk.builtin.str(this.$module) : Sk.builtin.none.none$;
     }
     ),
     new Sk.GetSetDef("__doc__",  
     function () {
-        return this.m$doc ? new Sk.builtin.str(this.m$doc) : Sk.builtin.none.none$;
+        return this.$doc ? new Sk.builtin.str(this.$doc) : Sk.builtin.none.none$;
     }
     ),
     new Sk.GetSetDef("__name__",
     function () {
-        return new Sk.builtin.str(this.m$name);
+        return new Sk.builtin.str(this.$name);
     }
     ),
     new Sk.GetSetDef("__self__", 
     function () {
-        return this.m$self;
+        return this.$self;
     }
     ),
     new Sk.GetSetDef("__text_signature__")
