09e37cac30ca2fee6d2b1829d1c8071f0da1e36a
mrcork <stucork@gmail.com>
2020-06-02 15:51:25 +0800

    range is sequence of mapping

--- src/range.js
+++ src/range.js
@@ -16,8 +16,10 @@ Sk.builtin.range_ = function (start, stop, step, lst) {
 
 Sk.abstr.setUpInheritance("range", Sk.builtin.range_, Sk.builtin.object);
 Sk.builtin.range_.sk$acceptable_as_base_class = false;
+Sk.builtin.range_.prototype.tp$as_sequence_or_mapping = true;
 
-Sk.builtin.range_.prototype.tp$doc = "range(stop) -> range object\nrange(start, stop[, step]) -> range object\n\nReturn an object that produces a sequence of integers from start (inclusive)\nto stop (exclusive) by step.  range(i, j) produces i, i+1, i+2, ..., j-1.\nstart defaults to 0, and stop is omitted!  range(4) produces 0, 1, 2, 3.\nThese are exactly the valid indices for a list of 4 elements.\nWhen step is given, it specifies the increment (or decrement).";
+Sk.builtin.range_.prototype.tp$doc =
+    "range(stop) -> range object\nrange(start, stop[, step]) -> range object\n\nReturn an object that produces a sequence of integers from start (inclusive)\nto stop (exclusive) by step.  range(i, j) produces i, i+1, i+2, ..., j-1.\nstart defaults to 0, and stop is omitted!  range(4) produces 0, 1, 2, 3.\nThese are exactly the valid indices for a list of 4 elements.\nWhen step is given, it specifies the increment (or decrement).";
 
 Sk.builtin.range_.prototype.tp$new = function (args, kwargs) {
     if (kwargs && kwargs.length) {
@@ -71,10 +73,6 @@ Sk.builtin.range_.prototype.mp$subscript = function (index) {
     return sub;
 };
 
-// Sk.builtin.range_.prototype.__getitem__ = new Sk.builtin.func(function (self, index) {
-//     return Sk.builtin.range_.prototype.mp$subscript.call(self, index);
-// });
-
 Sk.builtin.range_.prototype.sq$contains = function (item) {
     return this.v.sq$contains(item);
 };
@@ -93,40 +91,55 @@ Sk.builtin.range_.prototype.tp$richcompare = function (w, op) {
 Sk.builtin.range_.prototype.tp$iter = function () {
     // Hijack the list iterator
     const iter = this.v.tp$iter();
-    iter.$r = function () {
-        return new Sk.builtin.str("<rangeiterator>");
-    };
+    iter.tp$name = "range_iterator";
     return iter;
 };
 
-Sk.builtin.range_.prototype.__iter__ = new Sk.builtin.func(function (self) {
-    Sk.builtin.pyCheckArgsLen("__iter__", arguments.length, 1, 1);
-    return self.tp$iter();
-});
-
-Sk.builtin.range_.prototype.__contains__ = new Sk.builtin.func(function (self, item) {
-    Sk.builtin.pyCheckArgsLen("__contains__", arguments.length, 2, 2);
-    return new Sk.builtin.bool(self.sq$contains(item));
-});
-
-Sk.builtin.range_.prototype["index"] = new Sk.builtin.func(function (self, item, start, stop) {
-    Sk.builtin.pyCheckArgsLen("index", arguments.length, 2, 4);
-    return Sk.misceval.callsimArray(self.v.index, [self.v, item, start, stop]);
-});
-
-Sk.builtin.range_.prototype["count"] = new Sk.builtin.func(function (self, item) {
-    Sk.builtin.pyCheckArgsLen("count", arguments.length, 2, 2);
-    return Sk.misceval.callsimArray(self.v.count, [self.v, item]);
-});
-
 Sk.builtin.range_.prototype.tp$getsets = {
     start: {
-        $get: function () { return new Sk.builtin.int_(this.$start); }
+        $get: function () {
+            return new Sk.builtin.int_(this.$start);
+        },
     },
     step: {
-        $get: function () { return new Sk.builtin.int_(this.$step); }
+        $get: function () {
+            return new Sk.builtin.int_(this.$step);
+        },
     },
     stop: {
-        $get: function () { return new Sk.builtin.int_(this.$stop); }
-    }
+        $get: function () {
+            return new Sk.builtin.int_(this.$stop);
+        },
+    },
+};
+
+Sk.builtin.range_.prototype.tp$methods = {
+    // __reversed__: {
+    //     $meth: function () {
+    //         return Sk.misceval.callsimArray(this.v.__reversed__, []);
+    //     },
+    //     $flags:{NoArgs: true},
+    //     $textsig: null,
+    //     $doc: "Return a reverse iterator." },
+    // __reduce__: {
+    //     $meth: methods.__reduce__,
+    //     $flags:{},
+    //     $textsig: null,
+    //     $doc: "" },
+    count: {
+        $meth: function (item) {
+            return Sk.misceval.callsimArray(this.v.count, [this.v, item]);
+        },
+        $flags: { OneArg: true },
+        $textsig: null,
+        $doc: "rangeobject.count(value) -> integer -- return number of occurrences of value",
+    },
+    index: {
+        $meth: function (item) {
+            return Sk.misceval.callsimArray(this.v.index, [this.v, item]);
+        },
+        $flags: { OneArg: true },
+        $textsig: null,
+        $doc: "rangeobject.index(value, [start, [stop]]) -> integer -- return index of value.\nRaise ValueError if the value is not present.",
+    },
 };
