bccbd53e6a00fe2e154beb169d6b80af7f3b25bf
mrcork <stucork@gmail.com>
2020-06-03 09:14:03 +0800

    tidy mapping proxy

--- src/mappingproxy.js
+++ src/mappingproxy.js
@@ -6,32 +6,30 @@
  *
  * @param {Object} d
  *
+ * @description
+ * This should be called with the prototype of a type object
+ * It returns a mapping proxy
+ * useful for when we do typeobject.__dict__
+ * or module.__dict__ since a module $d is an object literal
+ *
  */
 
 Sk.builtin.mappingproxy = Sk.abstr.buildNativeClass("mappingproxy", {
-    constructor: function (d) {
+    constructor: function mappingproxy (d) {
+        Sk.asserts.assert(this instanceof Sk.builtin.mappingproxy, "bad call to mapping proxy, use 'new'");
         if (d === undefined) {
             d = {};
         }
-
-        if (!(this instanceof Sk.builtin.mappingproxy)) {
-            return new Sk.builtin.mappingproxy(d);
-        }
         // mappingproxy's v is a javascript object
         this.v = Object.create(null);
-
         // might be passed a dict but dict may not have been implemented yet!
-        if (Sk.abstr.typeName(d) === "dict") {
-            let k, it;
-            for (it = d.tp$iter(), k = it.tp$iternext(); k !== undefined; k = it.tp$iternext()) {
+        if (d instanceof Sk.builtin.dict) {
+            for (let it = d.tp$iter(), k = it.tp$iternext(); k !== undefined; k = it.tp$iternext()) {
                 let v = d.mp$subscript(k);
-                if (v === undefined) {
-                    v = null;
-                }
                 this.v[k.$jsstr()] = v;
             }
         } else {
-            let d_copy = { ...d };
+            let d_copy = { ...d }; // we make a shallow copy in order to ignore inherited attibutes from the prototype
             delete d_copy["constructor"];
 
             if (d === Sk.builtin.type.prototype) {
@@ -50,36 +48,22 @@ Sk.builtin.mappingproxy = Sk.abstr.buildNativeClass("mappingproxy", {
         }
     },
     slots: {
-        $tp$as_sequence_or_mapping: true,
+        tp$as_sequence_or_mapping: true,
+        tp$hash: Sk.builtin.none.none$,
         $r: function () {
-            let repr = [];
+            const bits = [];
             for (let k in this.v) {
-                repr.push("'" + k + "' : " + Sk.misceval.objectRepr(this.v[k]).$jsstr());
+                bits.push("'" + k + "' : " + Sk.misceval.objectRepr(this.v[k]).$jsstr());
             }
-            repr = "mappingproxy({" + repr.join(", ") + "}";
+            const repr = "mappingproxy({" + bits.join(", ") + "}";
             return new Sk.builtin.str(repr);
         },
-        mp$lookup: function (key) {
-            if (typeof key === "string") {
-                return this.v[Sk.unfixReserved(key)];
-            } else if (key instanceof Sk.builtin.str) {
-                return this.v[key.$jsstr()];
-            } else {
-                return undefined;
-            }
-        },
         mp$subscript: function (key) {
-            Sk.builtin.pyCheckArgsLen("[]", arguments.length, 1, 2, false, false);
-            var s;
-            var res = this.mp$lookup(key);
-
+            const res = this.mp$lookup(key);
             if (res !== undefined) {
-                // Found in dictionary
                 return res;
             } else {
-                // Not found in dictionary
-                s = new Sk.builtin.str(key);
-                throw new Sk.builtin.KeyError(s.v);
+                throw new Sk.builtin.KeyError(Sk.misceval.objectRepr(key).$jsstr());
             }
         },
         sq$contains: function (key) {
@@ -94,12 +78,24 @@ Sk.builtin.mappingproxy = Sk.abstr.buildNativeClass("mappingproxy", {
         },
     },
     proto: {
+        mp$lookup: function (key) {
+            if (typeof key === "string") {
+                return this.v[Sk.unfixReserved(key)];
+            } else if (Sk.builtin.checkString(key)) {
+                return this.v[key.$jsstr()];
+            } else {
+                return undefined;
+            }
+        },
         sk$asarray: function () {
             return Object.keys(this.v).map((x) => new Sk.builtin.str(x));
         },
         keys: Sk.builtin.dict.prototype.keys,
         items: Sk.builtin.dict.prototype.items,
         values: Sk.builtin.dict.prototype.values,
+    },
+    flags: {
+        sk$acceptable_as_base_type: false,
     },
 });
 
