588f0c36e05a7e58ef95f128ab7153292a3da07e
mrcork <stucork@gmail.com>
2020-06-04 17:43:34 +0800

    format sk_method

--- src/sk_method.js
+++ src/sk_method.js
@@ -1,36 +1,35 @@
 /**
  * @constructor
  * Sk.builtin.sk_method
- * 
+ *
  * @description
  * this constructor is used by all builtin functions or methods
  * the tp$call method is defined based on the flags
- * 
+ *
  * A good way to determine the flags is to look at the textsignature of a function
  * or find the equivalent function in CPython and map the flags to skulpt flags
  * flags: {
  * NoArgs: true, raises exception if there are args or kwargs (METH_NOARGS)
  * OneArg: true, raises exception if there is more than one Arg (METH_O)
- * 
+ *
  * MinArgs: int (also assumes noKwargs)
  * MaxArgs: int optional (used in conjuntiontion with MinArgs)
- * 
+ *
  * NamedArgs: Array e.g. [null, null, "name1", "name2"]
- *            use null for posonly args 
+ *            use null for posonly args
  *            ensures that the total number of args (including kwargs) equals the named args
  *            the call sig will check the kwarg names are valid
  *            the call sig applies Defaults (if set) to any named args
  * Defaults: Array (used in conjunction with NamedArgs, can use [undefined] see dict.pop for use case)
- * 
+ *
  * FastCall && NoKwargs: true, check NoKewords and pass args the function will handle these (METH_FASTCALL)
  * FastCall: pass args, kwargs - the function will handle this (METH_FASTCALL || KEYWORDS)
- * 
+ *
  * if no flags are set then the tp$call = function.prototype.tp$call
- * 
+ *
  */
 Sk.builtin.sk_method = Sk.abstr.buildNativeClass("builtin_function_or_method", {
     constructor: function (method_def, self, module) {
-
         // here we set this.$meth binding it's call signature to self
         this.$meth = method_def.$meth.bind(self);
         this.$doc = method_def.$doc;
@@ -93,33 +92,37 @@ Sk.builtin.sk_method = Sk.abstr.buildNativeClass("builtin_function_or_method", {
             return new Sk.builtin.str("<built-in method " + this.$name + " of " + Sk.abstr.typeName(this.$self) + " object>");
         },
         tp$call: function (args, kwargs) {
-            // default implementation for all currently created functions that have yet to be be converted 
+            // default implementation for all currently created functions that have yet to be be converted
             // and don't utilise flagged calls
             if (this.$self !== undefined) {
                 args.unshift(this.$self);
             }
             return Sk.generic.functionCallMethod.call(this, args, kwargs);
-        }
+        },
     },
     getsets: {
         __module__: {
-            $get: function () { return this.$module; },
-            $set: function (value) { this.$module = value; }
+            $get: function () {
+                return this.$module;
+            },
+            $set: function (value) {
+                this.$module = value;
+            },
         },
         __doc__: {
             $get: function () {
                 return this.$doc ? new Sk.builtin.str(this.$doc) : Sk.builtin.none.none$;
-            }
+            },
         },
         __name__: {
             $get: function () {
                 return new Sk.builtin.str(this.$name);
-            }
+            },
         },
         __text_signature__: {
             $get: function () {
                 return new Sk.builtin.str(this.$textsig);
-            }
-        }
-    }
+            },
+        },
+    },
 });
