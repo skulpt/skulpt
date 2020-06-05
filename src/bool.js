cb963cec3df601c33ef6d3cfd14784557f0a2224
mrcork <stucork@gmail.com>
2020-06-05 08:21:20 +0800

    bool with new api

--- src/bool.js
+++ src/bool.js
@@ -5,62 +5,64 @@
  * @description
  * Constructor for Python bool. Also used for builtin bool() function.
  *
- * Where possible, do not create a new instance but use the constants 
+ * Where possible, do not create a new instance but use the constants
  * Sk.builtin.bool.true$ or Sk.builtin.bool.false$. These are defined in src/constant.js
  *
  * @extends {Sk.builtin.object}
- * 
+ *
  * @param  {(Object|number|boolean)} x Value to evaluate as true or false
  * @return {Sk.builtin.bool} Sk.builtin.bool.true$ if x is true, Sk.builtin.bool.false$ otherwise
  */
-Sk.builtin.bool = function (x) {
-    if (Sk.misceval.isTrue(x)) {
-        return Sk.builtin.bool.true$;
-    } else {
-        return Sk.builtin.bool.false$;
-    }
-};
+Sk.builtin.bool = Sk.abstr.buildNativeClass("bool", {
+    constructor: function (x) {
+        if (Sk.misceval.isTrue(x)) {
+            return Sk.builtin.bool.true$;
+        } else {
+            return Sk.builtin.bool.false$;
+        }
+    },
+    slots: {
+        tp$doc:
+            "bool(x) -> bool\n\nReturns True when the argument x is true, False otherwise.\nThe builtins True and False are the only two instances of the class bool.\nThe class bool is a subclass of the class int, and cannot be subclassed.",
+        tp$new: function (args, kwargs) {
+            Sk.abstr.checkNoKwargs("bool", kwargs);
+            Sk.abstr.checkArgsLen("bool", args, 0, 1);
+            return new Sk.builtin.bool(args[0]); //technically we don't need new but easier to keep consistent
+        },
+        $r: function () {
+            return this.v ? new Sk.builtin.str("True") : new Sk.builtin.str("False");
+        },
+        // TODO: these should return bools if both bools else pass up to int/Long
+        // tp$as_number: true,
+        // nb$and: function () {
+        // },
+        // nb$or: function () {
+        // },
+        // nb$xor: function () {
+        // },
+    },
+    flags: {
+        sk$acceptable_as_base_class: false,
+    },
+});
+Sk.exportSymbol("Sk.builtin.bool", Sk.builtin.bool);
 
-Sk.abstr.setUpInheritance("bool", Sk.builtin.bool, Sk.builtin.int_);
+Sk.builtin.str.$emptystr = new Sk.builtin.str("");
 
-Sk.builtin.bool.sk$acceptable_as_base_class = false;
-Sk.builtin.bool.tp$as_number = true;
-
-Sk.builtin.bool.prototype.tp$doc = "bool(x) -> bool\n\nReturns True when the argument x is true, False otherwise.\nThe builtins True and False are the only two instances of the class bool.\nThe class bool is a subclass of the class int, and cannot be subclassed.";
-
-Sk.builtin.bool.prototype.tp$new = function (args, kwargs) {
-    Sk.abstr.checkNoKwargs("bool", kwargs);
-    Sk.abstr.checkArgsLen("bool", args, 0, 1);
-    return Sk.builtin.bool(args[0]);
-};
-
-Sk.builtin.bool.prototype["$r"] = function () {
-    if (this.v) {
-        return new Sk.builtin.str("True");
-    }
-    return new Sk.builtin.str("False");
-};
-
-Sk.builtin.bool.prototype.tp$str = function () {
-    return this.$r();
-};
-
-Sk.builtin.bool.prototype.tp$hash = function () {
-    return new Sk.builtin.int_(this.v);
-};
-
-// Sk.builtin.bool.prototype.__int__ = new Sk.builtin.func(function (self) {
-//     var v = Sk.builtin.asnum$(self);
-
-//     return new Sk.builtin.int_(v);
-// });
-
-// Sk.builtin.bool.prototype.__float__ = new Sk.builtin.func(function (self) {
-//     return new Sk.builtin.float_(Sk.ffi.remapToJs(self));
-// });
-
-Sk.builtin.bool.prototype.__format__ = new Sk.builtin.func(function (self) {
-    return self.$r();
+/**
+ * Python bool True constant.
+ * @type {Sk.builtin.bool}
+ * @memberOf Sk.builtin.bool
+ */
+Sk.builtin.bool.true$ = Object.create(Sk.builtin.bool.prototype, {
+    v: { value: 1, enumerable: true },
 });
 
-Sk.exportSymbol("Sk.builtin.bool", Sk.builtin.bool);
+/**
+ * Python bool False constant.
+ * @type {Sk.builtin.bool}
+ * @memberOf Sk.builtin.bool
+ */
+Sk.builtin.bool.false$ = Object.create(Sk.builtin.bool.prototype, {
+    v: { value: 0, enumerable: true },
+});
