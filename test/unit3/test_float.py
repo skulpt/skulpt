9aa02e7c5fe3a53eab9214ba2512a684bff73e9e
mrcork <stucork@gmail.com>
2020-06-10 19:50:06 +0800

    shouldn't raise overflow error with a string

--- test/unit3/test_float.py
+++ test/unit3/test_float.py
@@ -36,7 +36,6 @@ class FloatTestCases(unittest.TestCase):
     def test_overflow(self):
         self.assertRaises(OverflowError, float, 2**1024)
         self.assertRaises(OverflowError, float, -2**1024)
-        self.assertRaises(OverflowError, float, str(2**1024))
 
 if __name__ == '__main__':
     unittest.main()
