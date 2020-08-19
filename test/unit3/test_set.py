22847010038c4847897d77bdd9ade485482e4c86
mrcork <stucork@gmail.com>
2020-08-19 22:48:03 +0800

    skulpt doesn't treat set.update(dict) differently to other iterators

--- test/unit3/test_set.py
+++ test/unit3/test_set.py
@@ -333,24 +333,25 @@ class TestJointOps:
     #         fo.close()
     #         support.unlink(support.TESTFN)
 
-    def test_do_not_rehash_dict_keys(self):
-        n = 10
-        d = dict.fromkeys(map(HashCountingInt, range(n)))
-        self.assertEqual(sum(elem.hash_count for elem in d), n)
-        s = self.thetype(d)
-        self.assertEqual(sum(elem.hash_count for elem in d), n)
-        s.difference(d)
-        self.assertEqual(sum(elem.hash_count for elem in d), n)
-        if hasattr(s, 'symmetric_difference_update'):
-            s.symmetric_difference_update(d)
-        self.assertEqual(sum(elem.hash_count for elem in d), n)
-        d2 = dict.fromkeys(set(d))
-        self.assertEqual(sum(elem.hash_count for elem in d), n)
-        d3 = dict.fromkeys(frozenset(d))
-        self.assertEqual(sum(elem.hash_count for elem in d), n)
-        d3 = dict.fromkeys(frozenset(d), 123)
-        self.assertEqual(sum(elem.hash_count for elem in d), n)
-        self.assertEqual(d3, dict.fromkeys(d, 123))
+    # Skulpt doesn't treat dicts differently to other iterator types
+    # def test_do_not_rehash_dict_keys(self):
+    #     n = 10
+    #     d = dict.fromkeys(map(HashCountingInt, range(n)))
+    #     self.assertEqual(sum(elem.hash_count for elem in d), n)
+    #     s = self.thetype(d)
+    #     self.assertEqual(sum(elem.hash_count for elem in d), n)
+    #     s.difference(d)
+    #     self.assertEqual(sum(elem.hash_count for elem in d), n)
+    #     if hasattr(s, 'symmetric_difference_update'):
+    #         s.symmetric_difference_update(d)
+    #     self.assertEqual(sum(elem.hash_count for elem in d), n)
+    #     d2 = dict.fromkeys(set(d))
+    #     self.assertEqual(sum(elem.hash_count for elem in d), n)
+    #     d3 = dict.fromkeys(frozenset(d))
+    #     self.assertEqual(sum(elem.hash_count for elem in d), n)
+    #     d3 = dict.fromkeys(frozenset(d), 123)
+    #     self.assertEqual(sum(elem.hash_count for elem in d), n)
+    #     self.assertEqual(d3, dict.fromkeys(d, 123))
 
     # def test_container_iterator(self):
     #     # Bug #3680: tp_traverse was not implemented for set iterator object
