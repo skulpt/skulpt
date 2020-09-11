ae9b9371c43b883146f02219e716a109063fd9e9
mrcork <stucork@gmail.com>
2020-09-11 22:02:33 +0800

    not C - javascript

--- src/lib/operator.js
+++ src/lib/operator.js
@@ -7,7 +7,7 @@ function $builtinmodule(name) {
     operator = {
         __name__: new Sk.builtin.str("operator"),
         __doc__: new Sk.builtin.str(
-            "Operator interface.\n\nThis module exports a set of functions implemented in C corresponding\nto the intrinsic operators of Python.  For example, operator.add(x, y)\nis equivalent to the expression x+y.  The function names are those\nused for special methods; variants without leading and trailing\n'__' are also provided for convenience."
+            "Operator interface.\n\nThis module exports a set of functions implemented in javascript corresponding\nto the intrinsic operators of Python.  For example, operator.add(x, y)\nis equivalent to the expression x+y.  The function names are those\nused for special methods; variants without leading and trailing\n'__' are also provided for convenience."
         ),
         __all__: new Sk.builtin.list(
             [
