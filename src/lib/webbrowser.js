0c483343a70c09c733fbd1e8492159fee83d550a
archibate <1931127624@qq.com>
2020-09-12 22:47:10 +0800

    redirect open, open_new and open_new_tab to open_tab

--- src/lib/webbrowser.js
+++ src/lib/webbrowser.js
@@ -2,7 +2,7 @@ var $builtinmodule = function(name){
     var mod = {};
     var inBrowser = (typeof window != "undefined") && (typeof window.navigator != "undefined");
 
-    mod.open = new Sk.builtin.func(function open(url) {
+    function open_tab(url) {
         Sk.builtin.pyCheckType("url", "string", Sk.builtin.checkString(url));
         if (!inBrowser) {
             return Sk.builtin.bool.false$;
@@ -12,6 +12,22 @@ var $builtinmodule = function(name){
         window.open(url, "_blank");
 
         return Sk.builtin.bool.true$;
+    }
+
+    mod.open = new Sk.builtin.func(function open(url) {
+        Sk.builtin.pyCheckArgsLen("open", arguments.length + 1, 1, 3);
+        return open_tab(url);
     });
+
+    mod.open_new = new Sk.builtin.func(function open_new(url) {
+        Sk.builtin.pyCheckArgsLen("open_new", arguments.length + 1, 1, 3);
+        return open_tab(url);
+    });
+
+    mod.open_new_tab = new Sk.builtin.func(function open_new_tab(url) {
+        Sk.builtin.pyCheckArgsLen("open_new_tab", arguments.length + 1, 1, 3);
+        return open_tab(url);
+    });
+
     return mod;
 };
