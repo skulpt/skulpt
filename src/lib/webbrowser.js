10930cfab12b2444842ac68049bca23ffe4450b9
archibate <1931127624@qq.com>
2020-09-13 00:33:53 +0800

    implement mod.DefaultBrowser, as the return of mod.get

--- src/lib/webbrowser.js
+++ src/lib/webbrowser.js
@@ -20,19 +20,42 @@ var $builtinmodule = function(name){
     });
 
     mod.open_new = new Sk.builtin.func(function open_new(url) {
-        Sk.builtin.pyCheckArgsLen("open_new", arguments.length + 1, 1, 1);
+        Sk.builtin.pyCheckArgsLen("open_new", arguments.length, 1, 1);
         return open_tab(url);
     });
 
     mod.open_new_tab = new Sk.builtin.func(function open_new_tab(url) {
-        Sk.builtin.pyCheckArgsLen("open_new_tab", arguments.length + 1, 1, 1);
+        Sk.builtin.pyCheckArgsLen("open_new_tab", arguments.length, 1, 1);
         return open_tab(url);
     });
 
+    function dflbrowser($gbl, $loc) {
+        $loc.__init__ = new Sk.builtin.func(function __init__(self) {
+            return Sk.builtin.none.none$;
+        });
+
+        $loc.open = new Sk.builtin.func(function open(self, url) {
+            Sk.builtin.pyCheckArgsLen("open", arguments.length, 2, 4);
+            return open_tab(url);
+        });
+
+        $loc.open_new = new Sk.builtin.func(function open_new(self, url) {
+            Sk.builtin.pyCheckArgsLen("open_new", arguments.length, 2, 2);
+            return open_tab(url);
+        });
+
+        $loc.open_new_tab = new Sk.builtin.func(function open_new_tab(self, url) {
+            Sk.builtin.pyCheckArgsLen("open_new_tab", arguments.length, 2, 2);
+            return open_tab(url);
+        });
+    }
+
+    mod.DefaultBrowser = Sk.misceval.buildClass(mod, dflbrowser, "DefaultBrowser", []);
+
     mod.get = new Sk.builtin.func(function get() {
         Sk.builtin.pyCheckArgsLen("get", arguments.length, 0, 1);
-        return $builtinmodule;
-    }
+        return Sk.misceval.callsimArray(mod.DefaultBrowser, []);
+    });
 
     return mod;
 };
