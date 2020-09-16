506f8812ffc95209825fdaa54cc2c72ee6d83aee
Shreyas Minocha <sm-git@riseup.net>
2020-09-16 21:22:38 +0530

    Test values of underscore literals

--- test/unit3/test_grammar.py
+++ test/unit3/test_grammar.py
@@ -83,27 +83,27 @@ INVALID_UNDERSCORE_LITERALS = [
 
 class TokenTests(unittest.TestCase):
     def test_underscore_literals(self):
-        0_0_0
-        4_2
-        1_0000_0000
-        0b1001_0100
-        0xffff_ffff
-        0o5_7_7
-        1_00_00.5
-        1_00_00.5e5
-        1_00_00e5_1
-        1e1_0
-        .1_4
-        .1_4e1
-        0b_0
-        0x_f
-        0o_5
-        1_00_00j
-        1_00_00.5j
-        1_00_00e5_1j
-        .1_4j
-        (1_2.5+3_3j)
-        (.5_6j)
+        self.assertEqual(0_0_0, 0)
+        self.assertEqual(4_2, 42)
+        self.assertEqual(1_0000_0000, 100000000)
+        self.assertEqual(0b1001_0100, 0b10010100)
+        self.assertEqual(0xffff_ffff, 0xffffffff)
+        self.assertEqual(0o5_7_7, 0o577)
+        self.assertEqual(1_00_00.5, 10000.5)
+        self.assertEqual(1_00_00.5e5, 10000.5e5)
+        self.assertEqual(1_00_00e5_1, 10000e51)
+        self.assertEqual(1e1_0, 1e10)
+        self.assertEqual(.1_4, .14)
+        self.assertEqual(.1_4e1, .14e1)
+        self.assertEqual(0b_0, 0b0)
+        self.assertEqual(0x_f, 0xf)
+        self.assertEqual(0o_5, 0o5)
+        self.assertEqual(1_00_00j, 10000j)
+        self.assertEqual(1_00_00.5j, 10000.5j)
+        self.assertEqual(1_00_00e5_1j, 10000e51j)
+        self.assertEqual(.1_4j, .14j)
+        self.assertEqual((1_2.5+3_3j), (12.5+33j))
+        self.assertEqual((.5_6j), (.56j))
 
         eval_alt = "Sk.importMainWithBody('test_literals', false, '{0}')"
         for lit in INVALID_UNDERSCORE_LITERALS:
