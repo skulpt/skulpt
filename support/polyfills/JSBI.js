81d463aa35dda36da4890a79c77ba320f993c543
mrcork <stucork@gmail.com>
2020-09-23 12:06:06 +0800

    BigInt version of exponentiate and powermod can use symbols and can use ===

--- support/polyfills/JSBI.js
+++ support/polyfills/JSBI.js
@@ -1,14 +1,14 @@
 /**
  * functional approach to BigInt
- * 
+ *
  * We use the JSBI library if BigInt is not available
  * https://github.com/GoogleChromeLabs/jsbi/
- * 
+ *
  * If BigInt is available then we use the same functions as defined in the JSBI library
  * but use BigInt as the primitive type
- * 
+ *
  */
-const { default: __JSBI } = require("jsbi"); 
+const { default: __JSBI } = require("jsbi");
 // use jsbi which is es5 compliant - change to ES6 in the compilation version
 var globalThis = Sk.global;
 
@@ -18,6 +18,19 @@ const JSBI = globalThis.JSBI;
 if (globalThis.BigInt === undefined) {
     // __isBigInt is not part of the public api so include it if this is ever removed
     JSBI.__isBigInt = JSBI.__isBigInt || ((x) => x instanceof JSBI);
+    JSBI.powermod = (x, y, z) => {
+        const One = JSBI.BigInt(1);
+        let number = One;
+        y = JSBI.greaterThan(y, JSBI.__ZERO) ? y : JSBI.unaryMinus(y);
+        while (JSBI.greaterThan(y, JSBI.__ZERO)) {
+            if (JSBI.bitwiseAnd(y, One)) {
+                number = JSBI.remainder(JSBI.multiply(number, x), z);
+            }
+            y = JSBI.signedRightShift(y, One);
+            x = JSBI.remainder(JSBI.multiply(x, x), z);
+        }
+        return number;
+    };
 } else {
     Object.assign(JSBI, {
         BigInt: globalThis.BigInt,
@@ -30,18 +43,31 @@ if (globalThis.BigInt === undefined) {
         bitwiseOr: (x, y) => x | y,
         bitwiseXor: (x, y) => x ^ y,
         /**
-         * x**y would be better but closure compilere changes that to Math.pow 
+         * x**y would be better but closure compilere changes that to Math.pow
          * https://github.com/google/closure-compiler/issues/3684 */
         exponentiate: (x, y) => {
             const One = JSBI.BigInt(1);
             let number = One;
-            y = JSBI.greaterThan(y, JSBI.__ZERO) ?  y : JSBI.unaryMinus(y);
-            while (JSBI.greaterThan(y, JSBI.__ZERO)) {
-                if (JSBI.bitwiseAnd(y, One)) {
-                    number = JSBI.multiply(number, x);
+            y = y > JSBI.__ZERO ? y : -y;
+            while (y > JSBI.__ZERO) {
+                if (y & One) {
+                    number = number * x;
                 }
-                y = JSBI.signedRightShift(y, One);
-                x = JSBI.multiply(x, x);
+                y = y >> One;
+                x = x * x;
+            }
+            return number;
+        },
+        powermod: (x, y, z) => {
+            const One = JSBI.BigInt(1);
+            let number = One;
+            y = y > JSBI.__ZERO ? y : -y;
+            while (y > JSBI.__ZERO) {
+                if (y & One) {
+                    number = (number * x) % z;
+                }
+                y = y >> One;
+                x = (x * x) % z;
             }
             return number;
         },
@@ -57,25 +83,11 @@ if (globalThis.BigInt === undefined) {
         lessThanOrEqual: (x, y) => x <= y,
         greaterThan: (x, y) => x > y,
         greaterThanOrEqual: (x, y) => x >= y,
-        equal: (x, y) => x == y,
-        notEqual: (x, y) => x != y,
+        equal: (x, y) => x === y,
+        notEqual: (x, y) => x !== y,
     });
 }
 JSBI.__ZERO = JSBI.BigInt(0);
 JSBI.__MAX_SAFE = JSBI.BigInt(Number.MAX_SAFE_INTEGER);
 JSBI.__MIN_SAFE = JSBI.BigInt(-Number.MAX_SAFE_INTEGER);
-JSBI.numberIfSafe = (val) =>  JSBI.lessThan(val, JSBI.__MAX_SAFE) && JSBI.greaterThan(val, JSBI.__MIN_SAFE) ? JSBI.toNumber(val) : val;
-
-JSBI.powermod = (x, y, z) => {
-    const One = JSBI.BigInt(1);
-    let number = One;
-    y = JSBI.greaterThan(y, JSBI.__ZERO) ? y : JSBI.unaryMinus(y);
-    while (JSBI.greaterThan(y, JSBI.__ZERO)) {
-        if (JSBI.bitwiseAnd(y, One)) {
-            number = JSBI.remainder(JSBI.multiply(number, x), z);
-        }
-        y = JSBI.signedRightShift(y, One);
-        x = JSBI.remainder(JSBI.multiply(x, x), z);
-    }
-    return number;
-};
+JSBI.numberIfSafe = (val) => (JSBI.lessThan(val, JSBI.__MAX_SAFE) && JSBI.greaterThan(val, JSBI.__MIN_SAFE) ? JSBI.toNumber(val) : val);
