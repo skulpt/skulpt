7e207e4cbb286e327b98537db9dab855553cad34
bofeng <vonbo@me.com>
2020-10-28 10:46:12 -0400

    fix(builtin function): add raw_input function back to builtins for py2 mode as user may hot switch v

--- src/builtindict.js
+++ src/builtindict.js
@@ -137,6 +137,7 @@ Sk.setupObjects = function (py3) {
         Sk.builtins["StandardError"] = Sk.builtin.Exception;
         Sk.builtins["unicode"] = Sk.builtin.str;
         Sk.builtins["basestring"] = Sk.builtin.str;
+        Sk.builtins["raw_input"] = new Sk.builtin.func(Sk.builtin.raw_input);
         Sk.builtin.str.prototype.decode = Sk.builtin.str.$py2decode;
         delete Sk.builtins["bytes"];
         delete Sk.builtins["ascii"];
