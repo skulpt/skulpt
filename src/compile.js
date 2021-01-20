235db49fcc819d45320f027b3fd0d0bb5135408e
mrcork <stucork@gmail.com>
2021-01-20 21:55:16 +0800

    fix: keep compiler symbol counter private

--- src/compile.js
+++ src/compile.js
@@ -1,7 +1,8 @@
 /** @param {...*} x */
 var out;
 
-Sk.gensymcount = 0;
+// Keep the counter off Sk so compilation works when the public object is frozen.
+var gensymcount = 0;
 
 /**
  * @constructor
@@ -118,7 +119,7 @@ Compiler.prototype.annotateSource = function (ast) {
 Compiler.prototype.gensym = function (hint) {
     hint = hint || "";
     hint = "$" + hint;
-    hint += Sk.gensymcount++;
+    hint += gensymcount++;
     return hint;
 };
 
@@ -3087,7 +3088,7 @@ Sk.compile = function (source, filename, mode, canSuspend) {
 Sk.exportSymbol("Sk.compile", Sk.compile);
 
 Sk.resetCompiler = function () {
-    Sk.gensymcount = 0;
+    gensymcount = 0;
 };
 
 Sk.exportSymbol("Sk.resetCompiler", Sk.resetCompiler);
