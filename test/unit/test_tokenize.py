c87fbbf4ccb281cefddb84a3708cd27d1d0b9858
Tarjei Bærland <tarjeibaerland@gmail.com>
2021-02-04 10:25:28 +0100

    Failing tests for tokenization
    The following tests are now failing for tokenization:
    >>> være = 42
    >>> å = 11

--- test/unit/test_tokenize.py
+++ test/unit/test_tokenize.py
@@ -17,6 +17,7 @@ class TokenTests(unittest.TestCase):
 class TokenizeTests(unittest.TestCase):
     def test_tokenize(self):
         code = ["print('a', 1 < 2)"]
+
         def readline():
             if code:
                 return code.pop()
@@ -45,6 +46,82 @@ class TokenizeTests(unittest.TestCase):
         (token_type, token_string, start, end, line) = tokens[4]
         self.assertEqual(token_type, token.OP)
         self.assertEqual(token_string, ",")
+
+    def test_unicode(self):
+        code = ["å = 11", "være = 42", "vare = 43"]
+
+        def readline():
+            if code:
+                return code.pop()
+            else:
+                return ''
+
+        tokens = list(tokenize.tokenize(readline))
+
+        self.assertEqual(tokens[0][0], token.ENCODING)
+
+        token_type, token_string, start, end, line = tokens[1]
+        self.assertEqual(token_type, token.NAME)
+        self.assertEqual(token_string, "vare")
+        self.assertEqual(start, (1, 0))
+        self.assertEqual(end, (1, 4))
+        self.assertEqual(line, "vare = 43")
+
+        token_type, token_string, start, end, line = tokens[2]
+        self.assertEqual(token_type, token.OP)
+        self.assertEqual(token_string, "=")
+        self.assertEqual(start, (1, 5))
+        self.assertEqual(end, (1, 6))
+        self.assertEqual(line, "vare = 43")
+
+        token_type, token_string, start, end, line = tokens[3]
+        self.assertEqual(token_type, token.NUMBER)
+        self.assertEqual(token_string, "43")
+        self.assertEqual(start, (1, 7))
+        self.assertEqual(end, (1, 9))
+        self.assertEqual(line, "vare = 43")
+
+        token_type, token_string, start, end, line = tokens[4]
+        self.assertEqual(token_type, token.NAME)
+        self.assertEqual(token_string, "være")
+        self.assertEqual(start, (2,0))
+        self.assertEqual(end, (2, 4))
+        self.assertEqual(line, "være = 42")
+
+        token_type, token_string, start, end, line = tokens[5]
+        self.assertEqual(token_type, token.OP)
+        self.assertEqual(token_string, "=")
+        self.assertEqual(start, (2, 5))
+        self.assertEqual(end, (2, 6))
+        self.assertEqual(line, "være = 42")
+
+        token_type, token_string, start, end, line = tokens[6]
+        self.assertEqual(token_type, token.NUMBER)
+        self.assertEqual(token_string, "42")
+        self.assertEqual(start, (2, 7))
+        self.assertEqual(end, (2, 9))
+        self.assertEqual(line, "være = 43")
+
+        token_type, token_string, start, end, line = tokens[7]
+        self.assertEqual(token_type, token.NAME)
+        self.assertEqual(token_string, "å")
+        self.assertEqual(start, (3, 0))
+        self.assertEqual(end, (3, 1))
+        self.assertEqual(line, "å = 11")
+
+        token_type, token_string, start, end, line = tokens[8]
+        self.assertEqual(token_type, token.OP)
+        self.assertEqual(token_string, "=")
+        self.assertEqual(start, (3, 2))
+        self.assertEqual(end, (3, 3))
+        self.assertEqual(line, "å = 11")
+
+        token_type, token_string, start, end, line = tokens[9]
+        self.assertEqual(token_type, token.NUMBER)
+        self.assertEqual(token_string, "11")
+        self.assertEqual(start, (3, 4))
+        self.assertEqual(end, (3, 6))
+        self.assertEqual(line, "å = 11")
 
 if __name__ == '__main__':
     unittest.main()
