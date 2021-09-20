5a3a2c2a4e9efef7fe816ce24821b71ae37557c6
biomedtech <mozgunova.n240@gmail.com>
2021-09-20 13:46:59 +0200

    fix: add min args to lcm function flags

--- src/lib/math.js
+++ src/lib/math.js
@@ -994,7 +994,7 @@ const $builtinmodule = function (name) {
         },
         lcm: {
             $meth: lcm,
-            $flags: {},
+            $flags: { MinArgs: 0 },
             $textsig: "($module, *integers, /)",
             $doc: "Return the least common multiple of the specified integer arguments. If all arguments are nonzero, then the returned value is the smallest positive integer that is a multiple of all arguments. If any of the arguments is zero, then the returned value is 0. lcm() without arguments returns 1.",
         },
