917254b9076f41e21e5639a592db80ade2d14531
stu <stu@anvil.works>
2023-10-23 16:56:09 +0800

    update packages

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,21 @@ const shell = require("shelljs");
 const chalk = require("chalk");
 
 const ClosureWebpackPlugin = require("closure-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const GitRevisionPlugin = require("git-revision-webpack-plugin");
 const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 const git = new GitRevisionPlugin({ branch: true });
 
-const styleexcludes =
-    /(node_modules)|(support)|(gen)|(tokenize.js)|(symtable.js)|(compile.js)|(ast.js)|(internalpython.js)/;
+const styleexcludes = ["node_modules", "support", "gen", "*/tokenize.js", "*/symtable.js", "*/compile.js", "*/ast.js"];
 
 if (!shell.which("git")) {
     console.log(chalk.red("WARNING: Cannot find git!  Unsure if working directory is clean."));
 }
 
-var output = shell.exec("git diff-index --quiet HEAD");
+const output = shell.exec("git diff-index --quiet HEAD");
 if (output.code !== 0) {
     console.log(chalk.red("WARNING: Working directory is not clean."));
 } else {
@@ -26,13 +26,14 @@ if (output.code !== 0) {
 }
 
 module.exports = (env, argv) => {
-    var opt = {
+    let opt = {
         minimize: false,
     };
-    var outfile = "skulpt.js";
-    var assertfile = "./assert-dev.js";
-    var mod = {};
-    var languageOut = (env && env.languageOut) || "";
+    let outfile = "skulpt.js";
+    let assertfile = "./assert-dev.js";
+    const mod = {};
+    const languageOut = (env && env.languageOut) || "";
+    const plugins = [];
 
     if (argv.mode === "production") {
         opt = {
@@ -54,7 +55,7 @@ module.exports = (env, argv) => {
                             "visibility",
                         ],
                         jscomp_off: ["deprecated", "uselessCode", "suspiciousCode", "checkTypes"],
-                        languageOut: languageOut || "ECMASCRIPT_2015",
+                        languageOut: languageOut || "ECMASCRIPT_2022",
                         externs: "support/externs/sk.js",
                         rewritePolyfills: true,
                         // compiler flags here
@@ -71,19 +72,10 @@ module.exports = (env, argv) => {
         };
         outfile = "skulpt.min.js";
         assertfile = "./assert-prod.js";
-        mod = {
-            rules: [
-                {
-                    test: /\.js$/,
-                    enforce: "pre",
-                    exclude: styleexcludes,
-                    loader: "eslint-loader",
-                },
-            ],
-        };
+        plugins.push(new ESLintPlugin({ exclude: styleexcludes }));
     }
 
-    var config = {
+    const config = {
         entry: "./src/main.js",
         output: {
             path: path.resolve(__dirname, "dist"),
@@ -92,7 +84,7 @@ module.exports = (env, argv) => {
         devtool: "source-map",
         plugins: [
             new CleanWebpackPlugin(),
-            new CopyWebpackPlugin([{ from: "debugger/debugger.js", to: "debugger.js" }]),
+            new CopyWebpackPlugin({ patterns: [{ from: "debugger/debugger.js", to: "debugger.js" }] }),
             new webpack.DefinePlugin({
                 GITVERSION: JSON.stringify(git.version()),
                 GITHASH: JSON.stringify(git.commithash()),
@@ -103,6 +95,7 @@ module.exports = (env, argv) => {
                 include: /^skulpt\.min\.js$/,
                 algorithm: "gzip",
             }),
+            ...plugins,
         ],
         optimization: opt,
         resolve: {
