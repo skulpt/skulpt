ab69654c6f7eb3c19c51d2a38f2a052e03bf3333
paganaye <>
2024-05-11 22:22:34 +0200

    build unit2 and unit3.js on the fly in webserver.

--- support/run/brun.js
+++ support/run/brun.js
@@ -1,14 +1,18 @@
+"use strict"
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const open = require('open');
 const program = require('commander');
+const serveIndex = require('serve-index');
 
 const datafilelist = [
     "src/lib/StringIO.py",
     "test/unit/file.txt",
     "skulpt.py"
 ];
+let skulpt = skulptName();
+
 
 function skulptName() {
     let files = fs.readdirSync("dist");
@@ -38,36 +42,65 @@ function getFileNames(dir) {
     return filelist;
 }
 
+function getFileNamesAndContent(dir) {
+    let filelist = {};
+    let files = fs.readdirSync(dir);
+
+    files.forEach((file) => {
+        let fullname = path.resolve(dir, file);
+        let stat = fs.statSync(fullname);
+        let basename = path.basename(file, ".py");
+
+        if (stat.isFile() && basename.startsWith("test_") && (path.extname(file) == ".py")) {
+            filelist[dir + '/' + file] = fs.readFileSync(fullname).toString();
+        }
+    });
+
+    return filelist;
+}
+
+function showRunner(filePath, req, res) {
+    let prog;
+    try {
+        prog = fs.readFileSync(filePath, 'utf8');
+    } catch (error) {
+        prog = error.toString();
+    }
+    res.render(path.resolve('support', 'run', 'run_template'), {
+        skulpt: skulpt,
+        code: '#test \n' + prog
+    });
+}
+
+let serveIndexOptions = {
+    filter: (filename, _index, _files, dir) => {
+        const fullPath = path.join(dir, filename);
+        try {
+            const stat = fs.statSync(fullPath);
+            return stat.isDirectory() || path.extname(filename) === '.py';
+        } catch (error) {
+            return false;
+        }
+    },
+    icons: true,
+    view: 'details'
+}
+
 function brun(test, fname) {
     let app = express();
 
     // set the view engine to ejs
     app.set('view engine', 'ejs');
 
-    // Skulpt
-    app.use(express.static(path.resolve('dist')));
-    let skulpt = skulptName();
-
-    // use res.render to load up an ejs view file
-    app.get('/', function (req, res) {
-        res.render(path.resolve('support', 'run', 'home_page'), {
-        });
-
+    app.get('/test/unit2.js', function (req, res) {
+        let unit2 = getFileNamesAndContent('test/unit');
+        res.setHeader('Content-Type', 'application/javascript');
+        res.end('Sk.unit2 = { "files": ' + JSON.stringify(unit2, undefined, '  ') + '};');
     })
-    app.get('/run', function (req, res) {
-        let prog;
-        try {
-            // This is for the development server only.
-            // We allow reading of any file here
-            let fname = req.query['file']
-            prog = fs.readFileSync(fname, 'utf8');
-        } catch (error) {
-            prog = error.toString();
-        }
-        res.render(path.resolve('support', 'run', 'run_template'), {
-            skulpt: skulpt,
-            code: prog
-        });
+    app.get('/test/unit3.js', function (req, res) {
+        let unit3 = getFileNamesAndContent('test/unit3');
+        res.setHeader('Content-Type', 'application/javascript');
+        res.end('Sk.unit3 = { "files": ' + JSON.stringify(unit3, undefined, '  ') + '};');
     })
     app.get('/tests', function (req, res) {
         // Test file names
@@ -91,30 +124,36 @@ function brun(test, fname) {
 
     });
 
-    // support files
-    app.get('/test/unit2.js', function (req, res) {
-        res.sendFile(path.resolve('support', 'tmp', 'unit2.js'), {}, function (err) {
-            if (err) {
-                res.sendStatus(404);
-            }
-        });
+
+    app.use((req, res, next) => {
+        const filePath = path.join('.', req.path);
+        if (path.extname(req.path).toLowerCase() === '.py') {
+            let relativePath = req.path.substring(1);
+            showRunner(relativePath, req, res);
+        } else {
+            next(); // Continue with normal handling for non-.py files
+        }
     });
 
-    app.get('/test/unit3.js', function (req, res) {
-        res.sendFile(path.resolve('support', 'tmp', 'unit3.js'), {}, function (err) {
-            if (err) {
-                res.sendStatus(404);
-            }
-        });
-    });
+    app.use(express.static(path.resolve('.')));
 
-    app.get('/test/runner.js', function (req, res) {
-        res.sendFile(path.resolve('support', 'run', 'btestrunner.js'), {}, function (err) {
-            if (err) {
-                res.sendStatus(404);
+    let serveIndex0 = serveIndex('.', serveIndexOptions)
+    // Middleware to add a title to serveIndex
+    function modifiedServeIndex(req, res, next) {
+        let originalEnd = res.end;
+        res.end = function (chunk, encoding, callback) {
+            if (chunk) {
+                let body = chunk.toString();
+                body = body.replace(/(<body [^>]+>)/, `$1<h1>${skulpt}</h1>
+                <p>Select a python file to run</p>`);
+                chunk = Buffer.from(body, 'utf8');
             }
-        });
-    });
+            res.end = originalEnd;
+            res.end(chunk, encoding, callback);
+        };
+        serveIndex0(req, res, next);
+    }
+    app.use(modifiedServeIndex);
 
     app.use(function (req, res) {
         res.status(404).send('Page not found');
@@ -122,26 +161,16 @@ function brun(test, fname) {
 
 
     let port = 8080;
-
     let url = test ? `http://localhost:${port}/tests`
-        : `http://localhost:${port}/run?file=${encodeURIComponent(fname)}`;
+        : `http://localhost:${port}/${fname}`;
 
     console.log(`Navigate to ${url} if it doesn't open automatically.`);
-    try {
-        app.listen(port);
-    } finally {
-        open(url);
-    }
+    app.listen(port);
 }
 
 program
     .option('-t, --test', 'Run test suites')
     .option('-p, --program <file>', 'file to run')
     .parse(process.argv);
-
-if (!program.test && !program.program) {
-    console.log("error: option `-p, --program <file>' must specify a program to run");
-    process.exit();
-}
 
 brun(program.test, program.program);
